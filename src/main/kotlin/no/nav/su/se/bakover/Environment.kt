46e5bb35383358b8c114fa6b033a72f011d39492
Håkon <hakon.arneng.holmstedt@nav.no>
2020-01-06 13:17:25 +0100

    legger til noen local.env

--- src/main/kotlin/no/nav/su/se/bakover/Environment.kt
+++ src/main/kotlin/no/nav/su/se/bakover/Environment.kt
@@ -1,22 +1,18 @@
 package no.nav.su.se.bakover
 
-const val corsKey: String = "ALLOW_CORS_ORIGIN"
-
 data class Environment(
-        val suPersonUrl: String = "http://su-person",
-        val suInntektUrl: String = "http://su-inntekt",
-        val oidcConfigUrl: String = getEnvVar("OIDC_CONFIG_URL"),
-        val oidcClientId: String = getEnvVar("OIDC_CLIENT_ID"),
-        val oidcRequiredGroup: String = getEnvVar("OIDC_REQUIRED_GROUP"),
-        val allowCorsOrigin: String = getEnvVar(corsKey)
+    val suPersonUrl: String = "http://su-person",
+    val suInntektUrl: String = "http://su-inntekt",
+    val oidcConfigUrl: String = getEnvVar("OIDC_CONFIG_URL"),
+    val oidcClientId: String = getEnvVar("OIDC_CLIENT_ID"),
+    val oidcRequiredGroup: String = getEnvVar("OIDC_REQUIRED_GROUP"),
+    val allowCorsOrigin: String = getEnvVar("ALLOW_CORS_ORIGIN"),
+    val azureTenant:String = getEnvVar("AZURE_TENANT_ID"),
+    val jwtIssuer: String = "https://login.microsoftonline.com/$azureTenant/v2.0",
+    val jwksUrl: String = "https://login.microsoftonline.com/$azureTenant/discovery/v2.0/keys"
 )
 
 private fun getEnvVar(varName: String) = getOptionalEnvVar(varName) ?: throw Exception("mangler verdi for $varName")
 
 private fun getOptionalEnvVar(varName: String): String? = System.getenv(varName)
-    val allowCorsOrigin: String = envVar(CORS_KEY)
-    val authorizedUsers: List<String> = envVar("ALLOWED_ROLES").split(",")
-    val azureTenant = envVar("AZURE_TENANT_ID")
-    val jwtIssuer: String = "https://login.microsoftonline.com/$azureTenant/v2.0"
-    val jwksUrl: String = "https://login.microsoftonline.com/$azureTenant/discovery/v2.0/keys"
 
