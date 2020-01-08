128240cd727eb824bb41eb056b7f467b8a727772
Jacob Meidell <jacob.meidell@nav.no>
2020-01-08 08:15:54 +0100

    Added some basic tests for authentication.

---
+++ src/test/kotlin/no/nav/su/se/bakover/AuthenticationComponentTest.kt
@@ -0,0 +1,125 @@
+package no.nav.su.se.bakover
+
+import com.github.tomakehurst.wiremock.WireMockServer
+import com.github.tomakehurst.wiremock.client.WireMock
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration
+import io.ktor.http.HttpHeaders.Authorization
+import io.ktor.http.HttpMethod.Companion.Get
+import io.ktor.http.HttpStatusCode
+import io.ktor.http.HttpStatusCode.Companion.Unauthorized
+import io.ktor.server.testing.handleRequest
+import io.ktor.server.testing.withTestApplication
+import io.ktor.util.KtorExperimentalAPI
+import no.nav.su.se.bakover.nais.testEnv
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+import java.time.Instant
+import java.util.*
+import kotlin.test.assertEquals
+
+const val secureEndpoint = "/secretest"
+
+@KtorExperimentalAPI
+internal class AuthenticationComponentTest {
+
+    @Test
+    fun `secretest krever autentisering`() {
+        withTestApplication({
+            testEnv(wireMockServer)
+            susebakover()
+        }) {
+            handleRequest(Get, secureEndpoint)
+        }.apply {
+            assertEquals(Unauthorized, response.status())
+        }
+    }
+
+    @Test
+    fun `secretest ok med gyldig token`() {
+        val token = jwtStub.createTokenFor()
+
+        withTestApplication({
+            testEnv(wireMockServer)
+            susebakover()
+        }) {
+            handleRequest(Get, secureEndpoint) {
+                addHeader(Authorization, "Bearer $token")
+            }
+        }.apply {
+            assertEquals(HttpStatusCode.OK, response.status())
+        }
+    }
+
+    @Test
+    fun `forespørsel uten påkrevet audience skal svare med 401`() {
+        val token = jwtStub.createTokenFor(audience = "wrong_audience")
+
+        withTestApplication({
+            testEnv(wireMockServer)
+            susebakover()
+        }) {
+            handleRequest(Get, secureEndpoint) {
+                addHeader(Authorization, "Bearer $token")
+            }
+        }.apply {
+            Assertions.assertEquals(Unauthorized, response.status())
+        }
+    }
+
+    @Test
+    fun `forespørsel uten medlemskap i påkrevet gruppe skal svare med 401`() {
+        val token = jwtStub.createTokenFor(groups = listOf("WRONG_GROUP_UUID"))
+
+        withTestApplication({
+            testEnv(wireMockServer)
+            susebakover()
+        }) {
+            handleRequest(Get, secureEndpoint) {
+                addHeader(Authorization, "Bearer $token")
+            }
+        }.apply {
+            Assertions.assertEquals(Unauthorized, response.status())
+        }
+    }
+
+    @Test
+    fun `forespørsel med utgått token skal svare med 401`() {
+        val token = jwtStub.createTokenFor(expiresAt = Date.from(Instant.now().minusSeconds(1)))
+
+        withTestApplication({
+            testEnv(wireMockServer)
+            susebakover()
+        }) {
+            handleRequest(Get, secureEndpoint) {
+                addHeader(Authorization, "Bearer $token")
+            }
+        }.apply {
+            Assertions.assertEquals(Unauthorized, response.status())
+        }
+    }
+
+    companion object {
+        private val wireMockServer: WireMockServer = WireMockServer(WireMockConfiguration.options().dynamicPort())
+        private val jwtStub by lazy {
+            JwtStub(wireMockServer)
+        }
+
+        @BeforeAll
+        @JvmStatic
+        fun start() {
+            wireMockServer.start()
+            WireMock.stubFor(jwtStub.stubbedJwkProvider())
+            WireMock.stubFor(jwtStub.stubbedConfigProvider())
+        }
+
+        @AfterAll
+        @JvmStatic
+        fun stop() {
+            wireMockServer.stop()
+        }
+    }
+}
+
+
