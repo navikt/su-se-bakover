fbd0bf81d9562d86a0fe8aa61f9b619ec34371ad
Jacob Meidell <jacob.meidell@nav.no>
2020-01-09 14:19:46 +0100

    CORS - allow both http and https schemes.

--- src/main/kotlin/no/nav/su/se/bakover/Application.kt
+++ src/main/kotlin/no/nav/su/se/bakover/Application.kt
@@ -39,7 +39,7 @@ fun Application.module(
     install(CORS) {
         method(Options)
         header(Authorization)
-        host(fromEnvironment("cors.allow.origin"))
+        host(fromEnvironment("cors.allow.origin"), listOf("http","https"))
     }
 
     setupAuthentication(
