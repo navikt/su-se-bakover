422b8e13ea7b3d7a64eb192ef1201c7be395aaab
Jacob Meidell <jacob.meidell@nav.no>
2020-01-09 10:53:51 +0100

    Printing the keys exported by the init-script. Renamed env-vars.

--- init-scripts/export_app_envs.sh
+++ init-scripts/export_app_envs.sh
@@ -3,5 +3,6 @@
 APP_SECRETS_FOLDER=/var/run/secrets/nais.io/vault/su-se-bakover
 [ -d "$APP_SECRETS_FOLDER" ] && {
     . "$APP_SECRETS_FOLDER"/*.env
+    echo - exporting $(cut -d= -f1 "$APP_SECRETS_FOLDER"/*.env)
     export $(cut -d= -f1 "$APP_SECRETS_FOLDER"/*.env)
 }
