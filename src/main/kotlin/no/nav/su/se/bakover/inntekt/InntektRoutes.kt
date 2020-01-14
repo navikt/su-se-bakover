3e4cfe6ea38cfa9f20c0a87e5149b8661ffb8caf
J-K. Solbakken <jksolbakken@gmail.com>
2020-01-14 11:49:06 +0100

    audit log

--- src/main/kotlin/no/nav/su/se/bakover/inntekt/InntektRoutes.kt
+++ src/main/kotlin/no/nav/su/se/bakover/inntekt/InntektRoutes.kt
@@ -1,8 +1,10 @@
 package no.nav.su.se.bakover.inntekt
 
 import io.ktor.application.call
+import io.ktor.auth.*
+import io.ktor.auth.jwt.*
 import io.ktor.config.ApplicationConfig
-import io.ktor.http.HttpHeaders
+import io.ktor.http.*
 import io.ktor.request.header
 import io.ktor.response.respond
 import io.ktor.routing.Route
@@ -10,14 +12,22 @@ import io.ktor.routing.get
 import io.ktor.util.KtorExperimentalAPI
 import no.nav.su.se.bakover.azure.AzureClient
 import no.nav.su.se.bakover.getProperty
+import org.slf4j.*
 
 const val inntektPath = "/inntekt"
 const val identLabel = "ident"
 
+private val sikkerLogg = LoggerFactory.getLogger("sikkerLogg")
+
 @KtorExperimentalAPI
 fun Route.inntektRoutes(config: ApplicationConfig, azureClient: AzureClient, inntektClient: SuInntektClient) {
     get(path = inntektPath) {
-        val suInntektToken = azureClient.onBehalfOFToken(call.request.header(HttpHeaders.Authorization)!!, config.getProperty("integrations.suInntekt.clientId"))
-        call.respond(inntektClient.inntekt(ident = call.parameters[identLabel]!!, suInntektToken = suInntektToken))
+        call.parameters[identLabel]?.let { personIdent ->
+            val principal = (call.authentication.principal as JWTPrincipal).payload
+            sikkerLogg.info("${principal.subject} slår opp inntekt for person $personIdent")
+            val suInntektToken = azureClient.onBehalfOFToken(call.request.header(HttpHeaders.Authorization)!!,
+                    config.getProperty("integrations.suInntekt.clientId"))
+            call.respond(inntektClient.inntekt(ident = personIdent, suInntektToken = suInntektToken))
+            } ?: call.respond(HttpStatusCode.BadRequest, "query param '${identLabel}' må oppgis")
     }
 }
--- src/main/kotlin/no/nav/su/se/bakover/person/PersonRoutes.kt
+++ src/main/kotlin/no/nav/su/se/bakover/person/PersonRoutes.kt
@@ -1,8 +1,10 @@
 package no.nav.su.se.bakover.person
 
 import io.ktor.application.call
+import io.ktor.auth.*
+import io.ktor.auth.jwt.*
 import io.ktor.config.ApplicationConfig
-import io.ktor.http.HttpHeaders
+import io.ktor.http.*
 import io.ktor.request.header
 import io.ktor.response.respond
 import io.ktor.routing.Route
@@ -10,14 +12,22 @@ import io.ktor.routing.get
 import io.ktor.util.KtorExperimentalAPI
 import no.nav.su.se.bakover.azure.AzureClient
 import no.nav.su.se.bakover.getProperty
+import org.slf4j.*
 
 const val personPath = "/person"
 const val identLabel = "ident"
 
+private val sikkerLogg = LoggerFactory.getLogger("sikkerLogg")
+
 @KtorExperimentalAPI
 fun Route.personRoutes(config: ApplicationConfig, azureClient: AzureClient, personClient: SuPersonClient) {
     get(personPath) {
-        val suPersonToken = azureClient.onBehalfOFToken(call.request.header(HttpHeaders.Authorization)!!, config.getProperty("integrations.suPerson.clientId"))
-        call.respond(personClient.person(ident = call.parameters[identLabel]!!, suPersonToken = suPersonToken))
+        call.parameters[identLabel]?.let { personIdent ->
+            val principal = (call.authentication.principal as JWTPrincipal).payload
+            sikkerLogg.info("${principal.subject} gjør oppslag på person $personIdent")
+            val suPersonToken = azureClient.onBehalfOFToken(call.request.header(HttpHeaders.Authorization)!!,
+                    config.getProperty("integrations.suPerson.clientId"))
+            call.respond(personClient.person(ident = personIdent, suPersonToken = suPersonToken))
+        } ?: call.respond(HttpStatusCode.BadRequest, "query param '$identLabel' må oppgis")
     }
 }
