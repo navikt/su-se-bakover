9ac7068ebced69be06772fd3abb5432e9cd7a81e
Håkon <hakon.arneng.holmstedt@nav.no>
2020-01-16 14:41:47 +0100

    inntekt bruker post for å skjule fnr i url

--- src/main/kotlin/no/nav/su/se/bakover/inntekt/SuInntektClient.kt
+++ src/main/kotlin/no/nav/su/se/bakover/inntekt/SuInntektClient.kt
@@ -1,16 +1,17 @@
 package no.nav.su.se.bakover.inntekt
 
 import com.github.kittinunf.fuel.httpGet
+import com.github.kittinunf.fuel.httpPost
 import io.ktor.http.HttpHeaders.Authorization
 import io.ktor.http.HttpHeaders.XRequestId
 import org.slf4j.MDC
 
 class SuInntektClient(suInntektBaseUrl: String) {
     private val inntektResource = "$suInntektBaseUrl/inntekt"
-    private val suInntektIdentLabel = "ident"
+    private val suInntektIdentLabel = "fnr"
 
     fun inntekt(ident: String, suInntektToken: String): String {
-        val (_, _, result) = inntektResource.httpGet(listOf(suInntektIdentLabel to ident))
+        val (_, _, result) = inntektResource.httpPost(listOf(suInntektIdentLabel to ident))
                 .header(Authorization, "Bearer $suInntektToken")
                 .header(XRequestId, MDC.get(XRequestId))
                 .responseString()
