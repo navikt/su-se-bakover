5f6aae2c72fb2ad0019c4d8355b9dfe935824ca7
Jacob Meidell <jacob.meidell@nav.no>
2020-01-22 15:02:10 +0100

    income for 2020

--- src/main/kotlin/no/nav/su/se/bakover/inntekt/SuInntektClient.kt
+++ src/main/kotlin/no/nav/su/se/bakover/inntekt/SuInntektClient.kt
@@ -17,8 +17,8 @@ class SuInntektClient(suInntektBaseUrl: String) {
         val (_, _, result) = inntektResource.httpPost(
             listOf(
                 suInntektIdentLabel to ident,
-                "fom" to "2019-01",
-                "tom" to "2019-12"
+                "fom" to "2020-01",
+                "tom" to "2020-12"
             )
         )
             .header(Authorization, "Bearer $suInntektToken")
