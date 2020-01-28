5a9a23cf485a822946d2f17c6917187431005237
Jacob Meidell <jacob.meidell@nav.no>
2020-01-28 12:55:37 +0100

    Avoid json parse error frontend.

--- src/main/kotlin/no/nav/su/se/bakover/person/PersonRoutes.kt
+++ src/main/kotlin/no/nav/su/se/bakover/person/PersonRoutes.kt
@@ -4,12 +4,14 @@ import io.ktor.application.call
 import io.ktor.auth.authentication
 import io.ktor.auth.jwt.JWTPrincipal
 import io.ktor.config.ApplicationConfig
+import io.ktor.http.ContentType.Text
 import io.ktor.http.HttpHeaders.Authorization
 import io.ktor.http.HttpStatusCode.Companion.BadRequest
 import io.ktor.http.HttpStatusCode.Companion.OK
 import io.ktor.http.HttpStatusCode.Companion.fromValue
 import io.ktor.request.header
 import io.ktor.response.respond
+import io.ktor.response.respondText
 import io.ktor.routing.Route
 import io.ktor.routing.get
 import io.ktor.util.KtorExperimentalAPI
@@ -34,7 +36,7 @@ fun Route.personRoutes(config: ApplicationConfig, azureClient: AzureClient, pers
 
             when (val response = personClient.person(personIdent, suPersonToken)) {
                 is Ok -> call.respond(OK, response.json)
-                is Feil -> call.respond(fromValue(response.httpCode), response.message)
+                is Feil -> call.respondText(response.message, Text.Plain, fromValue(response.httpCode))
             }
         } ?: call.respond(BadRequest, "query param '$identLabel' må oppgis")
     }
