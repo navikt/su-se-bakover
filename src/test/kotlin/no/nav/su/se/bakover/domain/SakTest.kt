5a60c245b6e53637eea5792a9264c9ae968a3441
Jacob Meidell <jacob.meidell@nav.no>
2020-03-11 12:33:54 +0100

    Add correlation id to mdc for test.

--- src/test/kotlin/no/nav/su/se/bakover/domain/SakTest.kt
+++ src/test/kotlin/no/nav/su/se/bakover/domain/SakTest.kt
@@ -1,9 +1,12 @@
 package no.nav.su.se.bakover.domain
 
+import io.ktor.http.HttpHeaders.XCorrelationId
+import no.nav.su.se.bakover.DEFAULT_CALL_ID
 import no.nav.su.se.bakover.Either
 import no.nav.su.se.bakover.Fødselsnummer
 import no.nav.su.se.bakover.db.Repository
 import org.junit.jupiter.api.Test
+import org.slf4j.MDC
 import kotlin.random.Random
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
@@ -28,29 +31,30 @@ internal class SakTest {
     fun `factory må klare å lage en ny sak fra et fnr, når det ikke finnes en sak fra før`() {
         val nySakTest = AssertNySakOpprettet()
         SakFactory(
-            repository = TomtRepository(),
-            søknadFactory = SøknadFactory(TomtRepository(), emptyList()),
-            sakObservers = listOf(nySakTest)
+                repository = TomtRepository(),
+                søknadFactory = SøknadFactory(TomtRepository(), emptyList()),
+                sakObservers = listOf(nySakTest)
         )
-            .forFnr(førstegangssøker)
+                .forFnr(førstegangssøker)
 
         assertTrue(nySakTest.nySak, "Ny sak event skulle blitt trigget")
     }
 
     @Test
     fun `factory må klare å hente en sak fra repository, og så legge på en ny søknad`() {
+        MDC.put(XCorrelationId, DEFAULT_CALL_ID)
         val repository = RepositoryForNySøknad()
         val nySøknadTest = AssertNySøknadMottat()
         SakFactory(
-            repository = repository,
-            søknadFactory = SøknadFactory(
-                repository,
-                listOf(nySøknadTest)
-            ),
-            sakObservers = emptyList()
+                repository = repository,
+                søknadFactory = SøknadFactory(
+                        repository,
+                        listOf(nySøknadTest)
+                ),
+                sakObservers = emptyList()
         )
-            .forFnr(andregangssøker)
-            .nySøknad(søknadstekst)
+                .forFnr(andregangssøker)
+                .nySøknad(søknadstekst)
 
         assertTrue(nySøknadTest.nySøknad, "Søknad mottatt event skulle blitt trigget")
     }
@@ -58,11 +62,11 @@ internal class SakTest {
     @Test
     fun `factory må levere en Error ved henting av sak med en identitet som ikke finnes`() {
         val eitherSakOrNothing = SakFactory(
-            repository = TomtRepository(),
-            søknadFactory = SøknadFactory(TomtRepository(), emptyList()),
-            sakObservers = emptyList()
+                repository = TomtRepository(),
+                søknadFactory = SøknadFactory(TomtRepository(), emptyList()),
+                sakObservers = emptyList()
         ).forId(nySakId)
-        when(eitherSakOrNothing) {
+        when (eitherSakOrNothing) {
             is Either.Left -> assertTrue(true)
             is Either.Right -> fail("Skulle ikke ha funnet en sak")
         }
@@ -71,14 +75,14 @@ internal class SakTest {
     @Test
     fun `factory må klare å hente en sak fra repository basert på en identitet`() {
         val eitherSakOrNothing = SakFactory(
-            repository = RepositoryForNySøknad(),
-            søknadFactory = SøknadFactory(
-                RepositoryForNySøknad(),
-                emptyList()
-            ),
-            sakObservers = emptyList()
+                repository = RepositoryForNySøknad(),
+                søknadFactory = SøknadFactory(
+                        RepositoryForNySøknad(),
+                        emptyList()
+                ),
+                sakObservers = emptyList()
         ).forId(eksisterendeSakId)
-        when(eitherSakOrNothing) {
+        when (eitherSakOrNothing) {
             is Either.Left -> fail("Skulle ikke ha fått feil fra søknadFactory")
             is Either.Right -> assertTrue(true)
         }
@@ -96,7 +100,7 @@ internal class TomtRepository : Repository {
     override fun søknadForId(id: Long): Pair<Long, String>? = null
 }
 
-internal class RepositoryForNySøknad: Repository {
+internal class RepositoryForNySøknad : Repository {
     override fun nySak(fnr: Fødselsnummer): Long = throw RuntimeException("Skulle ikke lagre sak")
     override fun sakIdForFnr(fnr: Fødselsnummer): Long? = eksisterendeSakId
     override fun nySøknad(sakId: Long, json: String): Long = nySøknadId
@@ -119,11 +123,11 @@ internal class AssertNySøknadMottat : SøknadObserver {
     override fun søknadMottatt(event: SøknadObserver.SøknadMottattEvent) {
         nySøknad = true
         assertEquals(
-            SøknadObserver.SøknadMottattEvent(
-                sakId = eksisterendeSakId,
-                søknadId = nySøknadId,
-                søknadstekst = søknadstekst,
-                fnr = andregangssøker
-        ), event)
+                SøknadObserver.SøknadMottattEvent(
+                        sakId = eksisterendeSakId,
+                        søknadId = nySøknadId,
+                        søknadstekst = søknadstekst,
+                        fnr = andregangssøker
+                ), event)
     }
 }
