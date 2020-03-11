73eafc57fe1ba3d598bdf54da068993cca9d0625
Jacob Meidell <jacob.meidell@nav.no>
2020-03-11 10:26:15 +0100

    Add correlation id to events.

--- src/main/kotlin/no/nav/su/se/bakover/domain/Søknad.kt
+++ src/main/kotlin/no/nav/su/se/bakover/domain/Søknad.kt
@@ -1,22 +1,26 @@
 package no.nav.su.se.bakover.domain
 
+import io.ktor.http.HttpHeaders.XCorrelationId
 import no.nav.su.se.bakover.Either
-import no.nav.su.se.bakover.Either.*
+import no.nav.su.se.bakover.Either.Left
+import no.nav.su.se.bakover.Either.Right
 import no.nav.su.se.bakover.Fødselsnummer
 import no.nav.su.se.bakover.db.Repository
+import org.slf4j.MDC
 
 private const val NO_SUCH_IDENTITY = Long.MIN_VALUE
+
 internal class Søknad internal constructor(
-    private var id: Long = NO_SUCH_IDENTITY,
-    private val json: String,
-    private val observers: List<SøknadObserver>
+        private var id: Long = NO_SUCH_IDENTITY,
+        private val json: String,
+        private val observers: List<SøknadObserver>
 ) {
     internal fun lagreSøknad(sakId: Long, repository: Repository, fnr: Fødselsnummer): Søknad = this.also {
         id = repository.nySøknad(sakId, json)
         observers.forEach { it.søknadMottatt(SøknadObserver.SøknadMottattEvent(sakId = sakId, søknadId = id, søknadstekst = json, fnr = fnr)) }
     }
 
-    fun toJson():String = """
+    fun toJson(): String = """
         {
             "id": $id,
             "json": $json
@@ -34,6 +38,7 @@ internal class SøknadFactory(private val repository: Repository, private val ob
 }
 
 internal interface SøknadObserver {
-    data class SøknadMottattEvent(val sakId: Long, val søknadId: Long, val søknadstekst: String, val fnr: Fødselsnummer)
+    data class SøknadMottattEvent(val correlationId: String = MDC.get(XCorrelationId), val sakId: Long, val søknadId: Long, val søknadstekst: String, val fnr: Fødselsnummer)
+
     fun søknadMottatt(event: SøknadMottattEvent)
 }
--- src/main/kotlin/no/nav/su/se/bakover/kafka/SøknadMottattEmitter.kt
+++ src/main/kotlin/no/nav/su/se/bakover/kafka/SøknadMottattEmitter.kt
@@ -11,10 +11,10 @@ import org.apache.kafka.clients.producer.KafkaProducer
 
 @KtorExperimentalAPI
 internal class SøknadMottattEmitter(
-    private val kafka: KafkaProducer<String, String>,
-    private val azureClient: OAuth,
-    private val suPersonClientId: String,
-    private val personClient: PersonOppslag
+        private val kafka: KafkaProducer<String, String>,
+        private val azureClient: OAuth,
+        private val suPersonClientId: String,
+        private val personClient: PersonOppslag
 ) : SøknadObserver {
     override fun søknadMottatt(event: SøknadMottattEvent) {
         val token = azureClient.token(suPersonClientId)
@@ -22,4 +22,5 @@ internal class SøknadMottattEmitter(
         kafka.send(event.somNySøknad(aktørId).toProducerRecord(Topics.SØKNAD_TOPIC))
     }
 }
-private fun SøknadMottattEvent.somNySøknad(aktørId: String): NySøknad = NySøknad(fnr = fnr.toString(), sakId = "${sakId}", aktørId = aktørId, søknadId = "${søknadId}", søknad = søknadstekst)
+
+private fun SøknadMottattEvent.somNySøknad(aktørId: String): NySøknad = NySøknad(correlationId = correlationId, fnr = fnr.toString(), sakId = "${sakId}", aktørId = aktørId, søknadId = "${søknadId}", søknad = søknadstekst)
