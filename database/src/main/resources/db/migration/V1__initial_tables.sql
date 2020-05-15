71d7591dcaf19fea0d18293e905bc92be026131d
Håkon <hakon.arneng.holmstedt@nav.no>
2020-05-15 11:22:59 +0200

    forbereder flyway hack. Vi må få flyway til å kjøre på nytt fra v1, men v1 må ikke innføre endringer, så vi må innføre create if not exists (som vi burde hatt fra begynnelsen av) og så må vi slette innholdet i version-history-tabellen til flyway

--- database/src/main/resources/db/migration/V1__initial_tables.sql
+++ database/src/main/resources/db/migration/V1__initial_tables.sql
@@ -1,18 +1,18 @@
-create table sak
+create table if not exists sak
 (
     id        bigserial	 			   primary key,
     fnr       varchar(11)              not null,
     opprettet timestamp with time zone not null default (now() at time zone 'utc')
 );
 
-create table søknad
+create table if not exists søknad
 (
     id        bigserial	 			   primary key,
     json      JSONB                    not null,
     opprettet timestamp with time zone not null default (now() at time zone 'utc')
 );
 
-create table stønadsperiode
+create table if not exists stønadsperiode
 (
 	id bigserial primary key,
 	sakId bigint not null references sak(id),
