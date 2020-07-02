0f22eaaf7a35b12df2c295cb68cde3d9c82c3420
Jacob Meidell <jacob.meidell@nav.no>
2020-07-02 08:53:02 +0200

    Add config to jackson serde.

--- web/src/main/kotlin/no/nav/su/se/bakover/web/Jackson.kt
+++ web/src/main/kotlin/no/nav/su/se/bakover/web/Jackson.kt
@@ -1,9 +1,15 @@
 package no.nav.su.se.bakover.web
 
+import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 
 internal val objectMapper = jacksonObjectMapper()
+    .registerModule(JavaTimeModule())
+    .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+    .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
 
 inline fun <reified K, reified V> ObjectMapper.readMap(value: String): Map<K, V> = readValue<Map<K, V>>(
     value,
