f25331c888be4b60e17374abdeeaba0ca9b9c538
Ingar Almklov <ingara@gmail.com>
2020-07-09 10:44:44 +0200

    BehandlingRoutes: add endpoint for creating behandling
    Co-authored-by: Jacob Meidell <jacob.meidell@nav.no>
    Co-authored-by: Aryan Iranzamini <aryaniranzamini@gmail.com>

--- web/src/test/kotlin/no/nav/su/se/bakover/web/routes/behandling/BehandlingRoutesKtTest.kt
+++ web/src/test/kotlin/no/nav/su/se/bakover/web/routes/behandling/BehandlingRoutesKtTest.kt
@@ -1,9 +1,12 @@
 package no.nav.su.se.bakover.web.routes.behandling
 
 import com.fasterxml.jackson.module.kotlin.readValue
+import io.kotest.assertions.throwables.shouldNotThrow
 import io.kotest.matchers.shouldBe
 import io.ktor.http.HttpMethod
+import io.ktor.http.HttpStatusCode
 import io.ktor.locations.KtorExperimentalLocationsAPI
+import io.ktor.server.testing.setBody
 import io.ktor.server.testing.withTestApplication
 import io.ktor.util.KtorExperimentalAPI
 import no.nav.su.meldinger.kafka.soknad.SøknadInnholdTestdataBuilder
@@ -13,6 +16,7 @@ import no.nav.su.se.bakover.domain.Behandling
 import no.nav.su.se.bakover.web.FnrGenerator
 import no.nav.su.se.bakover.web.defaultRequest
 import no.nav.su.se.bakover.web.objectMapper
+import no.nav.su.se.bakover.web.routes.sak.sakPath
 import no.nav.su.se.bakover.web.routes.vilkårsvurdering.toJson
 import no.nav.su.se.bakover.web.testEnv
 import no.nav.su.se.bakover.web.testSusebakover
@@ -40,6 +44,24 @@ internal class BehandlingRoutesKtTest {
                     id = behandlingsId.toString(),
                     vilkårsvurderinger = vilkårsvurderinger.map { it.toDto() }.toJson()
                 )
+            }
+        }
+    }
+
+    @Test
+    fun `kan opprette behandling på en sak og søknad`() {
+        withTestApplication({
+            testEnv()
+            testSusebakover()
+        }) {
+            val sak = repo.opprettSak(FnrGenerator.random())
+            val søknad = sak.nySøknad(SøknadInnholdTestdataBuilder.build())
+
+            defaultRequest(HttpMethod.Post, "$sakPath/${sak.toDto().id}/behandlinger") {
+                setBody("""{ "soknadId": "${søknad.toDto().id}" }""")
+            }.apply {
+                response.status() shouldBe HttpStatusCode.Created
+                shouldNotThrow<Throwable> { objectMapper.readValue<BehandlingJson>(response.content!!) }
             }
         }
     }
