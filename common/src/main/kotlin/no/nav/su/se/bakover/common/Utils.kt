3af798be4d22ad4861e623906b2bb1d1c5839d0c
Jacob Meidell <jacob.meidell@nav.no>
2020-07-15 07:45:23 +0200

    Add convenient dates.
    Add test class for utils, remove old arrow test.

--- common/src/main/kotlin/no/nav/su/se/bakover/common/Utils.kt
+++ common/src/main/kotlin/no/nav/su/se/bakover/common/Utils.kt
@@ -1,4 +1,20 @@
 package no.nav.su.se.bakover.common
 
+import java.time.LocalDate
+import java.time.Month
+
 // NAIS_CLUSTER_NAME blir satt av Nais.
 fun Map<String, String>.isLocalOrRunningTests(): Boolean = this["NAIS_CLUSTER_NAME"] == null
+
+fun Int.januar(year: Int) = LocalDate.of(year, Month.JANUARY, this)
+fun Int.februar(year: Int) = LocalDate.of(year, Month.FEBRUARY, this)
+fun Int.mars(year: Int) = LocalDate.of(year, Month.MARCH, this)
+fun Int.april(year: Int) = LocalDate.of(year, Month.APRIL, this)
+fun Int.mai(year: Int) = LocalDate.of(year, Month.MAY, this)
+fun Int.juni(year: Int) = LocalDate.of(year, Month.JUNE, this)
+fun Int.juli(year: Int) = LocalDate.of(year, Month.JULY, this)
+fun Int.august(year: Int) = LocalDate.of(year, Month.AUGUST, this)
+fun Int.september(year: Int) = LocalDate.of(year, Month.SEPTEMBER, this)
+fun Int.oktober(year: Int) = LocalDate.of(year, Month.OCTOBER, this)
+fun Int.november(year: Int) = LocalDate.of(year, Month.NOVEMBER, this)
+fun Int.desember(year: Int) = LocalDate.of(year, Month.DECEMBER, this)
---
+++ common/src/test/kotlin/no/nav/su/se/bakover/common/UtilsKtTest.kt
@@ -0,0 +1,36 @@
+package no.nav.su.se.bakover.common
+
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import java.time.DateTimeException
+import java.time.LocalDate
+import java.time.Month
+
+internal class UtilsKtTest {
+    @Test
+    fun `should determine if running on local machine`() {
+        mapOf("NAIS_CLUSTER_NAME" to "dev-fss").isLocalOrRunningTests() shouldBe false
+        mapOf("key" to "value").isLocalOrRunningTests() shouldBe true
+    }
+
+    @Test
+    fun `convenient dates`() {
+        1.januar(2010) shouldBe LocalDate.of(2010, Month.JANUARY, 1)
+        2.februar(2011) shouldBe LocalDate.of(2011, Month.FEBRUARY, 2)
+        3.mars(2012) shouldBe LocalDate.of(2012, Month.MARCH, 3)
+        4.april(2013) shouldBe LocalDate.of(2013, Month.APRIL, 4)
+        5.mai(2014) shouldBe LocalDate.of(2014, Month.MAY, 5)
+        6.juni(2015) shouldBe LocalDate.of(2015, Month.JUNE, 6)
+        7.juli(2016) shouldBe LocalDate.of(2016, Month.JULY, 7)
+        8.august(2017) shouldBe LocalDate.of(2017, Month.AUGUST, 8)
+        9.september(2018) shouldBe LocalDate.of(2018, Month.SEPTEMBER, 9)
+        10.oktober(2019) shouldBe LocalDate.of(2019, Month.OCTOBER, 10)
+        11.november(2020) shouldBe LocalDate.of(2020, Month.NOVEMBER, 11)
+        12.desember(2021) shouldBe LocalDate.of(2021, Month.DECEMBER, 12)
+
+        assertThrows<DateTimeException> {
+            51.juni(2020)
+        }
+    }
+}
