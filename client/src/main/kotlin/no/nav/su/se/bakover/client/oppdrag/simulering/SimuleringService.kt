4e8c7dc6865a9d2afa88b38c144c6e2651890d4b
Jacob Meidell <jacob.meidell@nav.no>
2020-07-16 10:25:28 +0200

    Revert "Replace if with when."
    This reverts commit 53be50f61ac6171de32b0fdc8323cd24e46c336e.

--- client/src/main/kotlin/no/nav/su/se/bakover/client/oppdrag/simulering/SimuleringService.kt
+++ client/src/main/kotlin/no/nav/su/se/bakover/client/oppdrag/simulering/SimuleringService.kt
@@ -50,10 +50,9 @@ internal class SimuleringService(
                 else -> unknownTechnicalExceptionResponse(e)
             }
         } catch (e: WebServiceException) {
-            when (e.cause) {
-                is SSLException, is SocketException -> utenforÅpningstidResponse(e)
-                else -> unknownTechnicalExceptionResponse(e)
-            }
+            if (e.cause is SSLException || e.rootCause is SocketException) {
+                utenforÅpningstidResponse(e)
+            } else unknownTechnicalExceptionResponse(e)
         } catch (e: Throwable) {
             unknownTechnicalExceptionResponse(e)
         }
