860ccd999e3496784586ae80c996763728f27635
Richard Martinsen <github@martinsen.org>
2020-08-10 15:51:59 +0200

    Lagt til client for å hente kodeverk (poststed og kommunenavn)
    Co-authored-by: John Andre Hestad <john.andre.hestad@nav.no>

---
+++ client/src/main/kotlin/no/nav/su/se/bakover/client/kodeverk/KodeverkClient.kt
@@ -0,0 +1,11 @@
+package no.nav.su.se.bakover.client.kodeverk
+
+import arrow.core.Either
+
+interface KodeverkClient {
+    fun hentPoststed(postnummer: String): Either<CouldNotGetKode, String?>
+
+    fun hentKommunenavn(kommunenummer: String): Either<CouldNotGetKode, String?>
+
+    object CouldNotGetKode
+}
---
+++ client/src/test/kotlin/no/nav/su/se/bakover/client/kodeverk/KodeverkHttpClientTest.kt
@@ -0,0 +1,137 @@
+package no.nav.su.se.bakover.client.kodeverk
+
+import arrow.core.left
+import arrow.core.right
+import com.github.tomakehurst.wiremock.client.WireMock
+import io.kotest.matchers.shouldBe
+import no.nav.su.se.bakover.client.WiremockBase
+import no.nav.su.se.bakover.client.WiremockBase.Companion.wireMockServer
+import no.nav.su.se.bakover.client.kodeverk.KodeverkClient.CouldNotGetKode
+import org.junit.jupiter.api.Test
+
+internal class KodeverkHttpClientTest : WiremockBase {
+
+    @Test
+    fun `Sjekk at vi finner poststed`() {
+        wireMockServer.stubFor(
+            wiremockBuilder(kodeverkPoststedPath)
+                .willReturn(
+                    WireMock.ok(resultatPoststedJson)
+                )
+        )
+        val client = KodeverkHttpClient(wireMockServer.baseUrl(), "srvsupstonad")
+        client.hentPoststed("1479") shouldBe "KURLAND".right()
+    }
+
+    @Test
+    fun `Sjekk at vi får null hvis postnummer ikke finnes`() {
+        wireMockServer.stubFor(
+            wiremockBuilder(kodeverkPoststedPath)
+                .willReturn(
+                    WireMock.ok(resultatPoststedJson)
+                )
+        )
+        val client = KodeverkHttpClient(wireMockServer.baseUrl(), "srvsupstonad")
+        client.hentPoststed("XXXX") shouldBe null.right()
+    }
+
+    @Test
+    fun `Sjekk at vi får CouldNotGetKodeverk`() {
+        wireMockServer.stubFor(
+            wiremockBuilder(kodeverkPoststedPath)
+                .willReturn(
+                    WireMock.serverError()
+                )
+        )
+        val client = KodeverkHttpClient(wireMockServer.baseUrl(), "srvsupstonad")
+        client.hentPoststed("XXXX") shouldBe CouldNotGetKode.left()
+    }
+
+    @Test
+    fun `Sjekk at vi finner kommunenavn`() {
+        wireMockServer.stubFor(
+            wiremockBuilder(
+                kodeverkKommunePath
+            )
+                .willReturn(
+                    WireMock.ok(resultatKommuneJson)
+                )
+        )
+        val client = KodeverkHttpClient(wireMockServer.baseUrl(), "srvsupstonad")
+        client.hentKommunenavn("1103") shouldBe "Stavanger".right()
+    }
+
+    // language=JSON
+    val resultatKommuneJson =
+        """
+        {
+        "betydninger": {
+            "1101": [
+                  {
+                    "gyldigFra": "1900-01-01",
+                    "gyldigTil": "9999-12-31",
+                    "beskrivelser": {
+                      "nb": {
+                        "term": "Eigersund",
+                        "tekst": "Eigersund"
+                      }
+                    }
+                  }
+                ],
+            "1102": [],
+            "1103": [
+              {
+                "gyldigFra": "1900-01-01",
+                "gyldigTil": "9999-12-31",
+                "beskrivelser": {
+                  "nb": {
+                    "term": "Stavanger",
+                    "tekst": "Stavanger"
+                  }
+                }
+              }
+            ]
+          }
+        }
+        """.trimIndent()
+
+    // language=JSON
+    val resultatPoststedJson =
+        """
+            {
+              "betydninger": {
+                "0319": [
+                  {
+                    "gyldigFra": "1900-01-01",
+                    "gyldigTil": "9999-12-31",
+                    "beskrivelser": {
+                      "nb": {
+                        "term": "OSLO",
+                        "tekst": "OSLO"
+                      }
+                    }
+                  }
+                ],
+                "1479": [
+                  {
+                    "gyldigFra": "1900-01-01",
+                    "gyldigTil": "9999-12-31",
+                    "beskrivelser": {
+                      "nb": {
+                        "term": "KURLAND",
+                        "tekst": "KURLAND"
+                      }
+                    }
+                  }
+                ]
+              }
+            }
+        """.trimIndent()
+
+    private fun wiremockBuilder(path: String) = WireMock.get(WireMock.urlPathEqualTo(path))
+        .withHeader("Content-Type", WireMock.equalTo("application/json"))
+        .withHeader("Nav-Call-Id", WireMock.equalTo("correlationId"))
+        .withHeader("Nav-Consumer-Id", WireMock.equalTo("srvsupstonad"))
+        .withQueryParam("ekskluderUgyldige", WireMock.equalTo("true"))
+        .withQueryParam("spraak", WireMock.equalTo("nb"))
+}
