cfaaf461d57f150e2333f4643ec9c5a187bcec74
Christian H. Mosveen <mosveen@gmail.com>
2020-08-17 16:12:26 +0200

    Vedtaksbrev innvilgelse (#21)

--- domain/src/main/kotlin/no/nav/su/se/bakover/domain/VedtakInnhold.kt
+++ domain/src/main/kotlin/no/nav/su/se/bakover/domain/VedtakInnhold.kt
@@ -1,7 +1,6 @@
 package no.nav.su.se.bakover.domain
 
 import no.nav.su.se.bakover.domain.beregning.FradragDto
-import no.nav.su.se.bakover.domain.beregning.Sats
 
 data class VedtakInnhold(
     val dato: String,
@@ -14,9 +13,11 @@ data class VedtakInnhold(
     val månedsbeløp: Int?,
     val fradato: String?,
     val tildato: String?,
-    val nysøkdato: String?,
-    val sats: Sats?,
+    val sats: String?,
     val satsbeløp: Int?,
+    val satsGrunn: String,
+    val redusertStønadStatus: Boolean,
+    val redusertStønadGrunn: String?,
     val fradrag: List<FradragDto>,
     val fradragSum: Int,
     val status: Behandling.BehandlingsStatus
