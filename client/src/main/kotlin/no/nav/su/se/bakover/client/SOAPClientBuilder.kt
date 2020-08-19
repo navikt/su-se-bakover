d83245e9e8981e336948bb2e6638da38e339b42a
John Andre Hestad <john.hestad.andre@nav.no>
2020-08-19 13:11:33 +0200

    Use Simulering stub until further adue

--- client/src/main/kotlin/no/nav/su/se/bakover/client/SOAPClientBuilder.kt
+++ client/src/main/kotlin/no/nav/su/se/bakover/client/SOAPClientBuilder.kt
@@ -1,12 +1,8 @@
 package no.nav.su.se.bakover.client
 
 import io.github.cdimascio.dotenv.dotenv
-import no.nav.su.se.bakover.domain.oppdrag.simulering.SimuleringClient
-import no.nav.su.se.bakover.client.oppdrag.simulering.SimuleringConfig
-import no.nav.su.se.bakover.client.oppdrag.simulering.SimuleringSoapClient
 import no.nav.su.se.bakover.client.stubs.oppdrag.SimuleringStub
-import no.nav.su.se.bakover.common.isLocalOrRunningTests
-import org.apache.cxf.bus.extension.ExtensionManagerBus
+import no.nav.su.se.bakover.domain.oppdrag.simulering.SimuleringClient
 import org.slf4j.LoggerFactory
 
 data class SOAPClients(
@@ -19,17 +15,19 @@ object SOAPClientBuilder {
     }
     private val logger = LoggerFactory.getLogger(this::class.java)
     private fun simulering(
-        simuleringConfig: SimuleringConfig = SimuleringConfig(
-            simuleringServiceUrl = env["SIMULERING_URL"] ?: "",
-            stsSoapUrl = env["STS_URL_SOAP"] ?: "",
-            username = env["username"] ?: "username",
-            password = env["password"] ?: "password",
-            disableCNCheck = true
-        )
-    ): SimuleringClient = when (isLocalOrRunningTests()) {
-        true -> SimuleringStub.also { logger.warn("********** Using stub for ${SimuleringClient::class.java} **********") }
-        else -> SimuleringSoapClient(simuleringConfig.wrapWithSTSSimulerFpService(ExtensionManagerBus()))
-    }
+//        simuleringConfig: SimuleringConfig = SimuleringConfig(
+//            simuleringServiceUrl = env["SIMULERING_URL"] ?: "",
+//            stsSoapUrl = env["STS_URL_SOAP"] ?: "",
+//            username = env["username"] ?: "username",
+//            password = env["password"] ?: "password",
+//            disableCNCheck = true
+//        )
+    ): SimuleringClient = SimuleringStub.also { logger.warn("********** Using stub for ${SimuleringClient::class.java} **********") }
+    // TODO: Enable real client when we get the right credentials etc.
+//    when (isLocalOrRunningTests()) {
+//        true -> SimuleringStub.also { logger.warn("********** Using stub for ${SimuleringClient::class.java} **********") }
+//        else -> SimuleringSoapClient(simuleringConfig.wrapWithSTSSimulerFpService(ExtensionManagerBus()))
+//    }
 
     fun build(
         simulering: SimuleringClient = simulering()
