b9d8e19bfdbd5b34aa775f456f7f95c62d31ae54
John Andre Hestad <john.andre.hestad@nav.no>
2020-09-07 17:07:49 +0200

    Simplify and speed up lint_and_build by doing both steps as 1

--- lint_and_build.sh
+++ lint_and_build.sh
@@ -1,20 +1,2 @@
 #!/bin/bash
-expectedKtlintVersion=$(grep "val ktlintVersion" build.gradle.kts | sed 's/\(^.*"\)\(.*\)\(".*$\)/\2/')
-if ! command -v ktlint &>/dev/null; then
-  echo "Using ./gradlew ktlintFormat"
-  echo "You can increase the ktlint speed by installing https://github.com/pinterest/ktlint/releases/tag/${expectedKtlintVersion}"
-  ./gradlew ktlintFormat
-else
-  actualKtlintVersion=$(ktlint --version)
-  if [[ "$actualKtlintVersion" != "$expectedKtlintVersion" ]]; then
-    echo "Using ./gradlew ktlintFormat"
-    echo "Your ktlint version (${actualKtlintVersion}) does not match this project's. https://github.com/pinterest/ktlint/releases/tag/${expectedKtlintVersion}"
-    ./gradlew ktlintFormat
-  else
-    echo "Using $(which ktlint) instead of ./gradlew ktlintFormat for faster lint speed."
-    actualKtlintVersion=$(ktlint --version)
-    ktlint -F
-  fi
-fi
-echo "Linting complete, running: ./gradlew clean build"
-./gradlew build
+./gradlew ktlintFormat build
