2916e456b6ffc9643b62a16c2fb453aa9e1dec1d
Aryan Iranzamini <aryan.iranzamini@nav.no>
2020-09-07 10:30:59 +0200

    Put logic in BrevService instead of route
    Co-authored-by: Ramzi Abu Qassim <ramzi.abu.qassim@nav.no>

--- client/src/main/kotlin/no/nav/su/se/bakover/client/stubs/dokdistfordeling/DokDistFordelingStub.kt
+++ client/src/main/kotlin/no/nav/su/se/bakover/client/stubs/dokdistfordeling/DokDistFordelingStub.kt
@@ -10,8 +10,8 @@ object DokDistFordelingStub : DokDistFordeling {
         journalPostId: String
     ): Either<ClientError, String> =
         """
-                        {
-                            "bestillingsId": "51be490f-5a21-44dd-8d38-d762491d6b22" 
-                        }
+        {
+            "bestillingsId": "51be490f-5a21-44dd-8d38-d762491d6b22" 
+        }
         """.trimIndent().right()
 }
