b7c2bf9d4cd08817fbdc2402f48f78d299b7aaf7
John Andre Hestad <john.andre.hestad@nav.no>
2020-09-16 13:39:42 +0200

    Putt avstemmingroutes under Routes istedenfor Application

--- web/src/main/kotlin/no/nav/su/se/bakover/web/routes/avstemming/AvstemmingRoutes.kt
+++ web/src/main/kotlin/no/nav/su/se/bakover/web/routes/avstemming/AvstemmingRoutes.kt
@@ -1,30 +1,27 @@
 package no.nav.su.se.bakover.web.routes.avstemming
 
-import io.ktor.application.Application
 import io.ktor.application.call
 import io.ktor.http.HttpStatusCode
 import io.ktor.response.respond
+import io.ktor.routing.Route
 import io.ktor.routing.post
-import io.ktor.routing.routing
 import no.nav.su.se.bakover.database.ObjectRepo
 import no.nav.su.se.bakover.domain.oppdrag.avstemming.AvstemmingPublisher
 
 private const val AVSTEMMING_PATH = "/avstem"
 
 // TODO automatic job or something probably
-internal fun Application.avstemmingRoutes(
+internal fun Route.avstemmingRoutes(
     repo: ObjectRepo,
     publisher: AvstemmingPublisher
 ) {
-    routing {
-        post(AVSTEMMING_PATH) {
-            publisher.publish(AvstemmingBuilder(repo).build()).fold(
-                { call.respond(HttpStatusCode.InternalServerError, "Kunne ikke avstemme") },
-                {
-                    repo.opprettAvstemming(it).also { it.updateUtbetalinger() }
-                    call.respond("Avstemt ok")
-                }
-            )
-        }
+    post(AVSTEMMING_PATH) {
+        publisher.publish(AvstemmingBuilder(repo).build()).fold(
+            { call.respond(HttpStatusCode.InternalServerError, "Kunne ikke avstemme") },
+            {
+                repo.opprettAvstemming(it).also { it.updateUtbetalinger() }
+                call.respond("Avstemt ok")
+            }
+        )
     }
 }
