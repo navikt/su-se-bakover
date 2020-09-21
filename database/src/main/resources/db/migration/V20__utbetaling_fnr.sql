fd2d50228c251fe08a98f70da0e864c3a928d559
Jacob Meidell <jacob.meidell@nav.no>
2020-09-21 11:25:33 +0200

    Add default value for existing utbetalinger.

--- database/src/main/resources/db/migration/V20__utbetaling_fnr.sql
+++ database/src/main/resources/db/migration/V20__utbetaling_fnr.sql
@@ -1,3 +1,13 @@
 alter table utbetaling
     add column if not exists
-        fnr varchar(11) not null;
+        fnr varchar(11) not null default 'fnr';
+
+update utbetaling
+    set fnr = sak.fnr
+from (select s.fnr as fnr, o.id as oppdragid from sak s
+    join oppdrag o on o.sakid = s.id
+) as sak
+where utbetaling.oppdragid = sak.oppdragid;
+
+alter table utbetaling
+    alter column fnr drop default;
