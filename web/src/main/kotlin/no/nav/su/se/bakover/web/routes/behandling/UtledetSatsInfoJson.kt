b34049e6cfaaba4d40636d506e9d80c18f90357f
ramziabuqassim <ramzi.abu.qassim@nav.no>
2020-09-23 11:00:40 +0200

    simplify endpoint

--- web/src/main/kotlin/no/nav/su/se/bakover/web/routes/behandling/UtledetSatsInfoJson.kt
+++ web/src/main/kotlin/no/nav/su/se/bakover/web/routes/behandling/UtledetSatsInfoJson.kt
@@ -1,9 +1,6 @@
 package no.nav.su.se.bakover.web.routes.behandling
 
-import io.ktor.http.HttpStatusCode
-import no.nav.su.se.bakover.common.serialize
 import no.nav.su.se.bakover.domain.Behandling
-import no.nav.su.se.bakover.web.Resultat
 
 internal data class UtledetSatsInfoJson(
     val satsBeløp: Int?
@@ -12,6 +9,3 @@ internal data class UtledetSatsInfoJson(
 internal fun Behandling.toUtledetSatsInfoJson() = UtledetSatsInfoJson(
     satsBeløp = getUtledetSatsBeløp()
 )
-
-internal fun HttpStatusCode.utledetSatsJsonBody(behandling: Behandling) =
-    Resultat.json(this, serialize(behandling.toUtledetSatsInfoJson()))
