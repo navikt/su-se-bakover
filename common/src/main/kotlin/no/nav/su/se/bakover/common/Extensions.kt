7e99ea6ed3758a7113b48da55f13349930648854
Ingar Almklov <ingara@gmail.com>
2020-09-25 15:13:21 +0200

    Add extension for List.filterMap
    Since Arrow doesn't agree with themselves wether to use Option or not

--- common/src/main/kotlin/no/nav/su/se/bakover/common/Extensions.kt
+++ common/src/main/kotlin/no/nav/su/se/bakover/common/Extensions.kt
@@ -7,3 +7,14 @@ fun <A> Either.Companion.unsafeCatch(f: () -> A) =
     runBlocking {
         Either.catch { f() }
     }
+
+// Lager vår egen her fordi Arrow sin filterMap bruker Option, som Arrow selv sier er deprecated (noe som feiler bygget vårt)
+fun <A, B> List<A>.filterMap(predicate: Function1<A, Either<Unit, B>>): List<B> =
+    fold(emptyList()) { acc, a ->
+        predicate(a).let {
+            when (it) {
+                is Either.Left -> acc
+                is Either.Right -> acc.plus(it.b)
+            }
+        }
+    }
