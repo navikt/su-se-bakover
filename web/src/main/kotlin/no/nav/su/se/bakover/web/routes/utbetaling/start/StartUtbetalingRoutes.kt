e29b5da2390e366a8f8f7f482a2c5747f28f9c91
John Andre Hestad <john.andre.hestad@nav.no>
2020-09-29 10:48:03 +0200

    Wire up start utbetaling routes and add test

--- web/src/main/kotlin/no/nav/su/se/bakover/web/routes/utbetaling/start/StartUtbetalingRoutes.kt
+++ web/src/main/kotlin/no/nav/su/se/bakover/web/routes/utbetaling/start/StartUtbetalingRoutes.kt
@@ -1,11 +1,16 @@
 package no.nav.su.se.bakover.web.routes.utbetaling.start
 
 import io.ktor.application.call
+import io.ktor.http.HttpStatusCode.Companion.BadRequest
 import io.ktor.http.HttpStatusCode.Companion.InternalServerError
+import io.ktor.http.HttpStatusCode.Companion.NotFound
 import io.ktor.response.respond
 import io.ktor.routing.Route
 import io.ktor.routing.post
+import no.nav.su.se.bakover.common.serialize
+import no.nav.su.se.bakover.service.utbetaling.StartUtbetalingFeilet
 import no.nav.su.se.bakover.service.utbetaling.UtbetalingService
+import no.nav.su.se.bakover.web.routes.behandling.UtbetalingJson.Companion.toJson
 import no.nav.su.se.bakover.web.routes.sak.sakPath
 import no.nav.su.se.bakover.web.withSakId
 
@@ -15,8 +20,28 @@ internal fun Route.startutbetalingRoutes(
     post("$sakPath/{sakId}/utbetalinger/start") {
         call.withSakId { sakId ->
             service.startUtbetalinger(sakId).fold(
-                { call.respond(InternalServerError, "Kunne ikke starte utbetaling") },
-                { call.respond("Startet ok") }
+                {
+                    when (it) {
+                        StartUtbetalingFeilet.FantIkkeSak -> call.respond(NotFound, "Fant ikke sak")
+                        StartUtbetalingFeilet.HarIngenOversendteUtbetalinger -> call.respond(
+                            BadRequest,
+                            "Ingen utbetalinger"
+                        )
+                        StartUtbetalingFeilet.SisteUtbetalingErIkkeEnStansutbetaling -> call.respond(
+                            BadRequest,
+                            "Siste utbetaling er ikke en stans"
+                        )
+                        StartUtbetalingFeilet.SimuleringAvStartutbetalingFeilet -> call.respond(
+                            InternalServerError,
+                            "Simulering feilet"
+                        )
+                        StartUtbetalingFeilet.SendingAvUtebetalingTilOppdragFeilet -> call.respond(
+                            InternalServerError,
+                            "Oversendelse til oppdrag feilet"
+                        )
+                    }
+                },
+                { call.respond(serialize(it.toJson())) }
             )
         }
     }
