f37d7f78c8ea2005c2f4bdc1c8dbcaf5a43fc9a3
Aryan Iranzamini <aryan.iranzamini@nav.no>
2020-10-01 10:47:02 +0200

    Update only relevant tables

--- database/src/main/resources/db/migration/V25__rename_beregnet_status.sql
+++ database/src/main/resources/db/migration/V25__rename_beregnet_status.sql
@@ -1,2 +1,2 @@
-update behandling set status = REPLACE(status, 'BEREGNET', 'BEREGNET_INNVILGET');
-update behandling set status = REPLACE(status, 'BEREGNET_INVILGET', 'BEREGNET_INNVILGET');
+update behandling set status = REPLACE(status, 'BEREGNET', 'BEREGNET_INNVILGET') where behandling.status = 'BEREGNET';
+update behandling set status = REPLACE(status, 'BEREGNET_INVILGET', 'BEREGNET_INNVILGET') where behandling.status = 'BEREGNET_INVILGET';
