9bd4cd261b276b5b478169cac7840161ad61f2aa
ramziabuqassim <ramzi.abu.qassim@nav.no>
2020-10-09 09:29:36 +0200

    test -> søknader med behandling ikke avsluttes gjennom url-hack

--- database/src/test/kotlin/no/nav/su/se/bakover/database/søknad/SøknadPostgresRepoTest.kt
+++ database/src/test/kotlin/no/nav/su/se/bakover/database/søknad/SøknadPostgresRepoTest.kt
@@ -1,6 +1,7 @@
 package no.nav.su.se.bakover.database.søknad
 
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
 import no.nav.su.se.bakover.database.EmbeddedDatabase
 import no.nav.su.se.bakover.database.FnrGenerator
 import no.nav.su.se.bakover.database.TestDataHelper
@@ -61,6 +62,35 @@ internal class SøknadPostgresRepoTest {
 
             hentetSøknad!!.id shouldBe søknad.id
             hentetSøknad.avsluttetBegrunnelse shouldBe AvsluttSøkndsBehandlingBegrunnelse.Trukket
+        }
+    }
+
+    @Test
+    fun `søknader med en førstegangsbehandling skal ikke bli avsluttet`() {
+        withMigratedDb {
+            val sak = testDataHelper.insertSak(FNR)
+            val søknad = repo.opprettSøknad(
+                sakId = sak.id,
+                søknad = Søknad(
+                    id = UUID.randomUUID(),
+                    søknadInnhold = SøknadInnholdTestdataBuilder.build(),
+                )
+            )
+            val behandling = testDataHelper.insertBehandling(sak.id, søknad)
+
+            val avsluttSøknadsBehandlingBody = AvsluttSøknadsBehandlingBody(
+                sakId = sak.id,
+                søknadId = søknad.id,
+                avsluttSøkndsBehandlingBegrunnelse = AvsluttSøkndsBehandlingBegrunnelse.Trukket
+            )
+
+            repo.avsluttSøknadsBehandling(avsluttSøknadsBehandlingBody)
+            val hentetSøknad = repo.hentSøknad(søknad.id)
+
+            hentetSøknad!!.id shouldBe søknad.id
+            hentetSøknad.avsluttetBegrunnelse shouldBe null
+            behandling shouldNotBe null
+            behandling.id shouldNotBe null
         }
     }
 }
