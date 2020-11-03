b9cea2c6964eccfef494f4b6e6c7dfd0c9bfe9c3
Jacob Meidell <jacob.meidell@nav.no>
2020-11-03 16:26:17 +0100

    Cleanup db-changes.
    -Patch existing
    -Set not null by default
    -Drop default

--- database/src/main/resources/db/migration/V39__beregning-refactoring.sql
+++ database/src/main/resources/db/migration/V39__beregning-refactoring.sql
@@ -13,9 +13,26 @@ alter table fradrag
         opprettet timestamp with time zone
             default null;
 
+--Patch existing fradrag - setting values to those of parent beregning.
+with existingBeregning as(select id, opprettet, fom, tom from beregning)
+update fradrag set
+    opprettet = existingBeregning.opprettet,
+    fom = existingBeregning.fom,
+    tom = existingBeregning.tom from existingBeregning
+where existingBeregning.id = fradrag.beregningid
+
+alter table fradrag alter column fom set not null;
+alter table fradrag alter column fom drop default;
+
+alter table fradrag alter column tom set not null;
+alter table fradrag alter column tom drop default;
+
+alter table fradrag alter column opprettet set not null;
+alter table fradrag alter column opprettet drop default;
+
 alter table fradrag drop column if exists inntektDelerAvPeriode;
 
-DROP TABLE IF EXISTS månedsberegning;
+drop table if exists månedsberegning;
 
 alter table fradrag alter column beløp type decimal;
 alter table utbetalingslinje alter column beløp type decimal;
