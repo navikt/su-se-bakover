d7eaf3f214e13b1530ca294f86cb578e9eb0d298
Jacob Meidell <jacob.meidell@nav.no>
2020-11-13 10:48:55 +0100

    Add test for beregningseksempel enslig/eps over 67.
    Co-authored-by: Ingar Almklov <ingara@gmail.com>

---
+++ domain/src/test/kotlin/no/nav/su/se/bakover/domain/beregning/beregning/EnsligBeregningTest.kt
@@ -0,0 +1,68 @@
+package no.nav.su.se.bakover.domain.beregning.beregning
+
+import io.kotest.matchers.doubles.plusOrMinus
+import io.kotest.matchers.shouldBe
+import no.nav.su.se.bakover.common.april
+import no.nav.su.se.bakover.common.mai
+import no.nav.su.se.bakover.common.periode.Periode
+import no.nav.su.se.bakover.domain.beregning.BeregningStrategy
+import no.nav.su.se.bakover.domain.beregning.Beregningsgrunnlag
+import no.nav.su.se.bakover.domain.beregning.fradrag.FradragFactory
+import no.nav.su.se.bakover.domain.beregning.fradrag.FradragTilhører
+import no.nav.su.se.bakover.domain.beregning.fradrag.Fradragstype
+import org.junit.jupiter.api.Test
+
+internal class EnsligBeregningTest {
+
+    /**
+     * Eksempel fra 01.05.2020
+     +----------------+----------+----------------------------+----------+
+     |     Bruker     |          |           Total            |          |
+     +----------------+----------+----------------------------+----------+
+     | Stønadssats    | 251.350  | SUM SU/år                  | =5.027   |
+     | Arbeidsinntekt | -180.000 | SUM SU/mnd                 | =418.917 |
+     | Folketrygd     | -66.323  | Utbetalt SU/år (avrundet)  | =5.028   |
+     | SUM            | =5.027   | Utbetalt SU/mnd (avrundet) | =419     |
+     +----------------+----------+----------------------------+----------+
+     */
+    @Test
+    fun `beregningseksempel fra fagsiden`() {
+        val periode = Periode(1.mai(2020), 30.april(2021))
+        val arbeidsinntekt = 180000.0
+        val folketrygd = 66323.0
+        val beregningsgrunnlag = Beregningsgrunnlag(
+            periode = periode,
+            fradrag = listOf(
+                FradragFactory.ny(
+                    type = Fradragstype.ForventetInntekt,
+                    beløp = 0.0,
+                    periode = periode,
+                    utenlandskInntekt = null,
+                    tilhører = FradragTilhører.BRUKER
+                ),
+                FradragFactory.ny(
+                    type = Fradragstype.Arbeidsinntekt,
+                    beløp = arbeidsinntekt,
+                    periode = periode,
+                    utenlandskInntekt = null,
+                    tilhører = FradragTilhører.BRUKER
+                ),
+                FradragFactory.ny(
+                    type = Fradragstype.OffentligPensjon,
+                    beløp = folketrygd,
+                    periode = periode,
+                    utenlandskInntekt = null,
+                    tilhører = FradragTilhører.BRUKER
+                )
+            )
+        )
+
+        BeregningStrategy.BorAlene.beregn(beregningsgrunnlag).let {
+            it.getSumYtelse() shouldBe 5028
+            it.getSumFradrag() shouldBe (arbeidsinntekt + folketrygd).plusOrMinus(0.5)
+            it.getMånedsberegninger().forEach {
+                it.getSumYtelse() shouldBe 419
+            }
+        }
+    }
+}
---
+++ domain/src/test/kotlin/no/nav/su/se/bakover/domain/beregning/beregning/EpsOver67BeregningTest.kt
@@ -0,0 +1,79 @@
+package no.nav.su.se.bakover.domain.beregning.beregning
+
+import io.kotest.matchers.doubles.plusOrMinus
+import io.kotest.matchers.shouldBe
+import no.nav.su.se.bakover.common.april
+import no.nav.su.se.bakover.common.mai
+import no.nav.su.se.bakover.common.periode.Periode
+import no.nav.su.se.bakover.domain.beregning.BeregningStrategy
+import no.nav.su.se.bakover.domain.beregning.Beregningsgrunnlag
+import no.nav.su.se.bakover.domain.beregning.fradrag.FradragFactory
+import no.nav.su.se.bakover.domain.beregning.fradrag.FradragTilhører
+import no.nav.su.se.bakover.domain.beregning.fradrag.Fradragstype
+import org.junit.jupiter.api.Test
+
+internal class EpsOver67BeregningTest {
+
+    /**
+     * Eksempel fra 01.05.2020
+     +----------------+----------+-------------+----------+----------------------------+------------+
+     |     Bruker     |          |     EPS     |          |           Total            |            |
+     +----------------+----------+-------------+----------+----------------------------+------------+
+     | Stønadssats    | 231.080  | Folketrygd  |  190.000 | SUM Bruker                 | 39.999     |
+     | Arbeidsinntekt | -71.081  | MPN ordinær | -183.587 | Fradrag EPS                | -6.413     |
+     | Folketrygd     | -120.000 | SUM         |    6.413 | SUM SU/år                  | =33.586    |
+     | SUM            | =39.999  |             |          | SUM SU/mnd                 | =2.798,857 |
+     |                |          |             |          | Utbetalt SU/år (avrundet)  | =33.588    |
+     |                |          |             |          | Utbetalt SU/mnd (avrundet) | =2.799     |
+     +----------------+----------+-------------+----------+----------------------------+------------+
+     */
+    @Test
+    fun `beregningseksempel fra fagsiden`() {
+        val periode = Periode(1.mai(2020), 30.april(2021))
+        val arbeidsinntekt = 71081.0
+        val folketrygd = 120000.0
+        val epsFolketryd = 190000.0
+        val mpnOrdinær = 183587.0
+        val beregningsgrunnlag = Beregningsgrunnlag(
+            periode = periode,
+            fradrag = listOf(
+                FradragFactory.ny(
+                    type = Fradragstype.ForventetInntekt,
+                    beløp = 0.0,
+                    periode = periode,
+                    utenlandskInntekt = null,
+                    tilhører = FradragTilhører.BRUKER
+                ),
+                FradragFactory.ny(
+                    type = Fradragstype.Arbeidsinntekt,
+                    beløp = arbeidsinntekt,
+                    periode = periode,
+                    utenlandskInntekt = null,
+                    tilhører = FradragTilhører.BRUKER
+                ),
+                FradragFactory.ny(
+                    type = Fradragstype.OffentligPensjon,
+                    beløp = folketrygd,
+                    periode = periode,
+                    utenlandskInntekt = null,
+                    tilhører = FradragTilhører.BRUKER
+                ),
+                FradragFactory.ny(
+                    type = Fradragstype.OffentligPensjon,
+                    beløp = epsFolketryd,
+                    periode = periode,
+                    utenlandskInntekt = null,
+                    tilhører = FradragTilhører.EPS
+                )
+            )
+        )
+
+        BeregningStrategy.EpsOver67År.beregn(beregningsgrunnlag).let {
+            it.getSumYtelse() shouldBe 33588
+            it.getSumFradrag() shouldBe (arbeidsinntekt + folketrygd + (epsFolketryd - mpnOrdinær)).plusOrMinus(0.5)
+            it.getMånedsberegninger().forEach {
+                it.getSumYtelse() shouldBe 2799
+            }
+        }
+    }
+}
