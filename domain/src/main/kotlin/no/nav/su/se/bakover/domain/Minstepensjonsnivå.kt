6ab95165f3e0a443439ab13725bb5ad65645dd35
Jacob Meidell <jacob.meidell@nav.no>
2020-11-13 09:34:52 +0100

    Add periodization of minstepensjonsnivå.
    Co-authored-by: Ingar Almklov <ingara@gmail.com>

--- domain/src/main/kotlin/no/nav/su/se/bakover/domain/Minstepensjonsnivå.kt
+++ domain/src/main/kotlin/no/nav/su/se/bakover/domain/Minstepensjonsnivå.kt
@@ -1,5 +1,6 @@
 package no.nav.su.se.bakover.domain
 
+import no.nav.su.se.bakover.common.periode.Periode
 import java.time.LocalDate
 import java.time.Month
 
@@ -7,16 +8,20 @@ enum class Minstepensjonsnivå {
     // Dette er den eneste vi trenger p.t., men det finnes også lav og høy
     Ordinær;
 
-    fun forDato(dato: LocalDate): Int = datoToBeløp.entries
-        .sortedByDescending { it.key }
-        .first { dato.isAfter(it.key) || dato.isEqual(it.key) }
-        .value
-        .get(this)
-
-    private val datoToBeløp: Map<LocalDate, Pensjonsnivåverdier> = mapOf(
+    private val datoToMinstepensjonsnivå: Map<LocalDate, Pensjonsnivåverdier> = mapOf(
         LocalDate.of(2019, Month.SEPTEMBER, 1) to Pensjonsnivåverdier(ordinær = 181908),
         LocalDate.of(2020, Month.MAY, 1) to Pensjonsnivåverdier(ordinær = 183587),
     )
+
+    fun forDato(dato: LocalDate): Int = datoToMinstepensjonsnivå.entries
+        .sortedByDescending { it.key }
+        .first { dato.isAfter(it.key) || dato.isEqual(it.key) }.value.get(this)
+
+    fun periodiser(periode: Periode) = periode.tilMånedsperioder()
+        .map { it to minstepensjonsnivåSomMånedsbeløp(it.getTilOgMed()) }
+        .toMap()
+
+    private fun minstepensjonsnivåSomMånedsbeløp(dato: LocalDate) = this.forDato(dato) / 12.0
 
     private inner class Pensjonsnivåverdier(val ordinær: Int) {
         fun get(nivå: Minstepensjonsnivå) =
