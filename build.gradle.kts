2215d2f0145c75c18fa62f8b4abe0d91e65f51bf
Aryan Iranzamini <aryan.iranzamini@nav.no>
2020-12-09 17:52:51 +0100

    Deploy branch to test

--- build.gradle.kts
+++ build.gradle.kts
@@ -92,7 +92,7 @@ allprojects {
 
     tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
         kotlinOptions {
-            jvmTarget = "15"
+            jvmTarget = "1.8"
             freeCompilerArgs += "-Xopt-in=kotlin.RequiresOptIn"
             allWarningsAsErrors = true
         }
