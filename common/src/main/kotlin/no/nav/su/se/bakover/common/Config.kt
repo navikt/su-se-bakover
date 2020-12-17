00ed29bafcb98e1f5e8c28a8e0c49e71329edf4a
Richard Martinsen <github@martinsen.org>
2020-12-17 14:17:04 +0100

    Byttet til ny clientId

--- common/src/main/kotlin/no/nav/su/se/bakover/common/Config.kt
+++ common/src/main/kotlin/no/nav/su/se/bakover/common/Config.kt
@@ -18,7 +18,7 @@ object Config {
 
     val azureClientSecret = env["AZURE_APP_CLIENT_SECRET"] ?: "Denne håndteres av nais. Må ligge i .env lokalt."
     val azureWellKnownUrl = env["AZURE_APP_WELLKNOWN_URL"] ?: "https://login.microsoftonline.com/966ac572-f5b7-4bbe-aa88-c76419c0f851/v2.0/.well-known/openid-configuration"
-    val azureClientId = env["AZURE_APP_CLIENT_ID"] ?: "24ea4acb-547e-45de-a6d3-474bd8bed46e"
+    val azureClientId = env["AZURE_APP_CLIENT_ID"] ?: "26a62d18-70ce-48a6-9f4d-664607bd5188"
     val azureBackendCallbackUrl = env["BACKEND_CALLBACK_URL"] ?: "http://localhost:8080/callback"
     val azureGroupAttestant = env["AZURE_GROUP_ATTESTANT"] ?: "d75164fa-39e6-4149-956e-8404bc9080b6"
     val azureGroupSaksbehandler = env["AZURE_GROUP_SAKSBEHANDLER"] ?: "0ba009c4-d148-4a51-b501-4b1cf906889d"
