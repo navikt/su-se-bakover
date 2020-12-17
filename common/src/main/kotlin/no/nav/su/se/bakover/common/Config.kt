b3fb8eb48258df57ef46f0a52b7d066e48641032
Richard Martinsen <github@martinsen.org>
2020-12-17 12:54:01 +0100

    Mounter ikke azure secrets fra vault. Mounter azure secrets fra azure. Byttet tenant i dev til trygdeetaten igjen

--- common/src/main/kotlin/no/nav/su/se/bakover/common/Config.kt
+++ common/src/main/kotlin/no/nav/su/se/bakover/common/Config.kt
@@ -16,9 +16,9 @@ object Config {
     val databaseName = env["DATABASE_NAME"] ?: "supstonad-db-local"
     val jdbcUrl = env["DATABASE_JDBC_URL"] ?: "jdbc:postgresql://localhost:5432/supstonad-db-local"
 
-    val azureClientSecret = env["AZURE_CLIENT_SECRET"] ?: "Denne håndteres av nais. Må ligge i .env lokalt."
-    val azureWellKnownUrl = env["AZURE_WELLKNOWN_URL"] ?: "https://login.microsoftonline.com/966ac572-f5b7-4bbe-aa88-c76419c0f851/v2.0/.well-known/openid-configuration"
-    val azureClientId = env["AZURE_CLIENT_ID"] ?: "24ea4acb-547e-45de-a6d3-474bd8bed46e"
+    val azureClientSecret = env["AZURE_APP_CLIENT_SECRET"] ?: "Denne håndteres av nais. Må ligge i .env lokalt."
+    val azureWellKnownUrl = env["AZURE_APP_WELLKNOWN_URL"] ?: "https://login.microsoftonline.com/966ac572-f5b7-4bbe-aa88-c76419c0f851/v2.0/.well-known/openid-configuration"
+    val azureClientId = env["AZURE_APP_CLIENT_ID"] ?: "24ea4acb-547e-45de-a6d3-474bd8bed46e"
     val azureBackendCallbackUrl = env["BACKEND_CALLBACK_URL"] ?: "http://localhost:8080/callback"
     val azureGroupAttestant = env["AZURE_GROUP_ATTESTANT"] ?: "d75164fa-39e6-4149-956e-8404bc9080b6"
     val azureGroupSaksbehandler = env["AZURE_GROUP_SAKSBEHANDLER"] ?: "0ba009c4-d148-4a51-b501-4b1cf906889d"
