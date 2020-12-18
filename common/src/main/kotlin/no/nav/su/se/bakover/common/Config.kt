9d201335218dedc1e0385b6dd4afbb879ad4aaee
John Andre Hestad <john.andre.hestad@nav.no>
2020-12-18 13:02:10 +0100

    Use MQ_REPLY_TO as mqReplyTo in UtbetalingConfig

--- common/src/main/kotlin/no/nav/su/se/bakover/common/Config.kt
+++ common/src/main/kotlin/no/nav/su/se/bakover/common/Config.kt
@@ -179,7 +179,7 @@ data class ApplicationConfig(
             companion object {
                 fun createFromEnvironmentVariables() = UtbetalingConfig(
                     mqSendQueue = getEnvironmentVariableOrThrow("MQ_SEND_QUEUE_UTBETALING"),
-                    mqReplyTo = getEnvironmentVariableOrThrow("MQ_SEND_QUEUE_UTBETALING"),
+                    mqReplyTo = getEnvironmentVariableOrThrow("MQ_REPLY_TO"),
                 )
             }
         }
