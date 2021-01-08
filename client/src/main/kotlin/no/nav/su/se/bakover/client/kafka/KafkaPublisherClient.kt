2ff84729029743c2a0622c29510e58b2cf7bc4a0
Jacob Meidell <jacob.meidell@nav.no>
2021-01-08 15:07:40 +0100

    Refinement of error handling in kafkapublisher.
    Co-authored-by: Aryan Iranzamini <aryaniranzamini@gmail.com>

--- client/src/main/kotlin/no/nav/su/se/bakover/client/kafka/KafkaPublisherClient.kt
+++ client/src/main/kotlin/no/nav/su/se/bakover/client/kafka/KafkaPublisherClient.kt
@@ -2,38 +2,36 @@ package no.nav.su.se.bakover.client.kafka
 
 import no.nav.su.se.bakover.common.ApplicationConfig
 import org.apache.kafka.clients.producer.KafkaProducer
+import org.apache.kafka.clients.producer.Producer
 import org.apache.kafka.clients.producer.ProducerRecord
 import org.apache.kafka.common.errors.AuthorizationException
 import org.slf4j.LoggerFactory
 import java.time.Duration
 
 internal class KafkaPublisherClient(
-    private val kafkaConfig: ApplicationConfig.KafkaConfig
+    private val kafkaConfig: ApplicationConfig.KafkaConfig,
+    private val initProducer: () -> Producer<String, String> = { KafkaProducer(kafkaConfig.producerConfig) }
 ) : KafkaPublisher {
     private val log = LoggerFactory.getLogger(this::class.java)
-    private var producer = KafkaProducer<String, String>(kafkaConfig.producerConfig)
+    private var producer: Producer<String, String> = initProducer()
+    private val failed: MutableSet<ProducerRecord<String, String>> = mutableSetOf()
 
     override fun publiser(topic: String, melding: String) {
-        publiser(topic, melding, 0)
+        return publiser(topic, melding, 0)
     }
 
     private fun publiser(topic: String, melding: String, retries: Int) {
         try {
-            producer.send(ProducerRecord(topic, melding)) { recordMetadata, exception ->
+            val record = ProducerRecord<String, String>(topic, melding)
+            producer.send(record) { recordMetadata, exception ->
                 when (exception) {
                     null -> log.info("Publiserte meldig til $topic med offset: ${recordMetadata.offset()}")
                     is AuthorizationException -> {
                         log.warn("Autorisasjonsfeil ved publisering av melding til $topic, rekonfigurerer producer før retry.")
                         producer.close(Duration.ZERO)
-                        producer = KafkaProducer<String, String>(kafkaConfig.producerConfig).also {
-                            if (retries < 3) {
-                                log.info("Resender melding til $topic med ny producer, retry antall: $retries")
-                                Thread.sleep(500)
-                                publiser(topic, melding, retries + 1)
-                            } else {
-                                log.info("Antall retry overskredet, stopper.")
-                            }
-                        }
+                        producer = initProducer()
+                        // TODO solution for the record that failed
+                        failed.add(record)
                     }
                     else -> log.error("Ukejent feil ved publisering av melding til topic:$topic", exception)
                 }
