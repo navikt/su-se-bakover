0f2d2730e9c9e42dfc9d70c79deb0baa4887a29d
Jacob Meidell <jacob.meidell@nav.no>
2021-01-08 10:00:19 +0100

    Test creating new producer when authorization exception occurs.

--- client/src/main/kotlin/no/nav/su/se/bakover/client/kafka/KafkaPublisherClient.kt
+++ client/src/main/kotlin/no/nav/su/se/bakover/client/kafka/KafkaPublisherClient.kt
@@ -1,20 +1,41 @@
 package no.nav.su.se.bakover.client.kafka
 
+import no.nav.su.se.bakover.common.ApplicationConfig
 import org.apache.kafka.clients.producer.KafkaProducer
 import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.common.errors.AuthorizationException
 import org.slf4j.LoggerFactory
 
 internal class KafkaPublisherClient(
-    private val producer: KafkaProducer<String, String>
+    private val kafkaConfig: ApplicationConfig.KafkaConfig
 ) : KafkaPublisher {
     private val log = LoggerFactory.getLogger(this::class.java)
+    private var producer = KafkaProducer<String, String>(kafkaConfig.producerConfig)
 
     override fun publiser(topic: String, melding: String) {
+        publiser(topic, melding, 0)
+    }
+
+    private fun publiser(topic: String, melding: String, retries: Int) {
         try {
             producer.send(ProducerRecord(topic, melding)) { recordMetadata, exception ->
-                if (exception != null)
-                    log.error("Feil ved publisering av melding til topic:$topic", exception)
-                else log.info("Publiserte meldig til $topic med offset: ${recordMetadata.offset()}")
+                when (exception) {
+                    null -> log.info("Publiserte meldig til $topic med offset: ${recordMetadata.offset()}")
+                    is AuthorizationException -> {
+                        log.warn("Autorisasjonsfeil ved publisering av melding til $topic, rekonfigurerer producer før retry.")
+                        producer.close()
+                        producer = KafkaProducer<String, String>(kafkaConfig.producerConfig).also {
+                            if (retries < 3) {
+                                log.info("Resender melding til $topic med ny producer, retry antall: $retries")
+                                Thread.sleep(500)
+                                publiser(topic, melding, retries + 1)
+                            } else {
+                                log.info("Antall retry overskredet, stopper.")
+                            }
+                        }
+                    }
+                    else -> log.error("Ukejent feil ved publisering av melding til topic:$topic", exception)
+                }
             }
         } catch (exception: Throwable) {
             log.error("Fanget exception ved publisering av melding til topic:$topic", exception)
