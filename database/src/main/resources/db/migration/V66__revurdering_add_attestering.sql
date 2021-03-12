40dd4e6afb84348dc1b56e58a49be3a2b03f22a1
Aryan Iranzamini <aryan.iranzamini@nav.no>
2021-03-12 14:54:24 +0100

    Fix migration script for attestering in revurdering
    Co-authored-by: Ramzi Abu Qassim <ramzi.abu.qassim@nav.no>
    Co-authored-by: Christian Mosveen <christian.mosveen@nav.no>

--- database/src/main/resources/db/migration/V66__revurdering_add_attestering.sql
+++ database/src/main/resources/db/migration/V66__revurdering_add_attestering.sql
@@ -1,7 +1,7 @@
 alter table revurdering add column if not exists attestering jsonb default null;
 
-update revurdering set attestering =
-    json_build_object('type', 'Iverksatt', 'attestant', r.attestant) from revurdering r
-where r.attestant is not null;
+update revurdering
+set attestering = json_build_object('type', 'Iverksatt', 'attestant', attestant)
+where attestant is not null;
 
 alter table revurdering drop column attestant;
