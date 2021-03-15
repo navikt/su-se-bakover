178a12ccb256da1a3c59c77670f09304bcd4dc0c
Jacob Meidell <jacob.meidell@nav.no>
2021-03-15 14:42:13 +0100

    Add more utility functions to periode.

--- common/src/main/kotlin/no/nav/su/se/bakover/common/periode/Periode.kt
+++ common/src/main/kotlin/no/nav/su/se/bakover/common/periode/Periode.kt
@@ -27,8 +27,7 @@ data class Periode private constructor(
     }
 
     infix fun inneholder(other: Periode): Boolean =
-        (fraOgMed.isEqual(other.fraOgMed) || fraOgMed.isBefore(other.fraOgMed)) &&
-            (tilOgMed.isEqual(other.tilOgMed) || tilOgMed.isAfter(other.tilOgMed))
+        starterSamtidigEllerTidligere(other) && slutterSamtidigEllerSenere(other)
 
     infix fun tilstøter(other: Periode): Boolean {
         val sluttStart = Period.between(tilOgMed, other.fraOgMed)
@@ -38,15 +37,44 @@ data class Periode private constructor(
         return sluttStart == plussEnDag || sluttStart == minusEnDag || startSlutt == plussEnDag || startSlutt == minusEnDag
     }
 
+    infix fun overlapper(other: Periode): Boolean =
+        starterSamtidigEllerTidligere(other) && slutterInni(other) ||
+            other.starterSamtidigEllerTidligere(this) && other.slutterInni(this) ||
+            starterSamtidigEllerTidligere(other) && slutterEtter(other) ||
+            other.starterSamtidigEllerTidligere(this) && other.slutterEtter(this)
+
+    infix fun starterSamtidigEllerTidligere(other: Periode) = starterSamtidig(other) || starterTidligere(other)
+    infix fun starterSamtidigEllerSenere(other: Periode) = starterSamtidig(other) || starterEtter(other)
+    infix fun starterSamtidig(other: Periode) = fraOgMed.isEqual(other.fraOgMed)
+    infix fun starterTidligere(other: Periode) = fraOgMed.isBefore(other.fraOgMed)
+    infix fun starterEtter(other: Periode) = fraOgMed.isAfter(other.fraOgMed)
+
+    infix fun slutterSamtidigEllerTidligere(other: Periode) = slutterSamtidig(other) || slutterTidligere(other)
+    infix fun slutterSamtidigEllerSenere(other: Periode) = slutterSamtidig(other) || slutterEtter(other)
+    infix fun slutterSamtidig(other: Periode) = tilOgMed.isEqual(other.tilOgMed)
+    infix fun slutterTidligere(other: Periode) = tilOgMed.isBefore(other.tilOgMed)
+    infix fun slutterEtter(other: Periode) = tilOgMed.isAfter(other.tilOgMed)
+    infix fun slutterInni(other: Periode) = (starterSamtidigEllerTidligere(other) || starterEtter(other)) &&
+        !før(other) && slutterSamtidigEllerTidligere(other)
+
+    infix fun før(other: Periode) = tilOgMed.isBefore(other.fraOgMed)
+    infix fun etter(other: Periode) = fraOgMed.isAfter(other.tilOgMed)
+
     companion object {
         fun create(fraOgMed: LocalDate, tilOgMed: LocalDate): Periode {
             return tryCreate(fraOgMed, tilOgMed).getOrHandle { throw IllegalArgumentException(it.toString()) }
         }
 
         fun tryCreate(fraOgMed: LocalDate, tilOgMed: LocalDate): Either<UgyldigPeriode, Periode> {
-            if (fraOgMed.dayOfMonth != 1) { return UgyldigPeriode.FraOgMedDatoMåVæreFørsteDagIMåneden.left() }
-            if (tilOgMed.dayOfMonth != tilOgMed.lengthOfMonth()) { return UgyldigPeriode.TilOgMedDatoMåVæreSisteDagIMåneden.left() }
-            if (!fraOgMed.isBefore(tilOgMed)) { return UgyldigPeriode.FraOgMedDatoMåVæreFørTilOgMedDato.left() }
+            if (fraOgMed.dayOfMonth != 1) {
+                return UgyldigPeriode.FraOgMedDatoMåVæreFørsteDagIMåneden.left()
+            }
+            if (tilOgMed.dayOfMonth != tilOgMed.lengthOfMonth()) {
+                return UgyldigPeriode.TilOgMedDatoMåVæreSisteDagIMåneden.left()
+            }
+            if (!fraOgMed.isBefore(tilOgMed)) {
+                return UgyldigPeriode.FraOgMedDatoMåVæreFørTilOgMedDato.left()
+            }
 
             return Periode(fraOgMed, tilOgMed).right()
         }
@@ -56,9 +84,11 @@ data class Periode private constructor(
         object FraOgMedDatoMåVæreFørsteDagIMåneden : UgyldigPeriode() {
             override fun toString(): String = this.javaClass.simpleName
         }
+
         object TilOgMedDatoMåVæreSisteDagIMåneden : UgyldigPeriode() {
             override fun toString(): String = this.javaClass.simpleName
         }
+
         object FraOgMedDatoMåVæreFørTilOgMedDato : UgyldigPeriode() {
             override fun toString(): String = this.javaClass.simpleName
         }
