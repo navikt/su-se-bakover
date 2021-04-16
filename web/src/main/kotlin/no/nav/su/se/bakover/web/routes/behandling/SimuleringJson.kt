c9612a106a0aa027bce07700a418214d3418998b
Ingar Almklov <ingara@gmail.com>
2021-04-16 10:49:39 +0200

    Use TolketSimulering for SimuleringJson
    Co-authored-by: Jacob Meidell <jacob.meidell@nav.no>

--- web/src/main/kotlin/no/nav/su/se/bakover/web/routes/behandling/SimuleringJson.kt
+++ web/src/main/kotlin/no/nav/su/se/bakover/web/routes/behandling/SimuleringJson.kt
@@ -1,37 +1,11 @@
 package no.nav.su.se.bakover.web.routes.behandling
 
 import no.nav.su.se.bakover.domain.oppdrag.simulering.Simulering
-import no.nav.su.se.bakover.domain.oppdrag.simulering.SimulertPeriode
-import no.nav.su.se.bakover.web.routes.behandling.SimuleringJson.SimulertPeriodeJson.Companion.toJson
+import no.nav.su.se.bakover.domain.oppdrag.simulering.TolketDetalj
+import no.nav.su.se.bakover.domain.oppdrag.simulering.TolketPeriode
+import no.nav.su.se.bakover.domain.oppdrag.simulering.TolketSimulering
+import no.nav.su.se.bakover.domain.oppdrag.simulering.TolketUtbetaling
 import java.time.LocalDate
-
-data class SimuleringJson(
-    val totalBruttoYtelse: Int,
-    val perioder: List<SimulertPeriodeJson>
-) {
-    companion object {
-        fun Simulering.toJson(): SimuleringJson = SimuleringJson(
-            totalBruttoYtelse = bruttoYtelse(),
-            perioder = periodeList.toJson()
-        )
-    }
-
-    data class SimulertPeriodeJson(
-        val fraOgMed: LocalDate,
-        val tilOgMed: LocalDate,
-        val bruttoYtelse: Int
-    ) {
-        companion object {
-            fun List<SimulertPeriode>.toJson() = this.map {
-                SimulertPeriodeJson(
-                    fraOgMed = it.fraOgMed,
-                    tilOgMed = it.tilOgMed,
-                    bruttoYtelse = it.bruttoYtelse()
-                )
-            }
-        }
-    }
-}
 
 data class UtbetalingJson(
     val id: String,
@@ -40,3 +14,68 @@ data class UtbetalingJson(
     val beløp: Int,
     val type: String
 )
+
+enum class SimulertUtbetalingstype {
+    ETTERBETALING,
+    FEILUTBETALING,
+    ORDINÆR
+}
+
+data class SimuleringJson(
+    val perioder: List<SimulertPeriodeJson>,
+    val totalBruttoYtelse: Int,
+) {
+    data class SimulertPeriodeJson(
+        val fraOgMed: LocalDate,
+        val tilOgMed: LocalDate,
+        val type: SimulertUtbetalingstype,
+        val bruttoYtelse: Int,
+    )
+
+    companion object {
+        fun Simulering.toJson() = TolketSimulering(this).let {
+            SimuleringJson(
+                perioder = it.simulertePerioder.map { sp -> sp.toJson() },
+                totalBruttoYtelse = it.simulertePerioder
+                    .sumBy { sp ->
+                        sp.utbetalinger
+                            .sumBy { u -> u.bruttobeløp() }
+                    },
+            )
+        }
+    }
+}
+
+fun TolketPeriode.toJson(): SimuleringJson.SimulertPeriodeJson {
+    val utbetaling = utbetalinger.singleOrNull() ?: throw MerEnnEnUtbetalingIMinstEnAvPeriodene
+    return when (utbetaling) {
+        is TolketUtbetaling.Etterbetaling -> SimuleringJson.SimulertPeriodeJson(
+            fraOgMed = fraOgMed,
+            tilOgMed = tilOgMed,
+            type = SimulertUtbetalingstype.ETTERBETALING,
+            bruttoYtelse = utbetaling.tolketDetalj
+                .filterIsInstance<TolketDetalj.Etterbetaling>()
+                .sumBy { it.beløp },
+        )
+        is TolketUtbetaling.Feilutbetaling -> SimuleringJson.SimulertPeriodeJson(
+            fraOgMed = fraOgMed,
+            tilOgMed = tilOgMed,
+            type = SimulertUtbetalingstype.FEILUTBETALING,
+            bruttoYtelse = utbetaling.tolketDetalj
+                .filterIsInstance<TolketDetalj.Feilutbetaling>()
+                .sumBy { it.beløp }
+                .times(-1),
+        )
+        is TolketUtbetaling.Ordinær -> SimuleringJson.SimulertPeriodeJson(
+            fraOgMed = fraOgMed,
+            tilOgMed = tilOgMed,
+            type = SimulertUtbetalingstype.ORDINÆR,
+            bruttoYtelse = utbetaling.tolketDetalj
+                .filterIsInstance<TolketDetalj.Ordinær>()
+                .sumBy { it.beløp },
+        )
+    }
+}
+
+object MerEnnEnUtbetalingIMinstEnAvPeriodene :
+    IllegalStateException("En periode i simuleringen inneholdt mer enn èn utbetaling. Dette støttes ikke p.t.")
