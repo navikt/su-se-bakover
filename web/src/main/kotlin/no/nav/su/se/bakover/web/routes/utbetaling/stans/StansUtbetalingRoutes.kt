fbd47dec53eb9048d642db595c95685333052d01
Jacob Meidell <jacob.meidell@nav.no>
2021-07-12 08:39:30 +0200

    Ta i bruk responskode for feilsituasjoner.

--- web/src/main/kotlin/no/nav/su/se/bakover/web/routes/utbetaling/stans/StansUtbetalingRoutes.kt
+++ web/src/main/kotlin/no/nav/su/se/bakover/web/routes/utbetaling/stans/StansUtbetalingRoutes.kt
@@ -3,7 +3,7 @@ package no.nav.su.se.bakover.web.routes.utbetaling.stans
 import io.ktor.application.call
 import io.ktor.http.HttpStatusCode.Companion.InternalServerError
 import io.ktor.http.HttpStatusCode.Companion.NotFound
-import io.ktor.response.respond
+import io.ktor.http.HttpStatusCode.Companion.OK
 import io.ktor.routing.Route
 import io.ktor.routing.post
 import no.nav.su.se.bakover.common.idag
@@ -13,13 +13,15 @@ import no.nav.su.se.bakover.domain.NavIdentBruker.Saksbehandler
 import no.nav.su.se.bakover.service.utbetaling.KunneIkkeStanseUtbetalinger
 import no.nav.su.se.bakover.service.utbetaling.UtbetalingService
 import no.nav.su.se.bakover.web.AuditLogEvent
+import no.nav.su.se.bakover.web.Resultat
 import no.nav.su.se.bakover.web.audit
+import no.nav.su.se.bakover.web.errorJson
 import no.nav.su.se.bakover.web.features.authorize
 import no.nav.su.se.bakover.web.features.suUserContext
-import no.nav.su.se.bakover.web.message
 import no.nav.su.se.bakover.web.routes.sak.SakJson.Companion.toJson
 import no.nav.su.se.bakover.web.routes.sak.sakPath
 import no.nav.su.se.bakover.web.sikkerlogg
+import no.nav.su.se.bakover.web.svar
 import no.nav.su.se.bakover.web.withSakId
 import java.time.temporal.TemporalAdjusters.firstDayOfNextMonth
 
@@ -29,29 +31,46 @@ internal fun Route.stansutbetalingRoutes(
     authorize(Brukerrolle.Saksbehandler) {
         post("$sakPath/{sakId}/utbetalinger/stans") {
             call.withSakId { sakId ->
-                utbetalingService.stansUtbetalinger(
-                    sakId = sakId,
-                    saksbehandler = call.suUserContext.navIdent.let { Saksbehandler(it) },
-                    stansDato = idag().with(firstDayOfNextMonth()),
-                ).fold(
-                    {
-                        when (it) {
-                            KunneIkkeStanseUtbetalinger.FantIkkeSak ->
-                                call.respond(NotFound, "Fant ikke sak")
-                            KunneIkkeStanseUtbetalinger.SimuleringAvStansFeilet ->
-                                call.respond(InternalServerError, "Simulering av stans feilet")
-                            KunneIkkeStanseUtbetalinger.SendingAvUtbetalingTilOppdragFeilet ->
-                                call.respond(InternalServerError, "Oversendelse til oppdrag feilet")
-                            KunneIkkeStanseUtbetalinger.KontrollAvSimuleringFeilet ->
-                                call.respond(InternalServerError, "Kontroll av simulering feilet")
-                        }
-                        InternalServerError.message("Kunne ikke stanse utbetalinger for sak med id $sakId")
-                    },
-                    {
-                        call.audit(it.fnr, AuditLogEvent.Action.UPDATE, null)
-                        call.sikkerlogg("Stanser utbetaling på sak $sakId")
-                        call.respond(serialize(it.toJson()))
-                    },
+                call.svar(
+                    utbetalingService.stansUtbetalinger(
+                        sakId = sakId,
+                        saksbehandler = call.suUserContext.navIdent.let { Saksbehandler(it) },
+                        stansDato = idag().with(firstDayOfNextMonth()),
+                    ).fold(
+                        {
+                            when (it) {
+                                KunneIkkeStanseUtbetalinger.FantIkkeSak -> {
+                                    NotFound.errorJson(
+                                        "Fant ikke sak",
+                                        "fant_ikke_sak",
+                                    )
+                                }
+                                KunneIkkeStanseUtbetalinger.SimuleringAvStansFeilet -> {
+                                    InternalServerError.errorJson(
+                                        "Simulering av stans feilet",
+                                        "simulering_av_stans_feilet",
+                                    )
+                                }
+                                KunneIkkeStanseUtbetalinger.SendingAvUtbetalingTilOppdragFeilet -> {
+                                    InternalServerError.errorJson(
+                                        "Oversendelse til oppdrag feilet",
+                                        "oversendelse_til_oppdrag_feilet",
+                                    )
+                                }
+                                KunneIkkeStanseUtbetalinger.KontrollAvSimuleringFeilet -> {
+                                    InternalServerError.errorJson(
+                                        "Kontroll av simulering feilet",
+                                        "kontroll_av_simulering_feilet",
+                                    )
+                                }
+                            }
+                        },
+                        {
+                            call.audit(it.fnr, AuditLogEvent.Action.UPDATE, null)
+                            call.sikkerlogg("Stanser utbetaling på sak $sakId")
+                            Resultat.json(OK, serialize(it.toJson()))
+                        },
+                    ),
                 )
             }
         }
