7075e50fb4009f2e83910e250dca998b47e94206
Richard Martinsen <github@martinsen.org>
2021-08-25 10:45:31 +0200

    legg til BeregnetFradragEPS og UnderMinstenivå unntakene i migrering

--- database/src/main/resources/db/migration/V98__fradrag_i_beregning_til_grunnlag_fra_behandling.sql
+++ database/src/main/resources/db/migration/V98__fradrag_i_beregning_til_grunnlag_fra_behandling.sql
@@ -1,17 +1,19 @@
-with fradrag_fra_revurdering (behandlingId, opprettet, fradrag) as (
+with fradrag_fra_behandling (behandlingId, opprettet, fradrag) as (
     select id, opprettet, jsonb_array_elements(beregning -> 'fradrag')
     from behandling
 ),
-     mapped_values as (select behandlingId as                                      behandlingId,
-                              opprettet    as                                      opprettet,
-                              (fradrag -> 'periode' ->> 'fraOgMed')::date          fraogmed,
-                              (fradrag -> 'periode' ->> 'tilOgMed')::date          tilogmed,
-                              fradrag -> 'tilhører'                                tilhører,
-                              fradrag -> 'fradragstype'                            fradragstype,
-                              round((fradrag ->> 'månedsbeløp')::double precision) månedsbeløp,
-                              fradrag -> 'utenlandskinntekt'                       utenlandskinntekt
-                       from fradrag_fra_revurdering
-                       where fradrag ->> 'fradragstype' != 'ForventetInntekt')
+     mapped_values as (select behandlingId as                                            behandlingId,
+                              opprettet    as                                            opprettet,
+                              (fradrag -> 'periode' ->> 'fraOgMed')::date                fraogmed,
+                              (fradrag -> 'periode' ->> 'tilOgMed')::date                tilogmed,
+                              fradrag -> 'tilhører'                                      tilhører,
+                              fradrag -> 'fradragstype'                                  fradragstype,
+                              round((fradrag ->> 'månedsbeløp')::double precision)       månedsbeløp,
+                              COALESCE((fradrag ->> 'utenlandskInntekt')::jsonb, 'null') utenlandskinntekt
+                       from fradrag_fra_behandling
+                       where fradrag ->> 'fradragstype' not in
+                             ('ForventetInntekt', 'BeregnetFradragEPS', 'UnderMinstenivå')
+     )
 
 insert
 into grunnlag_fradrag(id, opprettet, behandlingid, fraogmed, tilogmed, fradragstype, månedsbeløp, utenlandskinntekt,
