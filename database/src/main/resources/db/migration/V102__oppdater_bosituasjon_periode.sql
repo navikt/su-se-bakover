c81c540848cb0ea0f90e8924753a92c81a6a65ca
Richard Martinsen <github@martinsen.org>
2021-09-14 09:10:01 +0200

    Update V102__oppdater_bosituasjon_periode.sql

--- database/src/main/resources/db/migration/V102__oppdater_bosituasjon_periode.sql
+++ database/src/main/resources/db/migration/V102__oppdater_bosituasjon_periode.sql
@@ -1,7 +1,8 @@
 update grunnlag_bosituasjon g
 set fraOgMed = (stønadsperiode -> 'periode' ->> 'fraOgMed')::date,
-    tilogmed = (stønadsperiode -> 'periode' ->> 'tilOgMed')::date
+    tilOgmed = (stønadsperiode -> 'periode' ->> 'tilOgMed')::date
 from behandling b
 where b.id = g.behandlingid
   and (stønadsperiode -> 'periode' ->> 'fraOgMed')::date is not null
   and (stønadsperiode -> 'periode' ->> 'tilOgMed')::date is not null;
+  
