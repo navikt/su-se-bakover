438269bce3489a43a7d9823ee3583f70e6a1da88
Aryan Iranzamini <aryan.iranzamini@nav.no>
2021-12-06 11:25:35 +0100

    Utkommenter ./resetdb.sh i resetdb_and_create_søknad

--- service/src/test/kotlin/no/nav/su/se/bakover/service/klage/VurderKlageTest.kt
+++ service/src/test/kotlin/no/nav/su/se/bakover/service/klage/VurderKlageTest.kt
@@ -3,7 +3,6 @@ package no.nav.su.se.bakover.service.klage
 import arrow.core.left
 import io.kotest.matchers.shouldBe
 import no.nav.su.se.bakover.common.desember
-import no.nav.su.se.bakover.common.persistence.SessionFactory
 import no.nav.su.se.bakover.domain.NavIdentBruker
 import no.nav.su.se.bakover.domain.behandling.Attesteringshistorikk
 import no.nav.su.se.bakover.domain.klage.Hjemmel
