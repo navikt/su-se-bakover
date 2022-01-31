5953d4a20216a273b918456c7f479c1d7ef6735c
Richard Martinsen <github@martinsen.org>
2022-01-31 09:58:46 +0100

    Endre datoformat for opphørsdato i brev, til å vise måned med tekst

--- common/src/main/kotlin/no/nav/su/se/bakover/common/Utils.kt
+++ common/src/main/kotlin/no/nav/su/se/bakover/common/Utils.kt
@@ -6,6 +6,7 @@ import java.time.LocalDate
 import java.time.Month
 import java.time.ZoneId
 import java.time.format.DateTimeFormatter
+import java.util.Locale
 
 val zoneIdOslo: ZoneId = ZoneId.of("Europe/Oslo")
 
@@ -31,6 +32,7 @@ fun LocalDate.endOfMonth(): LocalDate = this.withDayOfMonth(this.lengthOfMonth()
 fun LocalDate.between(periode: Periode) = this.between(periode.fraOgMed, periode.tilOgMed)
 fun LocalDate.between(fraOgMed: LocalDate, tilOgMed: LocalDate) =
     (this == fraOgMed || this == tilOgMed) || this.isAfter(fraOgMed) && this.isBefore(tilOgMed)
+
 fun LocalDate.erFørsteDagIMåned() = dayOfMonth == 1
 fun LocalDate.erSisteDagIMåned() = dayOfMonth == lengthOfMonth()
 fun LocalDate.erMindreEnnEnMånedSenere(localDate: LocalDate) = this.isBefore(localDate.plusMonths(1))
@@ -39,3 +41,4 @@ fun Tidspunkt.between(fraOgMed: Tidspunkt, tilOgMed: Tidspunkt) =
     (this == fraOgMed || this == tilOgMed) || this.instant.isAfter(fraOgMed.instant) && this.instant.isBefore(tilOgMed.instant)
 
 fun LocalDate.ddMMyyyy(): String = this.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"))
+fun LocalDate.toBrevformat(): String = this.format(DateTimeFormatter.ofPattern("d. LLLL yyyy", Locale("nb", "NO")))
--- domain/src/main/kotlin/no/nav/su/se/bakover/domain/brev/LagBrevRequest.kt
+++ domain/src/main/kotlin/no/nav/su/se/bakover/domain/brev/LagBrevRequest.kt
@@ -6,6 +6,7 @@ import arrow.core.left
 import arrow.core.right
 import no.nav.su.se.bakover.common.Tidspunkt
 import no.nav.su.se.bakover.common.ddMMyyyy
+import no.nav.su.se.bakover.common.toBrevformat
 import no.nav.su.se.bakover.domain.Grunnbeløp
 import no.nav.su.se.bakover.domain.Person
 import no.nav.su.se.bakover.domain.Saksnummer
@@ -154,7 +155,7 @@ interface LagBrevRequest {
             opphørsgrunner = opphørsgrunner,
             avslagsparagrafer = opphørsgrunner.getDistinkteParagrafer(),
             forventetInntektStørreEnn0 = forventetInntektStørreEnn0,
-            opphørsdato = opphørsdato.ddMMyyyy(),
+            opphørsdato = opphørsdato.toBrevformat(),
             avkortingsBeløp = avkortingsBeløp,
         )
 
