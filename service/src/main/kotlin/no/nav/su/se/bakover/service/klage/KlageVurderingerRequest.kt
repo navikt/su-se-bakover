810afc2264f070d0762e62d85d9d6be445bbab6e
John Andre Hestad <john.andre.hestad@nav.no>
2022-01-31 10:34:07 +0100

    Legg til manglende klagehjemler i vurderingsrequesten

--- service/src/main/kotlin/no/nav/su/se/bakover/service/klage/KlageVurderingerRequest.kt
+++ service/src/main/kotlin/no/nav/su/se/bakover/service/klage/KlageVurderingerRequest.kt
@@ -77,9 +77,12 @@ data class KlageVurderingerRequest(
                     "SU_PARAGRAF_8" -> Hjemmel.SU_PARAGRAF_8
                     "SU_PARAGRAF_9" -> Hjemmel.SU_PARAGRAF_9
                     "SU_PARAGRAF_10" -> Hjemmel.SU_PARAGRAF_10
+                    "SU_PARAGRAF_11" -> Hjemmel.SU_PARAGRAF_11
                     "SU_PARAGRAF_12" -> Hjemmel.SU_PARAGRAF_12
                     "SU_PARAGRAF_13" -> Hjemmel.SU_PARAGRAF_13
+                    "SU_PARAGRAF_17" -> Hjemmel.SU_PARAGRAF_17
                     "SU_PARAGRAF_18" -> Hjemmel.SU_PARAGRAF_18
+                    "SU_PARAGRAF_21" -> Hjemmel.SU_PARAGRAF_21
                     else -> return KunneIkkeVurdereKlage.UgyldigOpprettholdelseshjemler.left()
                 }
             }.let {
