4e128b17a691b50adfe9cacb0f379525a49c096c
ramziabuqassim <ramzi.abu.qassim@nav.no>
2022-03-28 09:55:25 +0200

    Legg til type for Bostedsadresse og kontaktadresse & oppdater doc til å linke til master branchen

--- web/src/main/kotlin/no/nav/su/se/bakover/web/services/personhendelser/PersonhendelseMapper.kt
+++ web/src/main/kotlin/no/nav/su/se/bakover/web/services/personhendelser/PersonhendelseMapper.kt
@@ -17,8 +17,10 @@ internal object PersonhendelseMapper {
     private enum class Opplysningstype(val value: String) {
         DØDSFALL("DOEDSFALL_V1"),
         UTFLYTTING_FRA_NORGE("UTFLYTTING_FRA_NORGE"),
-        SIVILSTAND("SIVILSTAND_V1");
-        // https://github.com/navikt/pdl/blob/b483f1fee4b73fa79b4ccff6e3e953f33c2bd2dc/libs/contract-pdl-avro/src/main/java/no/nav/person/pdl/leesah/Opplysningstype.java
+        SIVILSTAND("SIVILSTAND_V1"),
+        BOSTEDSADRESSE("BOSTEDSADRESSE_V1"),
+        KONTAKTADRESSE("KONTAKTADRESSE_V1");
+        // https://github.com/navikt/pdl/blob/master/libs/contract-pdl-avro/src/main/java/no/nav/person/pdl/leesah/Opplysningstype.java
     }
 
     internal fun map(
