c0a6c5a8d16ab2e09079a8555ae39a479f25e429
John Andre Hestad <john.andre.hestad@nav.no>
2022-04-04 13:34:30 +0200

    Bytt tilbakekrevingshjemmel til SUL_13

--- client/src/main/kotlin/no/nav/su/se/bakover/client/oppdrag/tilbakekreving/TilbakekrevingSoapRequestMapper.kt
+++ client/src/main/kotlin/no/nav/su/se/bakover/client/oppdrag/tilbakekreving/TilbakekrevingSoapRequestMapper.kt
@@ -15,10 +15,11 @@ internal enum class AksjonsKode(val nummer: String) {
 
 /**
  * Dersom man bruker hjemmel 'ANNET' sender tilbakekrevingskomponenten posisjon 118 som blank til NAVI/Predator og den vil bli behandlet som foreldet.
- * Dersom man bruker hjemmel '22-15' vil tilbakekrevingskomponenten sende T på posisjon 118 istedet og vi vil få forventet oppførsel.
+ * Dersom man bruker hjemmel 'SUL_13' vil tilbakekrevingskomponenten sende T på posisjon 118 istedet og vi vil få forventet oppførsel.
+ * Vi har også bestilt SUL_13-1, SUL_13-2, SUL_13-3 og SUL_13-4 som vi ikke har tatt i bruk enda.
  */
 internal enum class TilbakekrevingsHjemmel(val value: String) {
-    T("22-15");
+    T("SUL_13");
 
     override fun toString() = value
 }
