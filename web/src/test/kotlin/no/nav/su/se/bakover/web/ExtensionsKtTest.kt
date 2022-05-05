a4d4dc524621965aa5d6cac0d9beaf98b645e669
John Andre Hestad <john.andre.hestad@nav.no>
2022-05-05 14:04:45 +0200

    Flytt og legg til manglende extensions-tester for common

--- web/src/test/kotlin/no/nav/su/se/bakover/web/ExtensionsKtTest.kt
+++ web/src/test/kotlin/no/nav/su/se/bakover/web/ExtensionsKtTest.kt
@@ -3,10 +3,6 @@ package no.nav.su.se.bakover.web
 import arrow.core.left
 import arrow.core.right
 import io.kotest.matchers.shouldBe
-import kotlinx.coroutines.runBlocking
-import no.nav.su.se.bakover.common.limitedUpwardsTo
-import no.nav.su.se.bakover.common.positiveOrZero
-import no.nav.su.se.bakover.common.roundToDecimals
 import org.junit.jupiter.api.Test
 import java.util.UUID
 
@@ -14,42 +10,13 @@ internal class ExtensionsKtTest {
 
     @Test
     fun `String toUUID gir fin feilmelding`() {
-        runBlocking {
-            "heisann".toUUID() shouldBe "heisann er ikke en gyldig UUID".left()
-        }
+        "heisann".toUUID() shouldBe "heisann er ikke en gyldig UUID".left()
     }
 
     @Test
     fun `String toUUID funker på gyldig UUID`() {
-        runBlocking {
-            UUID.randomUUID().let {
-                it.toString().toUUID() shouldBe it.right()
-            }
+        UUID.randomUUID().let {
+            it.toString().toUUID() shouldBe it.right()
         }
-    }
-
-    @Test
-    fun `sum limited to`() {
-        123000.0.limitedUpwardsTo(500.0) shouldBe 500.0
-        (-1234.0).limitedUpwardsTo(500.0) shouldBe -1234.0
-    }
-
-    @Test
-    fun `sum positive or zero`() {
-        123000.0.positiveOrZero() shouldBe 123000.0
-        (-1234.0).positiveOrZero() shouldBe 0
-    }
-
-    @Test
-    fun `round to amount of decimals`() {
-        1234.1234.roundToDecimals(1) shouldBe 1234.1
-        1234.1234.roundToDecimals(2) shouldBe 1234.12
-        1234.1234.roundToDecimals(3) shouldBe 1234.123
-        1234.1234.roundToDecimals(4) shouldBe 1234.1234
-
-        1234.5678.roundToDecimals(1) shouldBe 1234.6
-        1234.5678.roundToDecimals(2) shouldBe 1234.57
-        1234.5678.roundToDecimals(3) shouldBe 1234.568
-        1234.5678.roundToDecimals(4) shouldBe 1234.5678
     }
 }
