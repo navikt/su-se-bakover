0f11e7aa0fb53ba86615abfa5e425f39f10526f3
Jacob Meidell <jacob.meidell@nav.no>
2022-05-09 07:35:20 +0200

    Legg med sjekk av brev/dist

--- web-regresjonstest/src/test/kotlin/no/nav/su/se/bakover/web/komponenttest/AvslagKomponentTest.kt
+++ web-regresjonstest/src/test/kotlin/no/nav/su/se/bakover/web/komponenttest/AvslagKomponentTest.kt
@@ -1,13 +1,18 @@
 package no.nav.su.se.bakover.web.komponenttest
 
+import io.kotest.matchers.collections.shouldHaveSize
+import io.kotest.matchers.equality.shouldBeEqualToIgnoringFields
 import io.kotest.matchers.shouldBe
-import io.kotest.matchers.types.beOfType
+import io.kotest.matchers.shouldNotBe
 import no.nav.su.se.bakover.domain.Fnr
 import no.nav.su.se.bakover.domain.behandling.avslag.Avslagsgrunn
+import no.nav.su.se.bakover.domain.dokument.Dokument
 import no.nav.su.se.bakover.domain.søknadsbehandling.Søknadsbehandling
 import no.nav.su.se.bakover.domain.vedtak.Avslagsvedtak
+import no.nav.su.se.bakover.service.brev.HentDokumenterForIdType
 import no.nav.su.se.bakover.test.fixedClock
 import no.nav.su.se.bakover.test.generer
+import no.nav.su.se.bakover.test.shouldBeType
 import no.nav.su.se.bakover.web.søknadsbehandling.BehandlingJson
 import no.nav.su.se.bakover.web.søknadsbehandling.opprettAvslåttSøknadsbehandling
 import org.junit.jupiter.api.Test
@@ -28,12 +33,33 @@ class AvslagKomponentTest {
                 UUID.fromString(sakId)
             }
             val sak = appComponents.databaseRepos.sak.hentSak(sakId = sakId)!!
-            sak.søknadsbehandlinger.first() shouldBe beOfType<Søknadsbehandling.Iverksatt.Avslag.UtenBeregning>()
-            sak.vedtakListe.first() shouldBe beOfType<Avslagsvedtak.AvslagVilkår>()
-            (sak.vedtakListe.first() as Avslagsvedtak).avslagsgrunner shouldBe listOf(
-                Avslagsgrunn.UFØRHET,
-                Avslagsgrunn.FLYKTNING,
-            )
+
+            sak.vedtakListe.first().shouldBeType<Avslagsvedtak.AvslagVilkår>().let { avslagsvedtak ->
+                avslagsvedtak.behandling.shouldBeType<Søknadsbehandling.Iverksatt.Avslag.UtenBeregning>()
+                avslagsvedtak.avslagsgrunner shouldBe listOf(
+                    Avslagsgrunn.UFØRHET,
+                    Avslagsgrunn.FLYKTNING,
+                )
+                appComponents.services.brev.hentDokumenterFor(HentDokumenterForIdType.Vedtak(avslagsvedtak.id)).let {
+                    it shouldHaveSize 1
+                    it.first().shouldBeType<Dokument.MedMetadata.Vedtak>().let { dokument ->
+                        dokument.generertDokument shouldNotBe null
+                        dokument.generertDokumentJson shouldNotBe null
+                        dokument.tittel shouldBe "Vedtaksbrev for søknad om supplerende stønad"
+                        dokument.metadata.bestillBrev shouldBe true
+                        dokument.metadata.vedtakId shouldBe avslagsvedtak.id
+
+                        appComponents.databaseRepos.dokumentRepo.hentDokumenterForDistribusjon()
+                            .let { distribusjon ->
+                                distribusjon shouldHaveSize 1
+                                distribusjon.first().dokument.shouldBeEqualToIgnoringFields(
+                                    dokument,
+                                    Dokument::generertDokument,
+                                )
+                            }
+                    }
+                }
+            }
         }
     }
 }
