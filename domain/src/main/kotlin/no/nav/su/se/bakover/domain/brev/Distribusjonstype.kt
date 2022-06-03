2f7e35a0364a1a748960b77bd982c849d8c87359
ramziabuqassim <ramzi.abu.qassim@nav.no>
2022-06-03 09:19:51 +0200

    inn med litt docs & gjør opprettholdelse av klage til et viktig brev

--- domain/src/main/kotlin/no/nav/su/se/bakover/domain/brev/Distribusjonstype.kt
+++ domain/src/main/kotlin/no/nav/su/se/bakover/domain/brev/Distribusjonstype.kt
@@ -1,5 +1,8 @@
 package no.nav.su.se.bakover.domain.brev
 
+/**
+ * https://confluence.adeo.no/display/BOA/RDIST002-1.+DistribuerJournalpost+-+For+Konsumenter#RDIST0021.DistribuerJournalpostForKonsumenter-Distribusjonstype
+ */
 enum class Distribusjonstype {
     VEDTAK,
     VIKTIG,
