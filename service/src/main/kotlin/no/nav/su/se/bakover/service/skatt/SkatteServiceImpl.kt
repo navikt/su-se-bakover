03d14cacadf21ab058dc1d7db9678409c0fac633
Øyvind Haarberg <oyvind.haarberg@gmail.com>
2022-06-07 15:11:07 +0200

    Eksplisitt logging av hva som går feil i skatteoppslag

--- service/src/main/kotlin/no/nav/su/se/bakover/service/skatt/SkatteServiceImpl.kt
+++ service/src/main/kotlin/no/nav/su/se/bakover/service/skatt/SkatteServiceImpl.kt
@@ -8,6 +8,8 @@ import no.nav.su.se.bakover.client.isValid
 import no.nav.su.se.bakover.client.maskinporten.KunneIkkeHenteToken
 import no.nav.su.se.bakover.client.maskinporten.MaskinportenClient
 import no.nav.su.se.bakover.client.skatteetaten.Skatteoppslag
+import no.nav.su.se.bakover.client.skatteetaten.SkatteoppslagFeil
+import no.nav.su.se.bakover.common.log
 import no.nav.su.se.bakover.domain.Fnr
 import no.nav.su.se.bakover.domain.SamletSkattegrunnlag
 
@@ -31,7 +33,19 @@ class SkatteServiceImpl(
             .mapLeft { KunneIkkeHenteSkattemelding.KunneIkkeHenteAccessToken(it) }
             .flatMap { tokenResponse ->
                 skatteClient.hentSamletSkattegrunnlag(tokenResponse.accessToken, fnr)
-                    .mapLeft { KunneIkkeHenteSkattemelding.KallFeilet(it) }
+                    .mapLeft { feil ->
+                        KunneIkkeHenteSkattemelding.KallFeilet(feil).also { wrappedFeil ->
+                            val detaljOmFeil = when (val originalFeil = wrappedFeil.feil) {
+                                is SkatteoppslagFeil.KunneIkkeHenteSkattedata -> originalFeil.feilmelding
+                                is SkatteoppslagFeil.Nettverksfeil -> "Fikk ikke koblet opp mot skatt på grunn av nettverk"
+                            }
+                            log.warn(
+                                "Fikk ikke hentet skattemelding på grunn av feil i oppslaget: $detaljOmFeil. " +
+                                    "Dette betyr at saksbehandler ikke får opp informasjon om inntekt og formue " +
+                                    "fra skattemeldingen i saksbehandlingen."
+                            )
+                        }
+                    }
             }
     }
 }
