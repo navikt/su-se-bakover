5c2b7f7bae347d80d583c9f3ac815679c2f8f328
Øyvind Haarberg <oyvind.haarberg@gmail.com>
2022-06-07 16:30:09 +0200

    La oss bare hente token hver gang

--- service/src/main/kotlin/no/nav/su/se/bakover/service/skatt/SkatteServiceImpl.kt
+++ service/src/main/kotlin/no/nav/su/se/bakover/service/skatt/SkatteServiceImpl.kt
@@ -2,10 +2,6 @@ package no.nav.su.se.bakover.service.skatt
 
 import arrow.core.Either
 import arrow.core.flatMap
-import arrow.core.right
-import no.nav.su.se.bakover.client.ExpiringTokenResponse
-import no.nav.su.se.bakover.client.isValid
-import no.nav.su.se.bakover.client.maskinporten.KunneIkkeHenteToken
 import no.nav.su.se.bakover.client.maskinporten.MaskinportenClient
 import no.nav.su.se.bakover.client.skatteetaten.Skatteoppslag
 import no.nav.su.se.bakover.client.skatteetaten.SkatteoppslagFeil
@@ -18,18 +14,8 @@ class SkatteServiceImpl(
     private val maskinportenClient: MaskinportenClient,
 ) : SkatteService {
 
-    private var skatteetatenToken: ExpiringTokenResponse? = null
-
-    private fun medToken(): Either<KunneIkkeHenteToken, ExpiringTokenResponse> {
-        if (skatteetatenToken.isValid()) {
-            return skatteetatenToken!!.right()
-        }
-        return maskinportenClient.hentNyToken()
-            .tap { token -> skatteetatenToken = token }
-    }
-
     override fun hentSamletSkattegrunnlag(fnr: Fnr): Either<KunneIkkeHenteSkattemelding, SamletSkattegrunnlag> {
-        return medToken()
+        return maskinportenClient.hentNyToken()
             .mapLeft { KunneIkkeHenteSkattemelding.KunneIkkeHenteAccessToken(it) }
             .flatMap { tokenResponse ->
                 skatteClient.hentSamletSkattegrunnlag(tokenResponse.accessToken, fnr)
