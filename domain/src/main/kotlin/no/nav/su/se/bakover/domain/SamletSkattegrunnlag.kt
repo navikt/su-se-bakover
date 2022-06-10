11d6712474452ceab82fec5a6858f60238a0bf26
Øyvind Haarberg <oyvind.haarberg@gmail.com>
2022-06-10 14:54:10 +0200

    Legger inn informasjon om SamletSkattegrunnlag-datatypen

--- domain/src/main/kotlin/no/nav/su/se/bakover/domain/SamletSkattegrunnlag.kt
+++ domain/src/main/kotlin/no/nav/su/se/bakover/domain/SamletSkattegrunnlag.kt
@@ -1,5 +1,11 @@
 package no.nav.su.se.bakover.domain
 
+/**
+ * Mapper responsen fra Skatteetatens API for summert skattegrunnlag
+ *
+ * Swagger-dokumentasjon for APIet er på
+ * https://app.swaggerhub.com/apis/Skatteetaten_Deling/summert-skattegrunnlag-api/1.0.0
+ */
 data class SamletSkattegrunnlag(
     val personidentifikator: String,
     val inntektsaar: String,
