a1fe85423346f7939078daa6d1cd1ce0af842381
Jacob Meidell <jacob.meidell@nav.no>
2022-08-29 16:24:32 +0200

    Fjern dobbel null i db-migration.

--- database/src/main/resources/db/migration/V157__utbetalingslinje_virkningsperiode.sql
+++ database/src/main/resources/db/migration/V157__utbetalingslinje_virkningsperiode.sql
@@ -1,3 +1,3 @@
 alter table utbetalingslinje add column if not exists statusTilOgMed date;
 
-update utbetalingslinje set statusTilOgMed = tom where status is not null null and statusFraOgMed is not null;
+update utbetalingslinje set statusTilOgMed = tom where status is not null and statusFraOgMed is not null;
