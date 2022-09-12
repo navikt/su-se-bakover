57737b8a1f4fa4552168118da7ecfa445d721130
John Andre Hestad <john.andre.hestad@nav.no>
2022-09-12 18:44:16 +0200

    Legg til mulighet for å spesifisere antall lokale søknader/søknadsbehandlinger som skal genereres.

--- local-db-scripts/create-søknad.sh
+++ local-db-scripts/create-søknad.sh
@@ -6,4 +6,11 @@ do
   echo "Waiting for postgres to start. Sleeping one second ..."
   sleep 1
 done
-"${BASEDIR}/../gradlew" -p "${BASEDIR}/.."  nySøknad
+i=0
+until="${1:-1}"
+echo "Creating $until søknad(er)"
+while [ "$i" -ne "$until" ]
+do
+        i=$((i+1))
+        "${BASEDIR}/../gradlew" -p "${BASEDIR}/.."  nySøknad
+done
