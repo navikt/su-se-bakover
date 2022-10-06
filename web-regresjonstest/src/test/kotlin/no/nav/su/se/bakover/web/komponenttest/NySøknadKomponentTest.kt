918af101b1099112108f2446144a223c57592649
Jacob Meidell <jacob.meidell@nav.no>
2022-10-06 12:53:01 +0200

    Fjern bruk av db for web tester/slett/erstatt

---
+++ web-regresjonstest/src/test/kotlin/no/nav/su/se/bakover/web/komponenttest/NySøknadKomponentTest.kt
@@ -0,0 +1,39 @@
+package no.nav.su.se.bakover.web.komponenttest
+
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
+import no.nav.su.se.bakover.common.NavIdentBruker
+import no.nav.su.se.bakover.domain.Sakstype
+import no.nav.su.se.bakover.domain.journal.JournalpostId
+import no.nav.su.se.bakover.domain.oppgave.OppgaveId
+import no.nav.su.se.bakover.domain.søknad.Søknad
+import no.nav.su.se.bakover.test.fixedClock
+import no.nav.su.se.bakover.test.getOrFail
+import no.nav.su.se.bakover.test.shouldBeType
+import no.nav.su.se.bakover.web.søknad.ny.NySøknadJson
+import no.nav.su.se.bakover.web.søknad.ny.nyDigitalSøknad
+import org.junit.jupiter.api.Test
+import java.util.UUID
+
+internal class NySøknadKomponentTest {
+    @Test
+    fun `innsending av ny førstegangssøknad søknad oppretter sak, journalfører søknad og oppretter oppgave`() {
+        withKomptestApplication(
+            clock = fixedClock,
+        ) { appComponents ->
+            val (sakId, søknadId) = nyDigitalSøknad().let {
+                UUID.fromString(NySøknadJson.Response.hentSakId(it)) to UUID.fromString(NySøknadJson.Response.hentSøknadId(it))
+            }
+            val sak = appComponents.services.sak.hentSak(sakId).getOrFail()
+            appComponents.services.søknad.hentSøknad(søknadId).getOrFail().also { søknad ->
+                søknad.shouldBeType<Søknad.Journalført.MedOppgave.IkkeLukket>().let {
+                    it.fnr shouldBe sak.fnr
+                    it.type shouldBe Sakstype.UFØRE
+                    it.innsendtAv shouldBe NavIdentBruker.Veileder("Z990Lokal")
+                    it.oppgaveId shouldNotBe OppgaveId("stubbedOppgaveId")
+                    it.journalpostId shouldBe JournalpostId("1")
+                }
+            }
+        }
+    }
+}
