349ef5b1f785cc0caebc2fa59f1ea274ee1d8209
Jacob Meidell <jacob.meidell@nav.no>
2022-12-08 09:20:59 +0100

    Legg til innhold i main() og test logg

---
+++ datapakker/søknad/src/main/kotlin/no/nav.su.se.bakover.datapakker.soknad/App.kt
@@ -0,0 +1,9 @@
+package no.nav.su.se.bakover.datapakker.soknad
+
+import org.slf4j.LoggerFactory
+
+private val logger = LoggerFactory.getLogger("DatapakkerSøknad")
+
+fun main() {
+    logger.info("This is hello from ${logger.name}")
+}
