df94c6e7608da0019215faef0b53f00fc8ac1fe2
Jacob Meidell <jacob.meidell@nav.no>
2022-12-08 10:17:15 +0100

    Pakk main til app.jar

--- datapakker/søknad/build.gradle.kts
+++ datapakker/søknad/build.gradle.kts
@@ -1,2 +1,10 @@
+tasks.named<Jar>("jar") {
+    archiveBaseName.set("app")
+    archiveVersion.set("")
+    manifest {
+        attributes["Main-Class"] = "no.nav.su.se.bakover.datapakker.soknad.AppKt"
+    }
+}
+
 dependencies {
 }
