0a5ea9a970c22dd3bd7b552aa10da01487628d33
Jacob Meidell <jacob.meidell@nav.no>
2022-12-08 15:15:46 +0100

    Readme, test db, fiks secret env

--- datapakker/søknad/src/main/kotlin/no/nav.su.se.bakover.datapakker.soknad/App.kt
+++ datapakker/søknad/src/main/kotlin/no/nav.su.se.bakover.datapakker.soknad/App.kt
@@ -1,9 +1,25 @@
 package no.nav.su.se.bakover.datapakker.soknad
 
+import no.nav.su.se.bakover.database.Postgres
+import no.nav.su.se.bakover.database.VaultPostgres
 import org.slf4j.LoggerFactory
 
 private val logger = LoggerFactory.getLogger("DatapakkerSøknad")
 
 fun main() {
+    VaultPostgres(
+        jdbcUrl = "jdbc:postgresql://b27dbvl009.preprod.local:5432/supstonad-db-dev",
+        vaultMountPath = "postgresql/preprod-fss/",
+        databaseName = "supstonad-db-dev",
+    ).getDatasource(Postgres.Role.ReadOnly).let {
+        it.getConnection(it.username, it.password)
+    }
+
+    logger.info(
+        """
+        secret-env:${System.getProperty("secret-env")}
+        """.trimIndent(),
+    )
+
     logger.info("This is hello from ${logger.name}. Vi har vault=${!System.getProperty("bigquery", null).isNullOrEmpty()}")
 }
