df4c48add64501558cbae60a289f300098da7e8d
Jacob Meidell <jacob.meidell@nav.no>
2022-12-08 12:38:24 +0100

    O ø

--- datapakker/søknad/build.gradle.kts
+++ datapakker/søknad/build.gradle.kts
@@ -2,7 +2,7 @@ tasks.named<Jar>("jar") {
     archiveBaseName.set("app")
     archiveVersion.set("")
     manifest {
-        attributes["Main-Class"] = "no.nav.su.se.bakover.datapakker.søknad.AppKt"
+        attributes["Main-Class"] = "no.nav.su.se.bakover.datapakker.soknad.AppKt"
         attributes["Class-Path"] = configurations.runtimeClasspath.get().joinToString(separator = " ") {
             it.name
         }
