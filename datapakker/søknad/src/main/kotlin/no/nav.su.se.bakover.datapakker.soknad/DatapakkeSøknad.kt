adb4e049f0b59b1e0a59ccfa7c6abdca224bcd6e
ramziabuqassim <ramzi.abu.qassim@nav.no>
2022-12-09 13:32:22 +0100

    henter søknader og poster til bigQuery

---
+++ datapakker/søknad/src/main/kotlin/no/nav.su.se.bakover.datapakker.soknad/DatapakkeSøknad.kt
@@ -0,0 +1,34 @@
+package no.nav.su.se.bakover.datapakker.soknad
+
+import no.nav.su.se.bakover.common.Tidspunkt
+import java.time.LocalDate
+import java.util.UUID
+
+data class DatapakkeSøknad(
+    val id: UUID,
+    val opprettet: Tidspunkt,
+    val type: DatapakkeSøknadstype,
+    val mottaksdato: LocalDate,
+)
+
+enum class DatapakkeSøknadstype {
+    Papirsøknad,
+    DigitalSøknad,
+    ;
+
+    companion object {
+        fun stringToSøknadstype(s: String): DatapakkeSøknadstype {
+            return when (s) {
+                "Papirsøknad" -> Papirsøknad
+                "DigitalSøknad" -> DigitalSøknad
+                else -> throw IllegalArgumentException("ukjent type: $s")
+            }
+        }
+    }
+}
+
+fun List<DatapakkeSøknad>.toCSV(): String {
+    return this.joinToString(separator = "\n") {
+        "${it.id},${it.opprettet},${it.type}"
+    }
+}
