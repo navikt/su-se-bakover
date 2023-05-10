67587dd8d8c95b7088f497f265a84757389f63e3
ramziabuqassim <ramzi.abu.qassim@nav.no>
2023-05-10 14:25:59 +0200

    gjør code i ErrorJson non-nullable

--- common/src/main/kotlin/no/nav/su/se/bakover/common/infrastructure/web/ErrorJson.kt
+++ common/src/main/kotlin/no/nav/su/se/bakover/common/infrastructure/web/ErrorJson.kt
@@ -1,6 +1,5 @@
 package no.nav.su.se.bakover.common.infrastructure.web
 
-import com.fasterxml.jackson.annotation.JsonInclude
 import io.ktor.http.HttpStatusCode
 
 data class ErrorJson(
@@ -9,16 +8,8 @@ data class ErrorJson(
      * En maskintolkbar representasjon av feilmeldingen, unik innenfor sitt scope/endepunkt.
      * Ønskelig å holde koden statisk over tid (en del av APIet), men med mulighet for å endre innholdet i 'message'-feltet.
      * Format: snake_case
-     * I utgangspunktet nullable for å være bakoverkompatibel.
      * */
-    @JsonInclude(JsonInclude.Include.NON_NULL)
-    val code: String? = null,
+    val code: String,
 ) {
-    /**
-     * Kaster exception dersom denne brukes på en ErrorJson uten code
-     */
-    fun tilResultat(httpStatusCode: HttpStatusCode): Resultat {
-        if (code == null) throw IllegalArgumentException("Prøvde å gjøre en errorJson til resultat uten en error code. Error code er påkrevd")
-        return httpStatusCode.errorJson(message, code)
-    }
+    fun tilResultat(httpStatusCode: HttpStatusCode): Resultat = httpStatusCode.errorJson(message, code)
 }
