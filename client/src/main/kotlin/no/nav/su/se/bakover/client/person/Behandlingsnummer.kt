e99975a25d6683f96a89fda00a0a289c96dc158e
ramziabuqassim <ramzi.abu.qassim@nav.no>
2023-06-05 16:05:47 +0200

    Legg på behandlingsnummer for kall mot PDL

---
+++ client/src/main/kotlin/no/nav/su/se/bakover/client/person/Behandlingsnummer.kt
@@ -0,0 +1,9 @@
+package no.nav.su.se.bakover.client.person
+
+/**
+ * https://behandlingskatalog.nais.adeo.no/process/purpose/SUPPLERENDESTOENAD/bb50b682-633c-4e4c-aae0-99c4c95c264a
+ */
+enum class Behandlingsnummer(val value: String) {
+    UFØRE("B126"),
+    // ALDER("B237");
+}
