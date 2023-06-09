1e0a8472f38690345035d77296eabbe76b954647
RamziAbuQassim <59833292+RamziAbuQassim@users.noreply.github.com>
2023-06-09 15:31:25 +0200

    Update domain/src/main/kotlin/no/nav/su/se/bakover/domain/behandling/Avsluttet.kt
    Co-authored-by: John Andre Hestad <john.andre.hestad@nav.no>

--- domain/src/main/kotlin/no/nav/su/se/bakover/domain/behandling/Avsluttet.kt
+++ domain/src/main/kotlin/no/nav/su/se/bakover/domain/behandling/Avsluttet.kt
@@ -1,9 +1,7 @@
 package no.nav.su.se.bakover.domain.behandling
 
 /**
- * At noe er avsluttet vil si at den ikke kan endres. Eksempler kan være at man ferdig behandler noe, og den skal
- * bli låst.
- *
- * Eksempel er [no.nav.su.se.bakover.domain.vedtak.Vedtak]
- */
+  * Når noe er avsluttet, betyr det at det ikke kan endres. Eksempler kan være når noe er ferdigbehandlet og skal bli låst.
+  * Et eksempel er [no.nav.su.se.bakover.domain.vedtak.Vedtak]
+  */
 interface Avsluttet
