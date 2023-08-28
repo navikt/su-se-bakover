1d85c4fce613752a7be92a0374a293b0f141c5f3
ramziabuqassim <ramzi.abu.qassim@nav.no>
2023-08-28 13:07:47 +0200

    Oppdater vedtak_uten_fritekst.sql til å inkludere periode hvis man vil

--- local-db-scripts/vedtak_uten_fritekst.sql
+++ local-db-scripts/vedtak_uten_fritekst.sql
@@ -6,8 +6,12 @@ from klage k
 where length(trim(d.generertdokumentjson ->> 'fritekst')) < 1;
 
 -- Henter ut alle avslagsvedtak som har et dokument som ikke har fritekst
+-- I tillegg kan det legges til perioden som du vil sjekke for
 select count(d.generertdokumentjson)
 from vedtak v
          join dokument d on v.id = d.vedtakid
 where length(trim(d.generertdokumentjson ->> 'fritekst')) < 1
   and v.vedtaktype = 'AVSLAG';
+-- and Date(v.opprettet) >= '2023-05-01'
+-- and Date(v.opprettet) <= '2023-08-31';
+
