be41c8f3d13b0ac7bb67c547937f0ffa06798524
ramziabuqassim <ramzi.abu.qassim@nav.no>
2023-09-28 14:01:39 +0200

    legger på status - TilbakekrevingsbehandlingJson

--- tilbakekreving/presentation/src/main/kotlin/tilbakekreving/presentation/api/common/TilbakekrevingsbehandlingJson.kt
+++ tilbakekreving/presentation/src/main/kotlin/tilbakekreving/presentation/api/common/TilbakekrevingsbehandlingJson.kt
@@ -2,8 +2,13 @@ package tilbakekreving.presentation.api.common
 
 import no.nav.su.se.bakover.common.serialize
 import no.nav.su.se.bakover.common.tid.Tidspunkt
+import tilbakekreving.domain.IverksattTilbakekrevingsbehandling
+import tilbakekreving.domain.OpprettetTilbakekrevingsbehandling
 import tilbakekreving.domain.Tilbakekrevingsbehandling
+import tilbakekreving.domain.TilbakekrevingsbehandlingTilAttestering
+import tilbakekreving.domain.VurdertTilbakekrevingsbehandling
 import tilbakekreving.presentation.api.common.KravgrunnlagJson.Companion.toJson
+import java.lang.IllegalStateException
 
 data class TilbakekrevingsbehandlingJson(
     val id: String,
@@ -11,6 +16,7 @@ data class TilbakekrevingsbehandlingJson(
     val opprettet: Tidspunkt,
     val opprettetAv: String,
     val kravgrunnlag: KravgrunnlagJson,
+    val status: TilbakekrevingsbehandlingStatus,
 ) {
 
     companion object {
@@ -23,6 +29,14 @@ data class TilbakekrevingsbehandlingJson(
             opprettet = opprettet,
             opprettetAv = opprettetAv.toString(),
             kravgrunnlag = this.kravgrunnlag.toJson(),
+            status = when (this) {
+                is OpprettetTilbakekrevingsbehandling -> TilbakekrevingsbehandlingStatus.OPPRETTET
+                is VurdertTilbakekrevingsbehandling.Påbegynt -> TilbakekrevingsbehandlingStatus.VURDERT_UTEN_BREV
+                is VurdertTilbakekrevingsbehandling.Utfylt -> TilbakekrevingsbehandlingStatus.VURDERT_MED_BREV
+                is TilbakekrevingsbehandlingTilAttestering -> TilbakekrevingsbehandlingStatus.TIL_ATTESTERING
+                is IverksattTilbakekrevingsbehandling -> TilbakekrevingsbehandlingStatus.IVERKSATT
+                else -> throw IllegalStateException("tilbakekreving $id har ikke en mappet tilstand til frontend")
+            },
         )
     }
 }
---
+++ tilbakekreving/presentation/src/main/kotlin/tilbakekreving/presentation/api/common/TilbakekrevingsbehandlingStatus.kt
@@ -0,0 +1,9 @@
+package tilbakekreving.presentation.api.common
+
+enum class TilbakekrevingsbehandlingStatus {
+    OPPRETTET,
+    VURDERT_UTEN_BREV,
+    VURDERT_MED_BREV,
+    TIL_ATTESTERING,
+    IVERKSATT,
+}
