7185ccddc1ed06e5e83eebe651a129ad7c597d9c
John Andre Hestad <john.andre.hestad@nav.no>
2023-10-02 18:12:16 +0200

    Legg til RåttKravgrunnlagService

---
+++ tilbakekreving/application/src/main/kotlin/tilbakekreving/application/service/RåttKravgrunnlagService.kt
@@ -0,0 +1,33 @@
+package tilbakekreving.application.service
+
+import no.nav.su.se.bakover.common.CorrelationId
+import no.nav.su.se.bakover.common.tid.Tidspunkt
+import no.nav.su.se.bakover.hendelse.domain.HendelseId
+import no.nav.su.se.bakover.hendelse.domain.HendelseMetadata
+import tilbakekreving.domain.kravgrunnlag.KravgrunnlagRepo
+import tilbakekreving.domain.kravgrunnlag.RåttKravgrunnlag
+import tilbakekreving.domain.kravgrunnlag.RåttKravgrunnlagHendelse
+import java.time.Clock
+
+class RåttKravgrunnlagService(
+    private val kravgrunnlagRepo: KravgrunnlagRepo,
+    private val clock: Clock,
+) {
+    /**
+     * Vi har ikke noe de-dup i dette leddet, så vi kan få "duplikat"-hendelser i databasen.
+     * Vi løser de-dup i neste ledd, når vi knytter den mot en utbetaling på en sak.
+     */
+    fun lagreRåKvitteringshendelse(
+        råttKravgrunnlag: RåttKravgrunnlag,
+        correlationId: CorrelationId,
+    ) {
+        kravgrunnlagRepo.lagreRåttKravgrunnlagHendelse(
+            hendelse = RåttKravgrunnlagHendelse(
+                hendelseId = HendelseId.generer(),
+                hendelsestidspunkt = Tidspunkt.now(clock),
+                meta = HendelseMetadata.fraCorrelationId(correlationId),
+                råttKravgrunnlag = råttKravgrunnlag,
+            ),
+        )
+    }
+}
