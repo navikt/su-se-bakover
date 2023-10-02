a7d3abd98239d604db1adfdd8711c9d562c9364c
John Andre Hestad <john.andre.hestad@nav.no>
2023-10-02 19:40:28 +0200

    Forbered Kravgrunnlag-konsument

---
+++ tilbakekreving/presentation/src/main/kotlin/tilbakekreving/presentation/consumer/KravgrunnlagIbmMqConsumer.kt
@@ -0,0 +1,50 @@
+package tilbakekreving.presentation.consumer
+
+import no.nav.su.se.bakover.common.infrastructure.correlation.withCorrelationId
+import no.nav.su.se.bakover.common.sikkerLogg
+import org.slf4j.LoggerFactory
+import tilbakekreving.application.service.RåttKravgrunnlagService
+import tilbakekreving.domain.kravgrunnlag.RåttKravgrunnlag
+import javax.jms.JMSContext
+import javax.jms.Session
+
+/**
+ * Har ansvar for:
+ * 1. Motta kravgrunnlag fra meldingskøen.
+ * 2. Kalle Service for å lagre kravgrunnlaget.
+ *
+ * Denne skal erstatte TilbakekrevingIbmMqConsumer
+ */
+internal class KravgrunnlagIbmMqConsumer(
+    queueName: String,
+    globalJmsContext: JMSContext,
+    private val service: RåttKravgrunnlagService,
+) {
+    private val log = LoggerFactory.getLogger(this::class.java)
+    private val jmsContext = globalJmsContext.createContext(Session.AUTO_ACKNOWLEDGE)
+    private val consumer = jmsContext.createConsumer(jmsContext.createQueue(queueName))
+
+    init {
+        consumer.setMessageListener { message ->
+            try {
+                withCorrelationId { correlationId ->
+                    log.info("Mottok kravgrunnlag fra køen: $queueName. Se sikkerlogg for meldingsinnhold.")
+                    message.getBody(String::class.java).let {
+                        sikkerLogg.info("Kravgrunnlag lest fra $queueName, innhold: $it")
+                        service.lagreRåKvitteringshendelse(
+                            RåttKravgrunnlag(it),
+                            correlationId = correlationId,
+                        )
+                        log.info("Lagret kravgrunnlag fra køen: $queueName. Acker meldingen.")
+                    }
+                }
+            } catch (ex: Exception) {
+                log.error("Feil ved prosessering av melding fra: $queueName", ex)
+                throw ex
+            }
+        }
+        jmsContext.setExceptionListener { exception -> log.error("Feil mot $queueName", exception) }
+
+        jmsContext.start()
+    }
+}
