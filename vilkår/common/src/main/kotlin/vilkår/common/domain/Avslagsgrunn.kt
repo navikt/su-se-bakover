4e9107e0a40dd2e512ce3970c0ccd0d20e146c7e
ramziabuqassim <ramzi.abu.qassim@nav.no>
2024-01-30 14:43:07 +0100

    fjern kommentert ut kode

--- vilkår/common/src/main/kotlin/vilkår/common/domain/Avslagsgrunn.kt
+++ vilkår/common/src/main/kotlin/vilkår/common/domain/Avslagsgrunn.kt
@@ -24,26 +24,6 @@ enum class Avslagsgrunn(val paragrafer: NonEmptyList<Int>) {
     MANGLER_VEDTAK_ANDRE_NORSKE_PENSJONSORDNINGER(nonEmptyListOf(3)),
     MANGLER_VEDTAK_UTENLANDSKE_PENSJONSORDNINGER(nonEmptyListOf(3)),
     FAMILIEGJENFORENING(nonEmptyListOf(3)),
-
-//    // TODO: bør lage en paragraf-type/enum
-//    fun getParagrafer() = when (this) {
-//        UFØRHET -> listOf(1, 2)
-//        FLYKTNING -> listOf(2, 3)
-//        OPPHOLDSTILLATELSE -> listOf(1, 2)
-//        PERSONLIG_OPPMØTE -> listOf(17)
-//        FORMUE -> listOf(8)
-//        BOR_OG_OPPHOLDER_SEG_I_NORGE -> listOf(1, 2, 3, 4)
-//        FOR_HØY_INNTEKT -> listOf(5, 6, 7)
-//        SU_UNDER_MINSTEGRENSE -> listOf(5, 6, 9)
-//        UTENLANDSOPPHOLD_OVER_90_DAGER -> listOf(1, 2, 4)
-//        INNLAGT_PÅ_INSTITUSJON -> listOf(12)
-//        MANGLENDE_DOKUMENTASJON -> listOf(18)
-//        SØKNAD_MANGLER_DOKUMENTASJON -> listOf(18)
-//        MANGLER_VEDTAK_ALDERSPENSJON_FOLKETRYGDEN -> listOf(3)
-//        MANGLER_VEDTAK_ANDRE_NORSKE_PENSJONSORDNINGER -> listOf(3)
-//        MANGLER_VEDTAK_UTENLANDSKE_PENSJONSORDNINGER -> listOf(3)
-//        FAMILIEGJENFORENING -> listOf(3)
-//    }
 }
 
 fun List<Avslagsgrunn>.getDistinkteParagrafer(): List<Int> =
