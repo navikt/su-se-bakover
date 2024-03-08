63e76a4df18df3a0eb715ae8bda9bb0ed563b90b
John Andre Hestad <john.andre.hestad@nav.no>
2024-03-08 11:39:38 +0100

    verifyUniqueNames task: Ignore fritekstAvslag module

--- build.gradle.kts
+++ build.gradle.kts
@@ -268,6 +268,7 @@ tasks.register("verifyUniqueJarNames") {
         val allJarNames = allprojects.mapNotNull { project ->
             // :datapakker:soknad kjøres som egen pod og må hete app.jar (samme som application-modulen) pga. baseimages: https://github.com/navikt/baseimages/tree/master/java
             if (project.path == ":datapakker:soknad") return@mapNotNull null
+            if (project.path == ":datapakker:fritekstAvslag") return@mapNotNull null
             project.tasks.findByName("jar")?.let {
                 (it as? org.gradle.jvm.tasks.Jar)?.archiveBaseName?.get()
             }
