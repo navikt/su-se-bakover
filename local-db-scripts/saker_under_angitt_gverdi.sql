550eab856b5f23e98709726be23f3553c02ebef8
John Andre Hestad <john.andre.hestad@nav.no>
2024-05-27 13:57:09 +0200

    Oppdater sql: saker_under_angitt_gverdi-sql

--- local-db-scripts/saker_under_angitt_gverdi.sql
+++ local-db-scripts/saker_under_angitt_gverdi.sql
@@ -1,4 +1,7 @@
 with gVerdiOgSak as (select distinct s.saksnummer,
+                                     status,
+                                     (b.stønadsperiode -> 'periode' ->>'fraOgMed')::date         as fraOgMed,
+                                     (b.stønadsperiode->'periode' ->> 'tilOgMed' )::date        as tilOgMed,
                                      (jsonb_array_elements(beregning -> 'månedsberegninger') ->
                                       'benyttetGrunnbeløp')::integer as gverdi
                      from sak s
@@ -8,29 +11,30 @@ with gVerdiOgSak as (select distinct s.saksnummer,
                            ('BEREGNET_AVSLAG', 'BEREGNET_INNVILGET', 'SIMULERT', 'TIL_ATTESTERING_INNVILGET',
                             'TIL_ATTESTERING_AVSLAG',
                             'UNDERKJENT_INNVILGET', 'UNDERKJENT_AVSLAG')
-                       and lukket is false)
+                       and b.lukket is false)
 select *
 from gVerdiOgSak
-where gverdi < 118620;
+where gverdi < 118621 and tilOgMed >= '2024-05-01' order by saksnummer;
 
 
 with gVerdiOgSak as (select distinct s.saksnummer,
-                                     (b.periode ->>'fraOgMed')::date         as fraOgMed,
-                                     (b.periode ->> 'tilOgMed' )::date        as tilOgMed,
-                                     (jsonb_array_elements(beregning -> 'månedsberegninger') ->
+                                     r.revurderingstype,
+                                     (r.periode ->>'fraOgMed')::date         as fraOgMed,
+                                     (r.periode ->> 'tilOgMed' )::date        as tilOgMed,
+                                     (jsonb_array_elements(r.beregning -> 'månedsberegninger') ->
                                       'benyttetGrunnbeløp')::integer as gverdi
                      from sak s
-                              join revurdering b on s.id = b.sakid
-                     where revurderingstype
-                               in
-                           ('BEREGNET_OPPHØRT',
-                            'BEREGNET_INNVILGET',
-                            'SIMULERT_INNVILGET',
-                            'SIMULERT_OPPHØRT',
-                            'TIL_ATTESTERING_INNVILGET',
-                            'TIL_ATTESTERING_OPPHØRT',
-                            'UNDERKJENT_INNVILGET',
-                            'UNDERKJENT_AVSLAG'))
+                              join revurdering r on s.id = r.sakid
+                     where r.avsluttet is null and r.revurderingstype
+                         in
+                                                   ('BEREGNET_OPPHØRT',
+                                                    'BEREGNET_INNVILGET',
+                                                    'SIMULERT_INNVILGET',
+                                                    'SIMULERT_OPPHØRT',
+                                                    'TIL_ATTESTERING_INNVILGET',
+                                                    'TIL_ATTESTERING_OPPHØRT',
+                                                    'UNDERKJENT_INNVILGET',
+                                                    'UNDERKJENT_AVSLAG'))
 select *
 from gVerdiOgSak
-where gverdi < 118620 and fraOgMed > '2023-05-01';
+where gverdi < 118621 and tilogmed >= '2024-05-01' order by saksnummer;
