7d57d8f9f5e0ed10210ece83406a2bc7cee0cbd6
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2025-04-15 10:00:36 +0200

    Smålogging

--- build.gradle.kts
+++ build.gradle.kts
@@ -215,7 +215,7 @@ tasks.register<Copy>("gitHooks") {
     into(".git/hooks")
 }
 
-//TODO: burde ligge direkte inne i hooks vel også kan hooken kjøre en egendefinert task ala //dependsOn("spotlessApply")...
+//TODO: burde fligge direkte inne i hooks vel også kan hooken kjøre en egendefinert task ala //dependsOn("spotlessApply")...
 tasks.named("build") {
     dependsOn(":gitHooks")
 }
