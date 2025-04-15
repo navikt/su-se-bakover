7bc90165b318c7de2b4892828491e6d0528f94b9
Bjorn Golberg <bjorn.golberg@gmail.com>
2025-04-15 11:56:16 +0200

    Kommenterarer for å vise revurderingsteg som er unik for saktype

--- domain/src/main/kotlin/no/nav/su/se/bakover/domain/revurdering/steg/Revurderingsteg.kt
+++ domain/src/main/kotlin/no/nav/su/se/bakover/domain/revurdering/steg/Revurderingsteg.kt
@@ -2,20 +2,21 @@ package no.nav.su.se.bakover.domain.revurdering.steg
 
 enum class Revurderingsteg(val vilkår: String) {
     // BorOgOppholderSegINorge("BorOgOppholderSegINorge"),
-    Flyktning("Flyktning"),
     Formue("Formue"),
-
     Oppholdstillatelse("Oppholdstillatelse"),
-
     PersonligOppmøte("PersonligOppmøte"),
-    Uførhet("Uførhet"),
     Bosituasjon("Bosituasjon"),
-
     Institusjonsopphold("Institusjonsopphold"),
     Utenlandsopphold("Utenlandsopphold"),
     Inntekt("Inntekt"),
     Opplysningsplikt("Opplysningsplikt"),
-    Pensjon("Pensjon"),
     FastOppholdINorge("FastOppholdINorge"),
+
+    // kun for uføre
+    Flyktning("Flyktning"),
+    Uførhet("Uførhet"),
+
+    // Kun for alder
+    Pensjon("Pensjon"),
     Familiegjenforening("Familiegjenforening"),
 }
