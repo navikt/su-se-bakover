dbcb93adbac9dd4b7d7ee2b73ba2555e68ca0c25
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2025-06-18 16:22:00 +0200

    Fiks pdfopplasting

--- web/src/main/kotlin/no/nav/su/se/bakover/web/routes/sak/SakRoutesEx.kt
+++ web/src/main/kotlin/no/nav/su/se/bakover/web/routes/sak/SakRoutesEx.kt
@@ -1,17 +1,17 @@
 package no.nav.su.se.bakover.web.routes.sak
 
 import dokument.domain.distribuering.Distribueringsadresse
-import io.ktor.http.content.MultiPartData
 import io.ktor.http.content.PartData
+import io.ktor.http.content.forEachPart
 import io.ktor.server.application.ApplicationCall
 import io.ktor.server.request.receiveMultipart
+import io.ktor.utils.io.readByteArray
 import io.ktor.utils.io.readRemaining
 import kotlinx.io.readByteArray
 import no.nav.su.se.bakover.common.deserialize
 import no.nav.su.se.bakover.common.domain.PdfA
 import no.nav.su.se.bakover.common.infrastructure.web.suUserContext
 import no.nav.su.se.bakover.domain.sak.JournalførOgSendOpplastetPdfSomBrevCommand
-import java.util.ArrayList
 import java.util.UUID
 
 data class DistribueringsadresseBody(
@@ -37,66 +37,65 @@ data class DokumentBody(
     val distribusjonstype: Distribusjonstype,
 )
 
+/**
+ * Vi forventer en viss rekkefølge fra frontend på innholdet i formdata
+ * 1. journaltittel
+ * 2. distribusjonstype
+ * 3. pdf
+ * 4. distribueringsadresse - Denne er den eneste som er optional, og kommer sist i rekkefølgen
+ */
+
 suspend fun ApplicationCall.lagCommandForLagreOgSendOpplastetPdfPåSak(
     sakId: UUID,
 ): JournalførOgSendOpplastetPdfSomBrevCommand {
-    val parts = this.receiveMultipart().readAllParts()
+    val parts = this.receiveMultipart()
 
-    /**
-     * Vi forventer en viss rekkefølge fra frontend på innholdet i formdata
-     * 1. journaltittel
-     * 2. distribusjonstype
-     * 3. pdf
-     * 4. distribueringsadresse - Denne er den eneste som er optional, og kommer sist i rekkefølgen
-     */
-    val journaltittel: String = (parts[0] as PartData.FormItem).let { partdata ->
-        partdata.value.also {
-            partdata.dispose
-        }
-    }
-    val distribusjonstype: dokument.domain.Distribusjonstype =
-        (parts[1] as PartData.FormItem).let { partdata ->
-            Distribusjonstype.valueOf(partdata.value).toDomain().also {
-                partdata.dispose
+    var pdfContent: ByteArray? = null
+    var journaltittel: String? = null
+    var distribueringsadresse: Distribueringsadresse? = null
+    var distribusjonstype: dokument.domain.Distribusjonstype? = null
+    parts.forEachPart { part ->
+        when (part) {
+            is PartData.FormItem -> {
+                when (part.name) {
+                    "distribusjonstype" -> {
+                        distribusjonstype = Distribusjonstype.valueOf(part.value).toDomain()
+                        part.dispose()
+                    }
+                    "tittel" -> {
+                        journaltittel = part.value
+                        part.dispose()
+                    }
+                    "adresse" -> {
+                        val distribueringsadresseAsJson = part.value
+                        part.dispose()
+                        distribueringsadresse = deserialize<DistribueringsadresseBody>(distribueringsadresseAsJson).toDomain()
+                    }
+                    else -> {
+                        throw IllegalArgumentException("Ikke riktig name i multipart ${part.name}")
+                    }
+                }
+            }
+            is PartData.FileItem -> {
+                pdfContent = part.provider().readRemaining().readByteArray()
+                require(pdfContent.isNotEmpty()) { "Pdf innhold må ha størrelse, denne var 0 bytes" }
+                val minimumSizeForPdfv1 = 311
+                require(pdfContent.size > minimumSizeForPdfv1) { "Pdf er minimum 312 bytes, denne var ${pdfContent.size} bytes" }
+                part.dispose()
+            }
+
+            else -> {
+                throw IllegalStateException("Støtter kun vanlig verdi og pdf fil")
             }
         }
-
-    val pdfContent: ByteArray = (parts[2] as PartData.FileItem).let { partdata ->
-        partdata.provider().readRemaining().readByteArray().also {
-            partdata.dispose
-        }
-    }
-    require(pdfContent.isNotEmpty()) { "Pdf innhold må ha størrelse, denne var 0 bytes" }
-    val minimumSizeForPdfv1 = 311
-    require(pdfContent.size > minimumSizeForPdfv1) { "Pdf er minimum 312 bytes, denne var ${pdfContent.size} bytes" }
-    val distribueringsadresse: Distribueringsadresse? = parts.getOrNull(3)?.let {
-        val partdata = it as PartData.FormItem
-        val distribueringsadresseAsJson = partdata.value
-        partdata.dispose
-        deserialize<DistribueringsadresseBody>(distribueringsadresseAsJson).toDomain()
     }
 
     return JournalførOgSendOpplastetPdfSomBrevCommand(
         sakId = sakId,
         saksbehandler = this.suUserContext.saksbehandler,
-        journaltittel = journaltittel,
-        pdf = PdfA(content = pdfContent),
+        journaltittel = journaltittel ?: throw IllegalStateException("Mangler journaltittel"),
+        pdf = PdfA(content = pdfContent ?: throw IllegalStateException("Mangler Pdf innholt")),
         distribueringsadresse = distribueringsadresse,
-        distribusjonstype = distribusjonstype,
+        distribusjonstype = distribusjonstype ?: throw IllegalStateException("Mangler distribusjonstype"),
     )
-}
-
-// Kopiert fra ktor 3.1.2 - io.ktor.http.content
-// TODO jah: Bør skrive om dette til forEachPart, men kan kanskje være en idé og se på requesten fra frontend samtidig.
-private suspend fun MultiPartData.readAllParts(): List<PartData> {
-    var part = readPart() ?: return emptyList()
-    val parts = ArrayList<PartData>()
-    parts.add(part)
-
-    do {
-        part = readPart() ?: break
-        parts.add(part)
-    } while (true)
-
-    return parts
 }
