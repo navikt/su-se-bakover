49805862456f8469e63edca4c47ad8986ce8dee6
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2025-08-27 14:41:17 +0200

    Oppsett for overføring av stønadstatistikk til bigquery

---
+++ datapakker/stoenadstatistikk/src/main/kotlin/no/nav/su/se/bakover/datapakker/stoenadstatistikk/App.kt
@@ -0,0 +1,73 @@
+package no.nav.su.se.bakover.datapakker.stoenadstatistikk
+
+import com.google.auth.oauth2.GoogleCredentials
+import com.google.cloud.bigquery.BigQueryOptions
+import com.google.cloud.bigquery.FormatOptions
+import com.google.cloud.bigquery.JobId
+import com.google.cloud.bigquery.JobStatistics
+import com.google.cloud.bigquery.TableId
+import com.google.cloud.bigquery.WriteChannelConfiguration
+import no.nav.su.se.bakover.database.Postgres
+import no.nav.su.se.bakover.database.VaultPostgres
+import org.slf4j.LoggerFactory
+import java.io.File
+import java.io.FileInputStream
+import java.io.InputStream
+import java.nio.channels.Channels
+import java.util.UUID
+
+private val logger = LoggerFactory.getLogger("Stønadstatistikk")
+private const val LOCATION = "europe-north1"
+
+fun main() {
+    logger.info("Starter opp Stønadstatistikk")
+    val databaseUrl = System.getenv("DATABASE_JDBC_URL")
+    val datasource = VaultPostgres(
+        jdbcUrl = databaseUrl,
+        vaultMountPath = System.getenv("VAULT_MOUNTPATH"),
+        databaseName = System.getenv("DATABASE_NAME"),
+    ).getDatasource(Postgres.Role.ReadOnly).let {
+        logger.info("Startet database med url: $it")
+    }
+    /*
+        Her kommer koden for å hente data fra databasen, transformere den og laste den opp til BigQuery
+        mangler
+        writeToBigQuery(datasource.getStatistikk())
+     */
+    writeToBigQuery()
+    logger.info("Slutter jobb Stønadstatistikk")
+}
+
+fun writeToBigQuery(
+    data: String = "",
+) {
+    val jsonKey: InputStream = FileInputStream(File("/var/run/secrets/nais.io/vault/bigquery"))
+    val project: String = System.getenv("GCP_PROJECT")
+
+    val dataset = "statistikk"
+    val table = "stoendadstatistikk"
+
+    val credentials = GoogleCredentials.fromStream(jsonKey)
+    val bq = BigQueryOptions
+        .newBuilder()
+        .setCredentials(credentials)
+        .setLocation(LOCATION)
+        .setProjectId(project)
+        .build().service
+
+    val jobId = JobId.newBuilder().setLocation(LOCATION).setJob(UUID.randomUUID().toString()).build()
+    val configuration = WriteChannelConfiguration.newBuilder(
+        TableId.of(project, dataset, table),
+    ).setFormatOptions(FormatOptions.csv()).build()
+
+    val job = bq.writer(jobId, configuration).let {
+        it.use { channel ->
+            Channels.newOutputStream(channel).use { os ->
+                os.write("".toByteArray()) // TODO: her må vi ha dataen fra vår tabell
+            }
+        }
+        it.job.waitFor()
+    }
+
+    logger.info("Stønadstatistikkjob: ${job.getStatistics<JobStatistics.LoadStatistics>()}")
+}
