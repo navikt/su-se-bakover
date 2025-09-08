83f3760cb9024b07815e84309e8319d45913f004
Sebastian Oliver Søberg <82504565+sebassonav@users.noreply.github.com>
2025-09-08 11:50:34 +0200

    Bigquery job for å overføre data for saksstatistikk (#2176)
    * Bigquery job for å overføre data for saksstatistikk
    
    * Caching
    
    * Hver halvtime

---
+++ datapakker/saksstatistikk/build.gradle.kts
@@ -0,0 +1,26 @@
+tasks.named<Jar>("jar") {
+    archiveBaseName.set("app")
+    archiveVersion.set("")
+    manifest {
+        attributes["Main-Class"] = "no.nav.su.se.bakover.datapakker.saksstatistikk.AppKt"
+        attributes["Class-Path"] =
+            configurations.runtimeClasspath.get().joinToString(separator = " ") {
+                it.name
+            }
+    }
+    doLast {
+        configurations.runtimeClasspath.get().forEach {
+            val fileProvider: Provider<RegularFile> = layout.buildDirectory.file("libs/${it.name}")
+            val targetFile = File(fileProvider.get().toString())
+            if (!targetFile.exists()) {
+                it.copyTo(targetFile)
+            }
+        }
+    }
+}
+
+dependencies {
+    implementation(platform(rootProject.libs.google.cloud.bom))
+    implementation(rootProject.libs.google.cloud.bigquery)
+    implementation(project(":common:infrastructure"))
+}
