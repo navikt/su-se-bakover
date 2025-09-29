97452bafa7d07f6a423b7f9ba05b3ca6facaaacb
Sebastian Oliver Søberg <82504565+sebassonav@users.noreply.github.com>
2025-09-29 14:54:49 +0200

    Riktig validering på omgjøringstilknytning ved klage (#2221)
    * Riktig validering på omgjøringstilknytning ved klage
    
    * Mer riktig feilmelding
    
    * Riktig feilmelding til sb og

--- web/src/main/kotlin/no/nav/su/se/bakover/web/routes/vedtak/VedtakRoutes.kt
+++ web/src/main/kotlin/no/nav/su/se/bakover/web/routes/vedtak/VedtakRoutes.kt
@@ -76,9 +76,9 @@ internal fun KunneIkkeStarteNySøknadsbehandling.tilResultat(): Resultat = when 
         "Klage er allerede knyttet til en behandling",
         "klage_er_allerede_knyttet_til_behandling",
     )
-    is KunneIkkeStarteNySøknadsbehandling.KlageErIkkeOversendt -> HttpStatusCode.BadRequest.errorJson(
-        "Klagen er ikke oversendt",
-        "klage_er_ikke_oversendt",
+    is KunneIkkeStarteNySøknadsbehandling.KlageErIkkeFerdigstilt -> HttpStatusCode.BadRequest.errorJson(
+        "Klagen er ikke ferdigstilt",
+        "klage_er_ikke_ferdigstilt",
     )
     is KunneIkkeStarteNySøknadsbehandling.KlageMåFinnesForKnytning -> HttpStatusCode.BadRequest.errorJson(
         "Klagen finnes ikke",
