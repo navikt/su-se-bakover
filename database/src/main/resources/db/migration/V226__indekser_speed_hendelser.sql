197a9b1a9e07f3f4d2d30120dd5bc112b73aca9e
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2025-10-06 23:11:01 +0200

    Unngå duplikater indeks making

--- database/src/main/resources/db/migration/V226__indekser_speed_hendelser.sql
+++ database/src/main/resources/db/migration/V226__indekser_speed_hendelser.sql
@@ -1,17 +1,18 @@
-CREATE INDEX ON hendelse ((data->'kravgrunnlag'->>'behandlingsId'));
+CREATE INDEX IF NOT EXISTS idx_hendelse_behandlingsid
+    ON hendelse ((data->'kravgrunnlag'->>'behandlingsId'));
 
-CREATE INDEX idx_hendelse_ekstern_tidspunkt_text
+CREATE INDEX IF NOT EXISTS idx_hendelse_ekstern_tidspunkt_text
     ON hendelse ((data->'kravgrunnlag'->>'eksternTidspunkt'));
 
-CREATE INDEX idx_hendelse_ekstern_vedtak_id
+CREATE INDEX IF NOT EXISTS idx_hendelse_ekstern_vedtak_id
     ON hendelse ((data->'kravgrunnlag'->>'eksternVedtakId'));
 
-CREATE INDEX idx_hendelse_type_kravgrunnlag
+CREATE INDEX IF NOT EXISTS idx_hendelse_type_kravgrunnlag
     ON hendelse (type)
     WHERE data ?? 'kravgrunnlag';
 
 
-CREATE INDEX idx_hendelse_kravgrunnlag_vedtak_tidspunkt
+CREATE INDEX IF NOT EXISTS idx_hendelse_kravgrunnlag_vedtak_tidspunkt
     ON hendelse (
                  (data->'kravgrunnlag'->>'eksternVedtakId'),
                  ((data->'kravgrunnlag'->>'eksternTidspunkt'))
@@ -19,10 +20,10 @@ CREATE INDEX idx_hendelse_kravgrunnlag_vedtak_tidspunkt
     WHERE type = 'KNYTTET_KRAVGRUNNLAG_TIL_SAK'
         AND data ? 'kravgrunnlag';
 
-CREATE INDEX idx_hendelse_type_status ON hendelse(type) WHERE data ? 'status';
-CREATE INDEX idx_hendelse_eksternVedtakId ON hendelse((data->>'eksternVedtakId'));
-CREATE INDEX idx_hendelse_eksternTidspunkt ON hendelse(((data->>'eksternTidspunkt')));
-CREATE INDEX idx_hendelse_behandlingsId_versjon ON hendelse ((data->>'behandlingsId'), versjon);
-CREATE INDEX idx_kravgrunnlag_status ON hendelse (((data -> 'kravgrunnlag' ->> 'status')));
+CREATE INDEX IF NOT EXISTS idx_hendelse_type_status ON hendelse(type) WHERE data ? 'status';
+CREATE INDEX IF NOT EXISTS idx_hendelse_eksternVedtakId ON hendelse((data->>'eksternVedtakId'));
+CREATE INDEX IF NOT EXISTS idx_hendelse_eksternTidspunkt ON hendelse(((data->>'eksternTidspunkt')));
+CREATE INDEX IF NOT EXISTS idx_hendelse_behandlingsId_versjon ON hendelse ((data->>'behandlingsId'), versjon);
+CREATE INDEX IF NOT EXISTS idx_kravgrunnlag_status ON hendelse (((data -> 'kravgrunnlag' ->> 'status')));
 
 -- Disse optimaliseringene gjør henting fra hendelser er mulig uten timeout. Avg før var 32s nå er det 320 ms
