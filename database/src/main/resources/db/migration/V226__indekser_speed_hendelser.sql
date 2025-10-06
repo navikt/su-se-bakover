4ec9df207ad78f63adabf51f8f77308543776c98
Sebastian Oliver Søberg <82504565+sebassonav@users.noreply.github.com>
2025-10-06 23:37:58 +0200

    Fjern for like indekser (#2256)
    * Fjern for like indekser
    
    * Nitpick

--- database/src/main/resources/db/migration/V226__indekser_speed_hendelser.sql
+++ database/src/main/resources/db/migration/V226__indekser_speed_hendelser.sql
@@ -9,7 +9,7 @@ CREATE INDEX IF NOT EXISTS idx_hendelse_ekstern_vedtak_id
 
 CREATE INDEX IF NOT EXISTS idx_hendelse_type_kravgrunnlag
     ON hendelse (type)
-    WHERE data ?? 'kravgrunnlag';
+    WHERE data ? 'kravgrunnlag';
 
 
 CREATE INDEX IF NOT EXISTS idx_hendelse_kravgrunnlag_vedtak_tidspunkt
@@ -22,8 +22,6 @@ CREATE INDEX IF NOT EXISTS idx_hendelse_kravgrunnlag_vedtak_tidspunkt
 
 CREATE INDEX IF NOT EXISTS idx_hendelse_type_status ON hendelse(type) WHERE data ? 'status';
 CREATE INDEX IF NOT EXISTS idx_hendelse_eksternVedtakId ON hendelse((data->>'eksternVedtakId'));
-CREATE INDEX IF NOT EXISTS idx_hendelse_eksternTidspunkt ON hendelse(((data->>'eksternTidspunkt')));
-CREATE INDEX IF NOT EXISTS idx_hendelse_behandlingsId_versjon ON hendelse ((data->>'behandlingsId'), versjon);
 CREATE INDEX IF NOT EXISTS idx_kravgrunnlag_status ON hendelse (((data -> 'kravgrunnlag' ->> 'status')));
 
 -- Disse optimaliseringene gjør henting fra hendelser er mulig uten timeout. Avg før var 32s nå er det 320 ms
