68b72012bfc03f76923915288ca00518ff74961c
sevdeoguz <sevde.oguz@nav.no>
2025-10-22 13:38:41 +0200

    Lagring av brev redigerings tekst for søknadsbehandling (#2283)
    * Lagring av brev redigerings tekst for søknadsbehandling
    
    * Fjernet revurdering. Endret fritekstTilBrev til fritekstEndringAttestering, og gjort nullable
    
    * Avslag fritekst funker nå
    
    * fix tests
    
    * small fix
    
    * small fix
    
    * fjerne sakid

--- domain/src/main/kotlin/no/nav/su/se/bakover/domain/søknadsbehandling/iverksett/IverksettSøknadsbehandlingCommand.kt
+++ domain/src/main/kotlin/no/nav/su/se/bakover/domain/søknadsbehandling/iverksett/IverksettSøknadsbehandlingCommand.kt
@@ -9,6 +9,7 @@ import no.nav.su.se.bakover.domain.søknadsbehandling.SøknadsbehandlingId
 data class IverksettSøknadsbehandlingCommand(
     val behandlingId: SøknadsbehandlingId,
     val attestering: Attestering.Iverksatt,
+    val fritekstEndringAttestering: String?,
     // TODO jah: Fjern etter vi flytter avslagManglendeDokumentasjon inn i den normale saksbehandlingsflyten.
     val saksbehandlerOgAttestantKanIkkeVæreDenSamme: Boolean = true,
 )
