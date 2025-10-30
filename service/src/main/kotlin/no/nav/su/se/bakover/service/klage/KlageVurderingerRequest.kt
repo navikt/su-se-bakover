87fdb4650ed382c326ce52cda3499b6b95f3eb4a
Sebastian Oliver Søberg <82504565+sebassonav@users.noreply.github.com>
2025-10-30 09:54:13 +0100

    Fiks validering klage hjemler (#2310)

--- service/src/main/kotlin/no/nav/su/se/bakover/service/klage/KlageVurderingerRequest.kt
+++ service/src/main/kotlin/no/nav/su/se/bakover/service/klage/KlageVurderingerRequest.kt
@@ -51,23 +51,9 @@ data class KlageVurderingerRequest(
         }
 
         private fun hjemmelToDomain(hjemler: List<String>): Either<KunneIkkeVurdereKlage.UgyldigOpprettholdelseshjemler, Klagehjemler> {
-            return hjemler.map { hjemmel ->
-                when (hjemmel) {
-                    "SU_PARAGRAF_3" -> Hjemmel.SU_PARAGRAF_3
-                    "SU_PARAGRAF_4" -> Hjemmel.SU_PARAGRAF_4
-                    "SU_PARAGRAF_5" -> Hjemmel.SU_PARAGRAF_5
-                    "SU_PARAGRAF_6" -> Hjemmel.SU_PARAGRAF_6
-                    "SU_PARAGRAF_7" -> Hjemmel.SU_PARAGRAF_7
-                    "SU_PARAGRAF_8" -> Hjemmel.SU_PARAGRAF_8
-                    "SU_PARAGRAF_9" -> Hjemmel.SU_PARAGRAF_9
-                    "SU_PARAGRAF_10" -> Hjemmel.SU_PARAGRAF_10
-                    "SU_PARAGRAF_11" -> Hjemmel.SU_PARAGRAF_11
-                    "SU_PARAGRAF_12" -> Hjemmel.SU_PARAGRAF_12
-                    "SU_PARAGRAF_13" -> Hjemmel.SU_PARAGRAF_13
-                    "SU_PARAGRAF_17" -> Hjemmel.SU_PARAGRAF_17
-                    "SU_PARAGRAF_18" -> Hjemmel.SU_PARAGRAF_18
-                    "SU_PARAGRAF_21" -> Hjemmel.SU_PARAGRAF_21
-                    else -> return KunneIkkeVurdereKlage.UgyldigOpprettholdelseshjemler.left()
+            return hjemler.map { hjemmelStr ->
+                runCatching { enumValueOf<Hjemmel>(hjemmelStr) }.getOrElse {
+                    return KunneIkkeVurdereKlage.UgyldigOpprettholdelseshjemler.left()
                 }
             }.let {
                 if (it.isEmpty()) return Klagehjemler.empty().right()
--- service/src/test/kotlin/no/nav/su/se/bakover/service/klage/KlageVurderingerRequestTest.kt
+++ service/src/test/kotlin/no/nav/su/se/bakover/service/klage/KlageVurderingerRequestTest.kt
@@ -1,5 +1,6 @@
 package no.nav.su.se.bakover.service.klage
 
+import behandling.klage.domain.Hjemmel
 import io.kotest.assertions.arrow.core.shouldBeLeft
 import io.kotest.assertions.arrow.core.shouldBeRight
 import org.junit.jupiter.api.Test
@@ -8,22 +9,10 @@ internal class KlageVurderingerRequestTest {
 
     @Test
     fun `gyldige hjemler`() {
+        val allHjemmelStrings = Hjemmel.entries.map { it.name }
+
         KlageVurderingerRequest.Oppretthold(
-            listOf(
-                "SU_PARAGRAF_3",
-                "SU_PARAGRAF_4",
-                "SU_PARAGRAF_5",
-                "SU_PARAGRAF_6",
-                "SU_PARAGRAF_8",
-                "SU_PARAGRAF_9",
-                "SU_PARAGRAF_10",
-                "SU_PARAGRAF_11",
-                "SU_PARAGRAF_12",
-                "SU_PARAGRAF_13",
-                "SU_PARAGRAF_17",
-                "SU_PARAGRAF_18",
-                "SU_PARAGRAF_21",
-            ),
+            hjemler = allHjemmelStrings,
             klagenotat = "klagenotat",
         ).toDomain().shouldBeRight()
     }
