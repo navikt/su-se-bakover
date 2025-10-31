06b1c8828357a95e15756c4c7c9b1b3a38742018
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2025-10-31 09:01:05 +0100

    Logg info

--- domain/src/main/kotlin/no/nav/su/se/bakover/domain/vedtak/brev/MapVedtakSomKanRevurderesTilDokumentCommand.kt
+++ domain/src/main/kotlin/no/nav/su/se/bakover/domain/vedtak/brev/MapVedtakSomKanRevurderesTilDokumentCommand.kt
@@ -14,13 +14,12 @@ import satser.domain.SatsFactory
 import vedtak.domain.VedtakSomKanRevurderes
 import java.time.Clock
 
-// TODO jah: Bør heller lage en abstract function på VedtakSomKanRevurderes. Da slipper vi when-else
 fun VedtakSomKanRevurderes.lagDokumentKommando(
     clock: Clock,
     satsFactory: SatsFactory,
 ): GenererDokumentCommand {
     return when (this) {
-        is VedtakOpphørUtenUtbetaling -> throw IllegalStateException("Historisk vedtak. Skal ikke sende nye brev for VedtakOpphørUtenUtbetaling.")
+        is VedtakOpphørUtenUtbetaling -> fail("Historisk vedtak. Skal ikke sende nye brev for VedtakOpphørUtenUtbetaling.")
 
         is VedtakOpphørMedUtbetaling -> this.behandling.lagDokumentKommando(
             clock = clock,
@@ -32,8 +31,9 @@ fun VedtakSomKanRevurderes.lagDokumentKommando(
              * Vi har ikke et så godt skille mellom det som er forhåndsvisning av vedtaket, og det som faktisk er utsendingen.
              * Ved forhåndsvisning krever vi ikke noe fritekst, men vi gjør det når brevet skal sendes ut.
              */
-            require(this.behandling.brevvalgRevurdering is BrevvalgRevurdering.Valgt.SendBrev && this.behandling.brevvalgRevurdering.fritekst != null) {
-                "Generering av dokument for vedtaket forutsetter at vi skal sende brev, og at friteksten er satt"
+            val brevvalg = this.behandling.brevvalgRevurdering
+            if (brevvalg !is BrevvalgRevurdering.Valgt.SendBrev || brevvalg.fritekst == null) {
+                fail("Generering av dokument for vedtaket forutsetter at vi skal sende brev, og at friteksten er satt.")
             }
             this.behandling.lagDokumentKommando(
                 satsFactory = satsFactory,
@@ -45,9 +45,14 @@ fun VedtakSomKanRevurderes.lagDokumentKommando(
             satsFactory = satsFactory,
         )
 
-        is VedtakGjenopptakAvYtelse -> throw IllegalStateException("Skal ikke sende brev for VedtakGjenopptakAvYtelse")
-        is VedtakInnvilgetRegulering -> throw IllegalStateException("Skal ikke sende brev for VedtakInnvilgetRegulering")
-        is VedtakStansAvYtelse -> throw IllegalStateException("Skal ikke sende brev for VedtakStansAvYtelse")
-        else -> throw java.lang.IllegalStateException("Ikke tatt høyde for ${this::class.simpleName} ved generering av dokumentkommando")
+        is VedtakGjenopptakAvYtelse -> fail("Skal ikke sende brev for VedtakGjenopptakAvYtelse")
+        is VedtakInnvilgetRegulering -> fail("Skal ikke sende brev for VedtakInnvilgetRegulering")
+        is VedtakStansAvYtelse -> fail("Skal ikke sende brev for VedtakStansAvYtelse")
+        else -> fail("Ikke tatt høyde for ${this::class.simpleName} ved generering av dokumentkommando")
     }
+}
+
+internal fun VedtakSomKanRevurderes.fail(message: String): Nothing {
+    val contextMessage = "Vedtakid: ${this.id}, Sakid: ${this.sakId}. $message"
+    throw IllegalStateException(contextMessage)
 }
