443fe21d66ff7b049f123b1b4f565efb70fff7a2
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2025-11-18 13:31:11 +0100

    Bedre migrering

--- database/src/main/resources/db/migration/V235__versjoner_klage.sql
+++ database/src/main/resources/db/migration/V235__versjoner_klage.sql
@@ -4,10 +4,6 @@ update klage set versjon = 1 where type in
                                    ('vilkårsvurdert_bekreftet_til_vurdering',
                                     'vilkårsvurdert_utfylt_til_vurdering', 'vilkårsvurdert_utfylt_avvist', 'vilkårsvurdert_bekreftet_avvist', 'vurdert_påbegynt', 'vurdert_utfylt',
                                    'vurdert_bekreftet', 'til_attestering_til_vurdering', 'til_attestering_avvist', 'oversendt', 'omgjort')
-AND fremsattrettsligklageinteresse is NULL;
-
-UPDATE klage
-SET versjon = 1
-WHERE opprettet < '2025-10-24' AND fremsattrettsligklageinteresse is NULL;
+AND fremsattrettsligklageinteresse is NULL AND opprettet < '2025-10-24';
 -- Da fremsattrettsligklageinteresse ble innført + 1 dag. Kan selvfølgelig være saksbehandlere som ikke refresha siden
 -- og lagret ned med gammel versjon men da får vi bare sette den versjonen til 1.
