1f3dccb808b2a18c59b4b33be9c2b9c92b2e20d0
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2025-11-18 13:34:10 +0100

    Format

--- database/src/main/resources/db/migration/V235__versjoner_klage.sql
+++ database/src/main/resources/db/migration/V235__versjoner_klage.sql
@@ -1,9 +1,19 @@
 ALTER TABLE klage ADD COLUMN versjon INT NOT NULL DEFAULT 2;
 
-update klage set versjon = 1 where type in
-                                   ('vilkårsvurdert_bekreftet_til_vurdering',
-                                    'vilkårsvurdert_utfylt_til_vurdering', 'vilkårsvurdert_utfylt_avvist', 'vilkårsvurdert_bekreftet_avvist', 'vurdert_påbegynt', 'vurdert_utfylt',
-                                   'vurdert_bekreftet', 'til_attestering_til_vurdering', 'til_attestering_avvist', 'oversendt', 'omgjort')
+update klage set versjon = 1
+WHERE type IN (
+               'vilkårsvurdert_bekreftet_til_vurdering',
+               'vilkårsvurdert_utfylt_til_vurdering',
+               'vilkårsvurdert_utfylt_avvist',
+               'vilkårsvurdert_bekreftet_avvist',
+               'vurdert_påbegynt',
+               'vurdert_utfylt',
+               'vurdert_bekreftet',
+               'til_attestering_til_vurdering',
+               'til_attestering_avvist',
+               'oversendt',
+               'omgjort'
+    )
 AND fremsattrettsligklageinteresse is NULL AND opprettet < '2025-10-24';
 -- Da fremsattrettsligklageinteresse ble innført + 1 dag. Kan selvfølgelig være saksbehandlere som ikke refresha siden
 -- og lagret ned med gammel versjon men da får vi bare sette den versjonen til 1.
