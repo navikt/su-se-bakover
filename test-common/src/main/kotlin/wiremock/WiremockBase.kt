25c2924462622bdc55b29f5f2de2ef9de56ca3e4
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2025-11-19 09:10:08 +0100

    Fiks test

--- test-common/src/main/kotlin/wiremock/WiremockBase.kt
+++ test-common/src/main/kotlin/wiremock/WiremockBase.kt
@@ -2,6 +2,9 @@ package no.nav.su.se.bakover.test.wiremock
 
 import com.github.tomakehurst.wiremock.WireMockServer
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration
+import kotlinx.coroutines.asContextElement
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 import no.nav.su.se.bakover.common.domain.auth.Kontekst
 import no.nav.su.se.bakover.common.domain.auth.TokenContext
 import no.nav.su.se.bakover.common.infrastructure.mdc.putInMdcIfMissing
@@ -10,14 +13,18 @@ import no.nav.su.se.bakover.common.infrastructure.mdc.putInMdcIfMissing
  * Starts a new WireMock server on a random port and sets the MDC variable 'X-Correlation-ID' to "correlationId".
  */
 fun startedWireMockServerWithCorrelationId(token: String = "Bearer token", block: WireMockServer.() -> Unit) {
-    WireMockServer(WireMockConfiguration.options().dynamicPort()).also { server ->
+    runBlocking {
+        val server = WireMockServer(WireMockConfiguration.options().dynamicPort())
         server.start()
         putInMdcIfMissing("X-Correlation-ID", "correlationId")
-        Kontekst.set(TokenContext(token)) // temporary, remove after block
+
+        val tokenContextElement = Kontekst.asContextElement(TokenContext(token))
+
         try {
-            block(server)
+            withContext(tokenContextElement) {
+                block(server)
+            }
         } finally {
-            Kontekst.remove()
             server.stop()
         }
     }
