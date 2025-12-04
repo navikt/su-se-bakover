5db76c4b4175733bc6e776fa8c160311989f6a4a
Sebastian Oliver Søberg <82504565+sebassonav@users.noreply.github.com>
2025-12-04 10:24:33 +0100

    Fiks 1 connection per job (#2387)

--- datapakker/stoenadstatistikk/src/main/kotlin/no/nav/su/se/bakover/datapakker/stoenadstatistikk/App.kt
+++ datapakker/stoenadstatistikk/src/main/kotlin/no/nav/su/se/bakover/datapakker/stoenadstatistikk/App.kt
@@ -31,6 +31,7 @@ fun main() {
         jdbcUrl = databaseUrl,
         vaultMountPath = System.getenv("VAULT_MOUNTPATH"),
         databaseName = System.getenv("DATABASE_NAME"),
+        maximumPoolSize = 1,
     ).getDatasource(Postgres.Role.ReadOnly).let {
         logger.info("Startet database med url: $databaseUrl")
         hentData(it, YearMonth.now().minusMonths(1))
