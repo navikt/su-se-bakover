c9da0b6af5218cf6c8b56fd57daad19b7fc4e632
Bjorn Golberg <bjorn.golberg@gmail.com>
2025-12-11 12:09:40 +0100

    Legger til sql sa hvis ikke finnes

--- database/src/main/resources/db/migration/V238__skrivetilgang_migrering_sa.sql
+++ database/src/main/resources/db/migration/V238__skrivetilgang_migrering_sa.sql
@@ -1,2 +1,11 @@
+DO $$
+    BEGIN
+        IF NOT EXISTS (
+            SELECT FROM pg_catalog.pg_roles WHERE rolname = 'cloudsqliamserviceaccount'
+        ) THEN
+            CREATE ROLE cloudsqliamserviceaccount LOGIN;
+        END IF;
+    END
+$$;
 GRANT USAGE on SCHEMA public to "cloudsqliamserviceaccount";
 GRANT INSERT ON ALL TABLES IN SCHEMA public TO "cloudsqliamserviceaccount";
