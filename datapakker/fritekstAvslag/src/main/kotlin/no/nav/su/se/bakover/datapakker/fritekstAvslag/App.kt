b5bc2836f4348ddc81dd0a43773b0c5a1ce8060d
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2025-12-15 08:44:08 +0100

    Sett siste til 1

--- datapakker/fritekstAvslag/src/main/kotlin/no/nav/su/se/bakover/datapakker/fritekstAvslag/App.kt
+++ datapakker/fritekstAvslag/src/main/kotlin/no/nav/su/se/bakover/datapakker/fritekstAvslag/App.kt
@@ -29,7 +29,7 @@ fun main() {
         jdbcUrl = databaseUrl,
         vaultMountPath = System.getenv("VAULT_MOUNTPATH"),
         databaseName = System.getenv("DATABASE_NAME"),
-        maximumPoolSize = 3,
+        maximumPoolSize = 1,
     ).getDatasource(Postgres.Role.ReadOnly).let {
         logger.info("Startet database med url: $databaseUrl")
         it.use { hentAntallAvslagsvedtakUtenFritekst(it) }
