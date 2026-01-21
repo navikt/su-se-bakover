bf3d6a98c85c3c1e7f22c4466dcd79c8f0a8ee43
Bjorn Golberg <bjorn.golberg@gmail.com>
2026-01-21 14:07:39 +0100

    Fjerner unødvendig sjekk på brevvalg/fritekst ved vedtaksbrevgenerering

--- domain/src/main/kotlin/no/nav/su/se/bakover/domain/vedtak/brev/MapVedtakSomKanRevurderesTilDokumentCommand.kt
+++ domain/src/main/kotlin/no/nav/su/se/bakover/domain/vedtak/brev/MapVedtakSomKanRevurderesTilDokumentCommand.kt
@@ -28,15 +28,6 @@ fun VedtakSomKanRevurderes.lagDokumentKommando(
         )
 
         is VedtakInnvilgetRevurdering -> {
-            /**
-             * Vi har ikke et så godt skille mellom det som er forhåndsvisning av vedtaket, og det som faktisk er utsendingen.
-             * Ved forhåndsvisning krever vi ikke noe fritekst, men vi gjør det når brevet skal sendes ut.
-             * Vi krever at fritekst ved revurdering er satt dersom BrevvalgRevurdering.Valgt.SendBrev er valgt før attestering for å løse dette [KunneIkkeSendeRevurderingTilAttestering.ManglerFritekstTilVedtaksbrev]
-             */
-            val brevvalg = this.behandling.brevvalgRevurdering
-            if (!brevvalg.kanSendesTilAttestering()) {
-                fail("Generering av dokument for vedtaket forutsetter at vi skal sende brev, og at friteksten er satt.")
-            }
             this.behandling.lagDokumentKommando(
                 satsFactory = satsFactory,
                 clock = clock,
