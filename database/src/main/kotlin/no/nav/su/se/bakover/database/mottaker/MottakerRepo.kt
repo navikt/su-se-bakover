4291909191e282f42c755b8fad0ff0ba2aa870b5
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2026-01-27 10:08:24 +0100

    Nitpick

--- database/src/main/kotlin/no/nav/su/se/bakover/database/mottaker/MottakerRepo.kt
+++ database/src/main/kotlin/no/nav/su/se/bakover/database/mottaker/MottakerRepo.kt
@@ -10,7 +10,7 @@ import no.nav.su.se.bakover.common.infrastructure.persistence.oppdatering
 import no.nav.su.se.bakover.common.persistence.SessionContext
 import no.nav.su.se.bakover.common.person.Fnr
 import no.nav.su.se.bakover.common.serialize
-import no.nav.su.se.bakover.domain.mottaker.Mottaker
+import no.nav.su.se.bakover.domain.mottaker.MottakerDomain
 import no.nav.su.se.bakover.domain.mottaker.MottakerRepo
 import java.util.UUID
 
@@ -18,7 +18,7 @@ data class MottakerRepo(
     private val sessionFactory: PostgresSessionFactory,
     private val dbMetrics: DbMetrics,
 ) : MottakerRepo {
-    fun hentMottaker(dokumentId: UUID, sessionContext: SessionContext? = null): Mottaker? =
+    override fun hentMottaker(dokumentId: UUID, sessionContext: SessionContext?): MottakerDomain? =
         dbMetrics.timeQuery("hentMottaker") {
             sessionFactory.withSession(sessionContext) { session ->
                 """
@@ -30,23 +30,21 @@ data class MottakerRepo(
             }
         }
 
-    fun lagreMottaker(mottaker: Mottaker, dokumentId: UUID) {
+    override fun lagreMottaker(mottaker: MottakerDomain, dokumentId: UUID) {
         dbMetrics.timeQuery("lagreMottaker") {
             sessionFactory.withSession { session ->
                 """
-                    insert into mottaker (id, navn, foedselsnummer, orgnummer, adresse, dokument_id)
-                    values (:id, :navn, :foedselsnummer, :orgnummer, :adresse::jsonb, :dokument_id)
+                    insert into mottaker (id, navn, foedselsnummer, adresse, dokument_id)
+                    values (:id, :navn, :foedselsnummer, :adresse::jsonb, :dokument_id)
                     on conflict (dokument_id) do update
                     set navn = :navn,
                         foedselsnummer = :foedselsnummer,
-                        orgnummer = :orgnummer,
                         adresse = :adresse::jsonb
                 """.trimIndent().insert(
                     mapOf(
                         "id" to mottaker.id,
                         "navn" to mottaker.navn,
                         "foedselsnummer" to mottaker.foedselsnummer,
-                        "orgnummer" to mottaker.orgnummer?.takeIf { it.isNotBlank() },
                         "adresse" to serialize(mottaker.adresse),
                         "dokument_id" to dokumentId,
                     ),
@@ -56,14 +54,13 @@ data class MottakerRepo(
         }
     }
 
-    fun oppdaterMottaker(mottaker: Mottaker, dokumentId: UUID) {
+    override fun oppdaterMottaker(mottaker: MottakerDomain, dokumentId: UUID) {
         dbMetrics.timeQuery("oppdaterMottaker") {
             sessionFactory.withSession { session ->
                 """
                     update mottaker
                     set navn = :navn,
                         foedselsnummer = :foedselsnummer,
-                        orgnummer = :orgnummer,
                         adresse = :adresse::jsonb
                     where id = :id and dokument_id = :dokument_id
                 """.trimIndent().oppdatering(
@@ -71,7 +68,6 @@ data class MottakerRepo(
                         "id" to mottaker.id,
                         "navn" to mottaker.navn,
                         "foedselsnummer" to mottaker.foedselsnummer.toString(),
-                        "orgnummer" to mottaker.orgnummer?.takeIf { it.isNotBlank() },
                         "adresse" to mottaker.adresse,
                         "dokument_id" to dokumentId,
                     ),
@@ -81,7 +77,7 @@ data class MottakerRepo(
         }
     }
 
-    fun slettMottaker(mottakerId: UUID, dokumentId: UUID) {
+    override fun slettMottaker(mottakerId: UUID, dokumentId: UUID) {
         dbMetrics.timeQuery("slettMottaker") {
             sessionFactory.withSession { session ->
                 """
@@ -98,11 +94,10 @@ data class MottakerRepo(
         }
     }
 
-    private fun rowToMottaker(row: Row) = Mottaker(
+    private fun rowToMottaker(row: Row) = MottakerDomain(
         id = row.uuid("id"),
         navn = row.string("navn"),
         foedselsnummer = row.stringOrNull("foedselsnummer")?.let { Fnr.tryCreate(it) }!!, // bad
-        orgnummer = row.stringOrNull("orgnummer"),
         adresse = deserialize(row.string("adresse")),
         dokumentId = row.uuid("dokumentId"),
     )
--- domain/src/main/kotlin/no/nav/su/se/bakover/domain/mottaker/MottakerService.kt
+++ domain/src/main/kotlin/no/nav/su/se/bakover/domain/mottaker/MottakerService.kt
@@ -1,33 +1,126 @@
 package no.nav.su.se.bakover.domain.mottaker
 
+import arrow.core.Either
+import arrow.core.getOrElse
+import arrow.core.left
+import arrow.core.right
 import no.nav.su.se.bakover.common.persistence.SessionContext
 import no.nav.su.se.bakover.common.person.Fnr
 import java.util.UUID
 
 interface MottakerRepo {
-    fun hentMottaker(dokumentId: UUID, sessionContext: SessionContext? = null): Mottaker?
-    fun lagreMottaker(mottaker: Mottaker, dokumentId: UUID)
-    fun oppdaterMottaker(mottaker: Mottaker, dokumentId: UUID)
+    fun hentMottaker(dokumentId: UUID, sessionContext: SessionContext? = null): MottakerDomain?
+    fun lagreMottaker(mottaker: MottakerDomain, dokumentId: UUID)
+    fun oppdaterMottaker(mottaker: MottakerDomain, dokumentId: UUID)
     fun slettMottaker(mottakerId: UUID, dokumentId: UUID)
 }
 
-class MottakerService
+class MottakerService(
+    private val mottakerRepo: MottakerRepo,
+) {
+
+    fun hentMottaker(
+        dokumentId: UUID,
+        sessionContext: SessionContext? = null,
+    ): MottakerDomain? =
+        mottakerRepo.hentMottaker(dokumentId, sessionContext)
+
+    fun lagreMottaker(
+        mottaker: Mottaker,
+        dokumentId: UUID,
+    ) {
+        val mottakerValidert = mottaker.toDomain().getOrElse { return }
+        mottakerRepo.lagreMottaker(mottakerValidert, dokumentId)
+    }
+
+    fun oppdaterMottaker(
+        mottaker: Mottaker,
+        dokumentId: UUID,
+    ) {
+        val mottakerValidert = mottaker.toDomain().getOrElse { return }
+
+        mottakerRepo.oppdaterMottaker(mottakerValidert, dokumentId)
+    }
+
+    fun slettMottaker(
+        mottakerId: UUID,
+        dokumentId: UUID,
+    ) {
+        mottakerRepo.slettMottaker(mottakerId, dokumentId)
+    }
+}
 
 data class Mottaker(
+    val id: String,
+    val navn: String,
+    val foedselsnummer: String,
+    val adresse: Adresse,
+    val dokumentId: String,
+) {
+    private fun erGyldig(): List<String> =
+        if (navn.isBlank()) {
+            listOf("Navn er blank")
+        } else if (foedselsnummer.isBlank()) {
+            listOf("Fødselsnummer er ikke angitt")
+        } else {
+            val erGyldig = adresse.erGyldig()
+            erGyldig
+        }
+
+    fun toDomain(): Either<List<String>, MottakerDomain> {
+        val erGyldig = this.erGyldig()
+        if (erGyldig.isEmpty()) {
+            return MottakerDomain(
+                id = UUID.fromString(id),
+                navn = navn,
+                foedselsnummer = Fnr(foedselsnummer),
+                adresse = adresse,
+                dokumentId = UUID.fromString(dokumentId),
+            ).right()
+        } else {
+            return erGyldig.left()
+        }
+    }
+}
+
+data class MottakerDomain(
     val id: UUID = UUID.randomUUID(),
     val navn: String,
     val foedselsnummer: Fnr,
-    val orgnummer: String? = null,
     val adresse: Adresse,
     val dokumentId: UUID,
 )
 
 data class Adresse(
-    val adresselinje1: String,
+    val adresseType: String, // TODO: må være PDL type?
+    val adresselinje1: String? = null,
     val adresselinje2: String? = null,
     val adresselinje3: String? = null,
-    val postnummer: String,
-    val poststed: String,
-    val landkode: String? = null,
-    val land: String? = null,
-)
+    val postnummer: String? = null,
+    val poststed: String? = null,
+    val landkode: String,
+    val land: String,
+) {
+    fun erGyldig(): List<String> =
+        if (adresseType.isBlank() || landkode.isBlank() || land.isBlank()) {
+            listOf(
+                "Adressetype ($adresseType), landkode ($landkode) eller land ($land) er blank. Sjekk om det er verge i saken. Da vet vi ikke hvor brevet skal.",
+            )
+        } else if (adresseType == "NORSKPOSTADRESSE") {
+            if (!(postnummer.isNullOrBlank() || poststed.isNullOrBlank())) {
+                emptyList()
+            } else {
+                listOf("Postnummer eller poststed er ikke angitt")
+            }
+        } else if (adresseType == "UTENLANDSKPOSTADRESSE") {
+            if (adresselinje1.isNullOrBlank()) {
+                listOf("Adresselinje1 er ikke angitt")
+            } else if (!postnummer.isNullOrBlank() || !poststed.isNullOrBlank()) {
+                listOf("Postnummer og poststed skal ikke brukes på utenlandsk adresse")
+            } else {
+                emptyList()
+            }
+        } else {
+            emptyList()
+        }
+}
