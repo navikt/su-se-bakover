83554eedf4a87cb7291464779f4e63b060465aeb
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2026-02-19 11:43:51 +0100

    Db migrering

--- database/src/main/resources/db/migration/V251__mottaker_brevtype.sql
+++ database/src/main/resources/db/migration/V251__mottaker_brevtype.sql
@@ -2,14 +2,12 @@ ALTER TABLE mottaker
     ADD COLUMN IF NOT EXISTS brevtype text;
 
 UPDATE mottaker
-SET brevtype = 'VEDTAK'
+SET brevtype = 'VEDTAKSBREV'
 WHERE brevtype IS NULL;
 
-ALTER TABLE dokument
-    ADD COLUMN IF NOT EXISTS brevtype text;
+ALTER TABLE mottaker
+    ALTER COLUMN brevtype SET DEFAULT 'VEDTAKSBREV',
+    ALTER COLUMN brevtype SET NOT NULL;
 
--- Vedtak kunne alltid identifiseres før denne kolonnen ble innført.
-UPDATE dokument
-SET brevtype = 'VEDTAK'
-WHERE brevtype IS NULL
-  AND type = 'VEDTAK';
+CREATE UNIQUE INDEX IF NOT EXISTS idx_mottaker_referanse_type_id_brevtype
+    ON mottaker (referanse_type, referanse_id, brevtype);
