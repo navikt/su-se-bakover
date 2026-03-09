6692b47dd5d9b0aaf8f6b80b90ab1011e435070b
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2026-03-09 09:51:54 +0100

    Gjør det i samme migreringsscript

--- database/src/main/resources/db/migration/V255__personhendelse_drop_pdl_snapshot.sql
+++ database/src/main/resources/db/migration/V255__personhendelse_drop_pdl_snapshot.sql
@@ -1,2 +1,8 @@
 ALTER TABLE personhendelse
     DROP COLUMN IF EXISTS pdl_snapshot;
+
+update personhendelse
+set pdl_diff = pdl_diff - 'pdlTreffAdresse'
+where pdl_diff is not null
+  and jsonb_typeof(pdl_diff) = 'object'
+  and pdl_diff ? 'pdlTreffAdresse';
--- database/src/main/resources/db/migration/V256__personhendelse_fjern_pdltreffadresse_fra_pdl_diff.sql
+++
@@ -1,5 +0,0 @@
-update personhendelse
-set pdl_diff = pdl_diff - 'pdlTreffAdresse'
-where pdl_diff is not null
-  and jsonb_typeof(pdl_diff) = 'object'
-  and pdl_diff ? 'pdlTreffAdresse';
