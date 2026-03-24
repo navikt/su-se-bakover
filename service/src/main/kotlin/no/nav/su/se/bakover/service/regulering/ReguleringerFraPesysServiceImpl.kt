1a67cc8a9afb1c30cf15ec7d68803e8c0fe36225
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2026-03-24 13:27:03 +0100

    Fiks import

--- service/src/main/kotlin/no/nav/su/se/bakover/service/regulering/ReguleringerFraPesysServiceImpl.kt
+++ service/src/main/kotlin/no/nav/su/se/bakover/service/regulering/ReguleringerFraPesysServiceImpl.kt
@@ -4,7 +4,6 @@ import arrow.core.Either
 import arrow.core.getOrElse
 import arrow.core.left
 import arrow.core.right
-import no.nav.su.se.bakover.client.pesys.AlderBeregningsperiode
 import no.nav.su.se.bakover.client.pesys.AlderBeregningsperioderPerPerson
 import no.nav.su.se.bakover.client.pesys.PesysClient
 import no.nav.su.se.bakover.client.pesys.PesysPeriode
