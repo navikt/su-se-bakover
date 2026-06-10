ab13c8cb0fd641b45e13300279b40079676e5573
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2026-06-10 14:41:53 +0200

    Ikke fiks testen her

--- common/infrastructure/src/test/kotlin/no/nav/su/se/bakover/common/infrastructure/persistence/MigrationsPostgresTest.kt
+++ common/infrastructure/src/test/kotlin/no/nav/su/se/bakover/common/infrastructure/persistence/MigrationsPostgresTest.kt
@@ -4,15 +4,12 @@ import io.kotest.matchers.shouldBe
 import no.nav.su.se.bakover.common.person.Fnr
 import no.nav.su.se.bakover.test.fixedTidspunkt
 import no.nav.su.se.bakover.test.generer
-import no.nav.su.se.bakover.test.persistence.DbExtension
+import no.nav.su.se.bakover.test.persistence.withMigratedDb
 import no.nav.su.se.bakover.test.persistence.withSession
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.extension.ExtendWith
 import java.util.UUID
-import javax.sql.DataSource
 
-@ExtendWith(DbExtension::class)
-internal class MigrationsPostgresTest(private val dataSource: DataSource) {
+internal class MigrationsPostgresTest {
 
     @Test
     fun `rader skal ikke lekke ut av withMigratedDb`() {
@@ -21,14 +18,17 @@ internal class MigrationsPostgresTest(private val dataSource: DataSource) {
                      insert into sak (id, fnr, opprettet, type) values ('${UUID.randomUUID()}', '${Fnr.generer()}', '$fixedTidspunkt', 'UFØRE')
             """.trimIndent().insert(emptyMap(), session)
         }
-        dataSource.withSession { session ->
-            insert(session)
-            "select count(1) from sak".antall(session = session) shouldBe 1
-        }
+        withMigratedDb { dataSource ->
 
-        // This test expects data to be cleared between test runs, which is handled by DbExtension
-        dataSource.withSession { session ->
-            "select count(1) from sak".antall(session = session) shouldBe 0
+            dataSource.withSession { session ->
+                insert(session)
+                "select count(1) from sak".antall(session = session) shouldBe 1
+            }
+        }
+        withMigratedDb { dataSource ->
+            dataSource.withSession { session ->
+                "select count(1) from sak".antall(session = session) shouldBe 0
+            }
         }
     }
 }
