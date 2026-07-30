67048f3260ec13cc2f85d4d506b1d0309e1bafe3
sevdeoguz <sevde.oguz@nav.no>
2026-07-30 10:21:34 +0200

    kommentar fix package

--- dokument/infrastructure/src/main/kotlin/no/nav/su/se/bakover/dokument/infrastructure/client/journalføring/kontrollnotat/JournalførKontrollnotatFakeClient.kt
+++ dokument/infrastructure/src/main/kotlin/no/nav/su/se/bakover/dokument/infrastructure/client/journalføring/kontrollnotat/JournalførKontrollnotatFakeClient.kt
@@ -1,9 +1,9 @@
 package no.nav.su.se.bakover.dokument.infrastructure.client.journalføring.kontrollnotat
 
-import JournalførKontrollnotatCommand
 import arrow.core.Either
 import arrow.core.right
 import dokument.domain.journalføring.kontrollnotat.JournalførKontrollnotatClient
+import dokument.domain.journalføring.kontrollnotat.JournalførKontrollnotatCommand
 import no.nav.su.se.bakover.common.domain.client.ClientError
 import no.nav.su.se.bakover.common.journal.JournalpostId
 import no.nav.su.se.bakover.dokument.infrastructure.client.journalføring.JournalpostIdGeneratorForFakes
