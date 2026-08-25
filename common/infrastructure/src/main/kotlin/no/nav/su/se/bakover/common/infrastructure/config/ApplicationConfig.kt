36d24eacc61dfb31bd4b4b3ed9a5fd0d21b5b030
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2026-08-25 12:31:22 +0200

    Ryddopp

--- common/infrastructure/src/main/kotlin/no/nav/su/se/bakover/common/infrastructure/config/ApplicationConfig.kt
+++ common/infrastructure/src/main/kotlin/no/nav/su/se/bakover/common/infrastructure/config/ApplicationConfig.kt
@@ -27,7 +27,6 @@ fun isDev(): Boolean {
     val runningEnv = getEnvironmentVariableOrDefault("NAIS_CLUSTER_NAME", "").toLowerCaseAsciiOnly()
     return runningEnv.contains("dev-gcp")
 }
-fun isQ1(): Boolean = getEnvironmentVariableOrDefault("DATABASE_NAME", "").toLowerCaseAsciiOnly().contains("q1")
 
 internal data object EnvironmentConfig {
     private val env by lazy {
@@ -175,18 +174,9 @@ data class ApplicationConfig(
         companion object {
             // GCP env vars postgres https://docs.nais.io/persistence/cloudsql/reference/?h=jdb#database-connnection
             fun createFromEnvironmentVariables(): DatabaseConfig {
-                if (isQ1()) {
-                    return StaticCredentials(
-                        getEnvironmentVariableOrThrow(DatabaseConfigEnvs.DB_JDBC_URL.key()),
-                        getEnvironmentVariableOrThrow(DatabaseConfigEnvs.DB_USERNAME.key()),
-                        getEnvironmentVariableOrThrow(DatabaseConfigEnvs.DB_PASSWORD.key()),
-                    )
-                }
                 return StaticCredentials(
                     getEnvironmentVariableOrThrow(DatabaseConfigEnvs.DB_JDBC_URL.key()),
-                    getEnvironmentVariableOrThrow(
-                        DatabaseConfigEnvs.DB_USERNAME.key(),
-                    ),
+                    getEnvironmentVariableOrThrow(DatabaseConfigEnvs.DB_USERNAME.key()),
                     getEnvironmentVariableOrThrow(DatabaseConfigEnvs.DB_PASSWORD.key()),
                 )
             }
