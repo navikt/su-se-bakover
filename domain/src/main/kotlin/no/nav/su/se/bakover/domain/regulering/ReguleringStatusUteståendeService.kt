fab9f88b0e9ce3983ce1143c4f85d9ac5dd05328
Bjorn Golberg <bjorn.golberg@gmail.com>
2026-08-31 10:56:34 +0200

    Mer logging reguleringsstatus

--- domain/src/main/kotlin/no/nav/su/se/bakover/domain/regulering/ReguleringStatusUteståendeService.kt
+++ domain/src/main/kotlin/no/nav/su/se/bakover/domain/regulering/ReguleringStatusUteståendeService.kt
@@ -38,7 +38,7 @@ class ReguleringStatusUteståendeService(
         if (reguleringStatusRepo.hentPågående().isNotEmpty()) {
             return StatusPågående.left()
         }
-        CoroutineScope(Dispatchers.IO).launch {
+        val job = CoroutineScope(Dispatchers.IO).launch {
             val idPågående = reguleringStatusRepo.lagreOppstartet()
             Either.catch {
                 produserStatusSisteGrunnbeløp(aar, idPågående)
@@ -50,6 +50,7 @@ class ReguleringStatusUteståendeService(
                 reguleringStatusRepo.lagreFeilet(idPågående)
             }
         }
+        job.hashCode()
         return StatusFullført.right()
     }
 
@@ -69,7 +70,10 @@ class ReguleringStatusUteståendeService(
         val sisteBeløper = satsFactory.grunnbeløpOgGarantipensjon(etterspurtMai)
 
         val (løpende, sakerMedGammeltGrunnbeløp) = sessionFactory.withTransactionContext { tx ->
+            var antallSaker = 0
             val sakInfoMedVedtakTidslinje = alleSaker.mapNotNull { sak ->
+                antallSaker++
+                log.info("hentStatusSisteGrunnbeløp henter vedtakslinjer for sak ${sak.saksnummer}, $antallSaker/${alleSaker.size}")
                 val vedtakSomKanRevurderes =
                     vedtakRepo.hentVedtakSomKanRevurderesForSakFraOgMed(sak.sakId, etterspurtMai, tx)
                 val vedtakstidslinje =
@@ -84,8 +88,11 @@ class ReguleringStatusUteståendeService(
             }
 
             // sender med sakInfoMedVedtakTidslinje for å ha med antall senere
+            var antallVedtaksdata = 0
             sakInfoMedVedtakTidslinje to sakInfoMedVedtakTidslinje.mapNotNull { (sakInfo, vedtaksdata) ->
                 vedtaksdata.firstNotNullOfOrNull {
+                    antallVedtaksdata++
+                    log.info("hentStatusSisteGrunnbeløp henter reguleringsstatus for sak ${sakInfo.saksnummer}, $antallVedtaksdata/${alleSaker.size}")
                     val beregning = it.originaltVedtak.beregning
                     if (beregning != null) {
                         val månedsbesberegning: Månedsberegning = beregning.getMånedsberegninger().first {
