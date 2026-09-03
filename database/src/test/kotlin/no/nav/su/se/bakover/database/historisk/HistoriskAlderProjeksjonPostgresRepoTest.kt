e4c531341f08948b6465ccbbcc9e760770063ce3
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2026-09-03 14:25:26 +0200

    Fiks compile

--- database/src/test/kotlin/no/nav/su/se/bakover/database/historisk/HistoriskAlderProjeksjonPostgresRepoTest.kt
+++ database/src/test/kotlin/no/nav/su/se/bakover/database/historisk/HistoriskAlderProjeksjonPostgresRepoTest.kt
@@ -28,6 +28,7 @@ import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.api.extension.ExtendWith
 import java.math.BigDecimal
 import java.time.LocalDate
+import java.util.UUID
 import javax.sql.DataSource
 
 @ExtendWith(DbExtension::class)
