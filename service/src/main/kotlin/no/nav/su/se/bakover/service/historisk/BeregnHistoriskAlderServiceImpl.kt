05ac089aa66a449172151402dc5d6cf158464b4e
Bjorn Golberg <bjorn.golberg@gmail.com>
2026-09-07 13:32:08 +0200

    Forsøk på å simulere

--- service/src/main/kotlin/no/nav/su/se/bakover/service/historisk/BeregnHistoriskAlderServiceImpl.kt
+++ service/src/main/kotlin/no/nav/su/se/bakover/service/historisk/BeregnHistoriskAlderServiceImpl.kt
@@ -4,15 +4,25 @@ import beregning.domain.BeregningFactory
 import beregning.domain.BeregningMedFradragBeregnetMånedsvis
 import beregning.domain.BeregningStrategy
 import beregning.domain.Beregningsperiode
+import no.nav.su.se.bakover.common.domain.Saksnummer
 import no.nav.su.se.bakover.common.domain.sak.Sakstype
+import no.nav.su.se.bakover.common.ident.NavIdentBruker
+import no.nav.su.se.bakover.common.person.Fnr
 import no.nav.su.se.bakover.common.tid.periode.Periode
+import no.nav.su.se.bakover.domain.oppdrag.Utbetalingsstrategi
+import no.nav.su.se.bakover.domain.oppdrag.simulering.simulerUtbetaling
 import satser.domain.SatsFactory
 import vilkår.inntekt.domain.grunnlag.Fradrag
+import økonomi.application.utbetaling.UtbetalingService
+import økonomi.domain.utbetaling.Utbetalinger
+import økonomi.domain.utbetaling.UtbetalingsinstruksjonForEtterbetalinger
 import java.time.Clock
+import java.util.UUID
 
 class BeregnHistoriskAlderServiceImpl(
-    val satsFactory: SatsFactory,
-    val clock: Clock,
+    private val satsFactory: SatsFactory,
+    private val utbetalingService: UtbetalingService,
+    private val clock: Clock,
 ) {
 
     fun beregnHistoriskAlder(grunnlag: HistoriskAlderBeregning.Grunnlag): HistoriskAlderBeregning {
@@ -26,6 +36,28 @@ class BeregnHistoriskAlderServiceImpl(
             beregningsperioder = beregningsperioder,
             fradrag = grunnlag.fradrag,
         )
+
+        val tidligereUtbetalinger = Utbetalinger()
+
+        val utbetalingForSimulering = Utbetalingsstrategi.NyAldersUtbetaling(
+            sakId = UUID.randomUUID(), // TODO finnes ikke i infotrygd så kan bare få et nytt et i suapp?
+            saksnummer = Saksnummer(123L), // TODO erstatt med saksnummer for historisk
+            fnr = Fnr.tryCreate("")!!,
+            eksisterendeUtbetalinger = tidligereUtbetalinger,
+            behandler = NavIdentBruker.Saksbehandler.systembruker(),
+            beregning = beregning,
+            clock = clock,
+            kjøreplan = UtbetalingsinstruksjonForEtterbetalinger.SammenMedNestePlanlagteUtbetaling,
+            sakstype = Sakstype.ALDER,
+            aksepterKvitteringMedFeil = false, // TODO hva betyr denne??
+        ).generate()
+
+        val simulertUtbetaling = simulerUtbetaling(
+            tidligereUtbetalinger = tidligereUtbetalinger,
+            utbetalingForSimulering = utbetalingForSimulering,
+            simuler = utbetalingService::simulerUtbetaling,
+        )
+
         return HistoriskAlderBeregning(beregning)
     }
 }
