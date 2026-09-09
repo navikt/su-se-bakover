841c86fcc16f17b435c1abc6b630ae4a62727450
Bjorn Golberg <bjorn.golberg@gmail.com>
2026-09-09 14:37:09 +0200

    Glemte et felt

--- domain/src/main/kotlin/no/nav/su/se/bakover/domain/sak/SakVedtak.kt
+++ domain/src/main/kotlin/no/nav/su/se/bakover/domain/sak/SakVedtak.kt
@@ -21,7 +21,6 @@ import java.util.UUID
  */
 fun Sak.hentBeregningForGjenopptakAvYtelse(
     vedtakId: UUID,
-    begrunnelse: String? = null,
     beregningStrategyFactory: BeregningStrategyFactory,
     clock: Clock,
 ): Beregning {
