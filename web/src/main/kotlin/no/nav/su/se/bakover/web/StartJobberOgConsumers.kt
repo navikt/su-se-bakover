f415d2015a2df5a47cb1c4c220084ad3c76a9deb
sevdeoguz <sevde.oguz@nav.no>
2026-09-15 12:21:32 +0200

    utvider tiden (#2988)
    * utvider tiden
    
    * endrer tid til 5 minutter

--- web/src/main/kotlin/no/nav/su/se/bakover/web/StartJobberOgConsumers.kt
+++ web/src/main/kotlin/no/nav/su/se/bakover/web/StartJobberOgConsumers.kt
@@ -184,7 +184,7 @@ private fun localJobberOgConsumers(
     val jobber = listOf(
         JournalførDokumentJob.startJob(
             initialDelay = initialDelay.next(),
-            periode = Duration.ofMinutes(1),
+            periode = Duration.ofMinutes(5),
             runCheckFactory = runCheckFactory,
             journalføringService = JournalføringService(
                 journalførDokumentService = journalførDokumentService,
@@ -194,7 +194,7 @@ private fun localJobberOgConsumers(
 
         DistribuerDokumentJob.startJob(
             initialDelay = initialDelay.next(),
-            periode = Duration.ofMinutes(1),
+            periode = Duration.ofMinutes(5),
             runCheckFactory = runCheckFactory,
             distribueringService = distribuerDokumentService,
         ),
@@ -245,7 +245,7 @@ private fun localJobberOgConsumers(
 
         LokalMottaKravgrunnlagJob.startJob(
             initialDelay = initialDelay.next(),
-            intervall = Duration.ofMinutes(1),
+            intervall = Duration.ofMinutes(5),
             sessionFactory = databaseRepos.sessionFactory,
             service = tilbakekrevingskomponenter.services.råttKravgrunnlagService,
             clock = clock,
@@ -259,27 +259,27 @@ private fun localJobberOgConsumers(
             oppdaterOppgaveKonsument = tilbakekrevingskomponenter.services.oppdaterOppgaveForTilbakekrevingshendelserKonsument,
             genererVedtaksbrevTilbakekrevingKonsument = tilbakekrevingskomponenter.services.vedtaksbrevTilbakekrevingKonsument,
             initialDelay = initialDelay.next(),
-            intervall = Duration.ofMinutes(1),
+            intervall = Duration.ofMinutes(5),
             runCheckFactory = runCheckFactory,
         ),
 
         DokumentJobber.startJob(
             initialDelay = initialDelay.next(),
-            intervall = Duration.ofMinutes(1),
+            intervall = Duration.ofMinutes(5),
             runCheckFactory = runCheckFactory,
             journalførtDokumentHendelserKonsument = dokumentKomponenter.services.journalførtDokumentHendelserKonsument,
             distribuerDokumentHendelserKonsument = dokumentKomponenter.services.distribuerDokumentHendelserKonsument,
         ),
 
         SendPåminnelseNyStønadsperiodeJob.startJob(
-            intervall = Duration.ofMinutes(1),
+            intervall = Duration.ofMinutes(5),
             initialDelay = initialDelay.next(),
             sendPåminnelseService = services.sendPåminnelserOmNyStønadsperiodeService,
             runCheckFactory = runCheckFactory,
         ),
 
         StansYtelseVedManglendeOppmøteKontrollsamtaleJob.startJob(
-            intervall = Duration.ofMinutes(1),
+            intervall = Duration.ofMinutes(5),
             initialDelay = initialDelay.next(),
             service = services.kontrollsamtaleSetup.utløptFristForKontrollsamtaleService,
             runCheckFactory = runCheckFactory,
@@ -287,12 +287,12 @@ private fun localJobberOgConsumers(
 
         LagStønadstatistikkForMånedJob.startJob(
             initialDelay = initialDelay.next(),
-            periode = Duration.of(1, ChronoUnit.MINUTES),
+            periode = Duration.of(5, ChronoUnit.MINUTES),
             runCheckFactory = runCheckFactory,
             stønadStatistikkJobService = services.stønadStatistikkJobService,
         ),
         ForsøkJournalføringKontrollnotatJob.startJob(
-            intervall = Duration.ofMinutes(1),
+            intervall = Duration.ofMinutes(5),
             initialDelay = initialDelay.next(),
             service = services.kontrollsamtaleNotatService,
             runCheckFactory = runCheckFactory,
