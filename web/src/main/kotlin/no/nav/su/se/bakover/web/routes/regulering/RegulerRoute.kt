dcf083f9a4be5115088926b2985ac2b583638aa6
Bjørn Golberg <bjorn.golberg@gmail.com>
2026-09-24 10:25:27 +0200

    Dokumenttilstand brev regulering (#3005)
    * Videreføre oppgaveid under attestering
    
    * Lagre vedtaksdokument for regulering riktig slik at det kobles til vedtaket
    
    * Sjekker om bruker har adresse under manuell regulering hvis det skal være brev
    
    * Riktig http respons
    
    * Unittester for manuell regulering
    
    * Flere tester

--- web/src/main/kotlin/no/nav/su/se/bakover/web/routes/regulering/RegulerRoute.kt
+++ web/src/main/kotlin/no/nav/su/se/bakover/web/routes/regulering/RegulerRoute.kt
@@ -551,7 +551,6 @@ internal fun KunneIkkeRegulereManuelt.tilResultat() = when (this) {
     KunneIkkeRegulereManuelt.SaksbehandlerKanIkkeAttestere -> reguleringSaksbehandlerKanIkkeAttestere
     KunneIkkeRegulereManuelt.KunneIkkeHenteOppgave -> kunneIkkeHenteGosysoppgave
     KunneIkkeRegulereManuelt.KunneIkkeOppretteOppgave -> kunneIkkeOppretteGosysoppgave
-    is KunneIkkeRegulereManuelt.KunneIkkeGenerereVedtaksbrev,
-    KunneIkkeRegulereManuelt.KunneIkkeLagreVedtaksbrev,
-    -> Feilresponser.Brev.kunneIkkeGenerereBrev
+    is KunneIkkeRegulereManuelt.KunneIkkeGenerereVedtaksbrev -> Feilresponser.Brev.kunneIkkeGenerereBrev
+    is KunneIkkeRegulereManuelt.FantIkkeAdresseTilBruker -> Feilresponser.Brev.fantIkkeAdresseTilBruker
 }
